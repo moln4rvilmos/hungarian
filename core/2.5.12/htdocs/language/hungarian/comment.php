e332c9242fa7cd9851b1903553d84cd82ebdbfa5
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:27:17 +0100

    Translate comment.php to Hungarian

--- core/2.5.12/htdocs/language/hungarian/comment.php
+++ core/2.5.12/htdocs/language/hungarian/comment.php
@@ -1,45 +1,45 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_CM_TITLE', 'Title');
-define('_CM_MESSAGE', 'Message');
-define('_CM_DOSMILEY', 'Enable Smiley Icons');
-define('_CM_DOHTML', 'Enable HTML Tags');
-define('_CM_DOAUTOWRAP', 'Auto wrap lines');
-define('_CM_DOXCODE', 'Enable XOOPS Codes');
-define('_CM_REFRESH', 'Refresh');
-define('_CM_PENDING', 'Pending');
-define('_CM_HIDDEN', 'Hidden');
-define('_CM_ACTIVE', 'Active');
-define('_CM_STATUS', 'Status');
-define('_CM_POSTCOMMENT', 'Publish Comment');
-define('_CM_REPLIES', 'Replies');
-define('_CM_PARENT', 'Parent');
-define('_CM_TOP', 'Top');
-define('_CM_BOTTOM', 'Bottom');
+// Translator: XOOPS Magyar Fordító Csapat
+define('_CM_TITLE', 'Cím');
+define('_CM_MESSAGE', 'Üzenet');
+define('_CM_DOSMILEY', 'Engedélyezi a hangulatjeleket (Smiley ikonokat)');
+define('_CM_DOHTML', 'Engedélyezi a HTML tageket');
+define('_CM_DOAUTOWRAP', 'Automatikus sortörés');
+define('_CM_DOXCODE', 'Engedélyezi a XOOPS kódokat (XCode)');
+define('_CM_REFRESH', 'Frissítés');
+define('_CM_PENDING', 'Függőben');
+define('_CM_HIDDEN', 'Rejtett');
+define('_CM_ACTIVE', 'Aktív');
+define('_CM_STATUS', 'Állapot');
+define('_CM_POSTCOMMENT', 'Megjegyzés közzététele');
+define('_CM_REPLIES', 'Válaszok');
+define('_CM_PARENT', 'Szülő');
+define('_CM_TOP', 'Teteje');
+define('_CM_BOTTOM', 'Alja');
 define('_CM_ONLINE', 'Online!');
-define('_CM_POSTED', 'Published'); // Posted date
-define('_CM_UPDATED', 'Updated');
-define('_CM_THREAD', 'Thread');
-define('_CM_POSTER', 'Author');
-define('_CM_JOINED', 'Joined');
-define('_CM_POSTS', 'Comments');
-define('_CM_FROM', 'From');
-define('_CM_COMDELETED', 'Comment(s) deleted.');
-define('_CM_COMDELETENG', 'Could not delete comment.');
-define('_CM_DELETESELECT', 'Delete all its child comments?');
-define('_CM_DELETEONE', 'No, delete only this comment');
-define('_CM_DELETEALL', 'Yes, delete all');
-define('_CM_THANKSPOST', 'Thanks for your comments!');
-define('_CM_NOTICE', "The comments are owned by the author. We aren't responsible for their content.");
-define('_CM_COMRULES', 'Comment Rules');
-define('_CM_COMAPPROVEALL', 'Comments are always approved');
-define('_CM_COMAPPROVEUSER', 'Comments by registered users are always approved');
-define('_CM_COMAPPROVEADMIN', 'All comments need to be approved by administrator');
-define('_CM_COMANONPOST', 'Allow anonymous comments?');
-define('_CM_COMNOCOM', 'Disable comments');
-define('_CM_USER', 'Name');
-define('_CM_EMAIL', 'Email');
-define('_CM_URL', 'Website');
+define('_CM_POSTED', 'Közzétéve'); // A közzététel dátuma
+define('_CM_UPDATED', 'Frissítve');
+define('_CM_THREAD', 'Beszélgetés (Thread)');
+define('_CM_POSTER', 'Szerző');
+define('_CM_JOINED', 'Csatlakozott');
+define('_CM_POSTS', 'Hozzászólások');
+define('_CM_FROM', 'Innen');
+define('_CM_COMDELETED', 'A megjegyzés(ek) törölve.');
+define('_CM_COMDELETENG', 'Nem sikerült törölni a megjegyzést.');
+define('_CM_DELETESELECT', 'Törlöd az összes gyermek megjegyzést is?');
+define('_CM_DELETEONE', 'Nem, csak ezt a megjegyzést törlöm');
+define('_CM_DELETEALL', 'Igen, törlöm az összeset');
+define('_CM_THANKSPOST', 'Köszönjük a megjegyzésedet!');
+define('_CM_NOTICE', "A megjegyzések a szerző tulajdonát képezik. Nem vállalunk felelősséget a tartalmukért.");
+define('_CM_COMRULES', 'Megjegyzési Szabályok');
+define('_CM_COMAPPROVEALL', 'A megjegyzések mindig jóváhagyottak');
+define('_CM_COMAPPROVEUSER', 'A regisztrált felhasználók megjegyzései mindig jóváhagyottak');
+define('_CM_COMAPPROVEADMIN', 'Minden megjegyzést az adminisztrátornak kell jóváhagynia');
+define('_CM_COMANONPOST', 'Engedélyezed a névtelen megjegyzéseket?');
+define('_CM_COMNOCOM', 'Megjegyzések letiltása');
+define('_CM_USER', 'Név');
+define('_CM_EMAIL', 'E-mail');
+define('_CM_URL', 'Webhely');
