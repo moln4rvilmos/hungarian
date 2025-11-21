bedf46ed5fe19a62e21e570e45c38bb3eee9b4ce
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:25:02 +0100

    Translate banner management strings to Hungarian
    Updated Hungarian translations for banner management strings.

--- core/2.5.12/htdocs/language/hungarian/banners.php
+++ core/2.5.12/htdocs/language/hungarian/banners.php
@@ -1,62 +1,61 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_BANNERS_MANAGEMENT', 'Banner Management');
-define('_BANNERS_WELCOMEBACK', 'Welcome back: %s');
-//define('_BANNERS_LOGGEDOUT','Logged out the advertisement panel');
-define('_BANNERS_LOGOUT', 'Logout Banner Management');
-define('_BANNERS_LOGIN_TITLE', 'Banner Statistics');
-define('_BANNERS_LOGIN_LOGIN', 'Login :');
-define('_BANNERS_LOGIN_INCORRECT', 'Incorrect Username or Password');
-define('_BANNERS_LOGIN_PASS', 'Password :');
-define('_BANNERS_LOGIN_INFO', 'Please type your client information');
-define('_BANNERS_LOGIN_OK', 'OK');
-define('_BANNERS_ID', 'Banner ID:');
-define('_BANNERS_TITLE', 'Active Banners');
-define('_BANNERS_URL', 'Change URL:');
-define('_BANNERS_NOTHINGFOUND', 'No Banners Found');
-define('_BANNERS_NO', '#');
-define('_BANNERS_FOW_IN', 'Displaying Active Banners: ');
-define('_BANNERS_IMP_MADE', 'Imp. Made');
-define('_BANNERS_IMP_TOTAL', 'Imp. Total');
-define('_BANNERS_IMP_LEFT', 'Imp. Left');
-define('_BANNERS_CLICKS', 'Clicks');
-define('_BANNERS_PER_CLICKS', '% Clicks');
-define('_BANNERS_FUNCTIONS', 'Functions');
-define('_BANNERS_STATS', 'Email Stats');
-define('_BANNERS_SHOWBANNER', 'Show Banner');
-define('_BANNERS_SEND_STATS', 'Send <a href=\'%s\' title=\'E-mail Stats\'/>Email Stats</a> for this Banner');
-define('_BANNERS_POINTS', 'This Banner points to <a href=\'%s\' title=\'\'>this URL</a>');
-define('_BANNERS_UNLIMITED', 'Unlimited');
-define('_BANNERS_FINISHED', 'Expired Banners');
-define('_BANNERS_STARTED', 'Start Date');
-define('_BANNERS_ENDED', 'End Date');
-define('_BANNERS_MAIL_SUBJECT', 'Your Banner Statistics at %s');
-define('_BANNERS_MAIL_MESSAGE', 'Available Banner Statistics for the selected Banner at %s :\n\n\n
-Client Name: %s\nBanner ID: %s\n
-Banner Image: %s\n
-Banner URL: %s\n\n
-Impressions Purchased: %s\n
-Impressions Made: %s\n
-Impressions Left: %s\n
-Clicks Received: %s\n
-Clicks Percent: %f \n\n\n
-Report Generated on: %s');
-define('_BANNERS_MAIL_NOT_OK', 'We have encountered an error sending your email. Please contact the webmaster regarding this issue.');
-define('_BANNERS_MAIL_OK', 'Available Banner statistics for the selected banner have been sent to your account email address.');
-define('_BANNERS_MAIL_ERROR', 'There is no email associated with client %s.<br>Please contact the Administrator');
-define('_BANNERS_DBUPDATED', 'Item modified and database updated');
-define('_BANNERS_DBERROR', 'Database was not updated due to an error!');
-define('_BANNERS_CHANGE', 'Change');
+// Translator: XOOPS Magyar Fordító Csapat
 
-define('_BANNERS_NO_LOGIN_DATA', 'No login data detected');
-define('_BANNERS_NO_REFERER', 'No referer detected');
-define('_BANNERS_NO_ID', 'No valid ID detected');
+define('_BANNERS_MANAGEMENT', 'Banner Kezelés');
+define('_BANNERS_WELCOMEBACK', 'Üdvözöllek újra: %s');
+//define('_BANNERS_LOGGEDOUT','Kijelentkeztél a hirdetési panelről');
+define('_BANNERS_LOGOUT', 'Kijelentkezés a Banner Kezelésből');
+define('_BANNERS_LOGIN_TITLE', 'Banner Statisztika');
+define('_BANNERS_LOGIN_LOGIN', 'Bejelentkezési név:');
+define('_BANNERS_LOGIN_INCORRECT', 'Helytelen felhasználónév vagy jelszó');
+define('_BANNERS_LOGIN_PASS', 'Jelszó:');
+define('_BANNERS_LOGIN_INFO', 'Kérlek, add meg az ügyfél adataidat');
+define('_BANNERS_LOGIN_OK', 'OK');
+define('_BANNERS_ID', 'Banner azonosító (ID):');
+define('_BANNERS_TITLE', 'Aktív Bannerek');
+define('_BANNERS_URL', 'URL módosítása:');
+define('_BANNERS_NOTHINGFOUND', 'Nincsenek Bannerek Találhatók');
+define('_BANNERS_NO', '#');
+define('_BANNERS_FOW_IN', 'Aktív bannerek megjelenítése: ');
+define('_BANNERS_IMP_MADE', 'Megjelenítések száma');
+define('_BANNERS_IMP_TOTAL', 'Összes megvásárolt megjelenítés');
+define('_BANNERS_IMP_LEFT', 'Hátralévő megjelenítések');
+define('_BANNERS_CLICKS', 'Kattintások');
+define('_BANNERS_PER_CLICKS', '% Kattintás');
+define('_BANNERS_FUNCTIONS', 'Funkciók');
+define('_BANNERS_STATS', 'E-mail statisztika');
+define('_BANNERS_SHOWBANNER', 'Banner Megjelenítése');
+define('_BANNERS_SEND_STATS', 'Statisztika <a href=\'%s\' title=\'E-mail Statisztika\'/>e-mailben történő elküldése</a> ehhez a bannerhez');
+define('_BANNERS_POINTS', 'Ez a banner erre az <a href=\'%s\' title=\'\'>URL-re mutat</a>');
+define('_BANNERS_UNLIMITED', 'Korlátlan');
+define('_BANNERS_FINISHED', 'Lejárt Bannerek');
+define('_BANNERS_STARTED', 'Kezdés Dátuma');
+define('_BANNERS_ENDED', 'Befejezés Dátuma');
+define('_BANNERS_MAIL_SUBJECT', 'A Banner statisztikáid itt: %s');
+define('_BANNERS_MAIL_MESSAGE', 'Elérhető banner statisztikák a kiválasztott bannerhez itt: %s :\n\n\n
+Ügyfél neve: %s\nBanner azonosító: %s\n
+Banner képe: %s\n
+Banner URL: %s\n\n
+Megvásárolt megjelenítések: %s\n
+Megvalósult megjelenítések: %s\n
+Hátralévő megjelenítések: %s\n
+Fogadott kattintások: %s\n
+Kattintások százaléka: %f \n\n\n
+Jelentés generálásának ideje: %s');
+define('_BANNERS_MAIL_NOT_OK', 'Hiba történt az e-mail elküldése során. Kérlek, lépj kapcsolatba a webmesterrel a probléma miatt.');
+define('_BANNERS_MAIL_OK', 'A kiválasztott bannerhez elérhető statisztikák elküldve a fiókodhoz tartozó e-mail címre.');
+define('_BANNERS_MAIL_ERROR', 'Nincs e-mail cím társítva a(z) %s ügyfélhez.<br>Kérlek, lépj kapcsolatba az Adminisztrátorral');
+define('_BANNERS_DBUPDATED', 'Az elem módosítva és az adatbázis frissítve');
+define('_BANNERS_DBERROR', 'Hiba miatt az adatbázis nem frissült!');
+define('_BANNERS_CHANGE', 'Módosítás');
+
+define('_BANNERS_NO_LOGIN_DATA', 'Nem észlelhető bejelentkezési adat');
+define('_BANNERS_NO_REFERER', 'Nem észlelhető hivatkozó (referer)');
+define('_BANNERS_NO_ID', 'Nem észlelhető érvényes azonosító (ID)');
 
 //2.5.12
 
-define('_BANNERS_NO_FLASH', '>Sorry, Flash (.swf) files are no longer supported. Please use a modern video format.');
-
-
+define('_BANNERS_NO_FLASH', '>Sajnáljuk, a Flash (.swf) fájlok már nem támogatottak. Kérlek, használj modern videó formátumot.');
