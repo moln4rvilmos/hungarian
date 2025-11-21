11c7b1b4854bdb8c0fefb7b26401410f2555cd18
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:42:06 +0100

    Translate module messages to Hungarian

--- core/2.5.12/htdocs/language/hungarian/xmf.php
+++ core/2.5.12/htdocs/language/hungarian/xmf.php
@@ -1,8 +1,8 @@
 <?php
 
-define('_AM_XMF_MODULE_NOTFOUND','Please install or reactivate %1$s module. Minimum version required: %2$s');
-define('_AM_XMF_MODULE_VERSION','Minimum %1$s module version required: %2$s (your version is %3$s)');
-define('_AM_XMF_MODULE_INSTALLED', 'The module \'%s\' is installed!');
-define('_AM_XMF_MODULE_NOT_INSTALLED', 'The module \'%s\' is not installed!');
+define('_AM_XMF_MODULE_NOTFOUND','Kérlek, telepítsd vagy aktiváld újra a(z) %1$s modult. Minimális szükséges verzió: %2$s');
+define('_AM_XMF_MODULE_VERSION','A(z) %1$s modul minimálisan szükséges verziója: %2$s (a te verziód: %3$s)');
+define('_AM_XMF_MODULE_INSTALLED', 'A(z) \'%s\' modul telepítve van!');
+define('_AM_XMF_MODULE_NOT_INSTALLED', 'A(z) \'%s\' modul nincs telepítve!');
 
-define('_DB_XMF_TABLE_IS_NOT_DEFINED','Table is not defined');
+define('_DB_XMF_TABLE_IS_NOT_DEFINED','A tábla nincs definiálva');
