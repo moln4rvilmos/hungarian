ef8d758769764f57a57c66cb238e18229355e9ef
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:40:36 +0100

    Translate uploader.php error messages to Hungarian

--- core/2.5.12/htdocs/language/hungarian/uploader.php
+++ core/2.5.12/htdocs/language/hungarian/uploader.php
@@ -1,37 +1,37 @@
 <?php
 //
-// _LANGCODE: en
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_ER_UP_MIMETYPELOAD', 'Error loading mime types definition');
-define('_ER_UP_FILENOTFOUND', 'File not found');
-define('_ER_UP_INVALIDFILESIZE', 'Invalid File Size');
-define('_ER_UP_FILENAMEEMPTY', 'Filename Is Missing');
-define('_ER_UP_NOFILEUPLOADED', 'No file uploaded');
-define('_ER_UP_ERROROCCURRED', 'Unexpected Error #%s');
-define('_ER_UP_UPLOADDIRNOTSET', 'Upload directory not set');
-define('_ER_UP_FAILEDOPENDIR', 'Failed opening directory: %s');
-define('_ER_UP_FAILEDOPENDIRWRITE', 'Failed opening directory with write permission: %s');
-define('_ER_UP_FILESIZETOOLARGE', 'File size too large (Maximum %u bytes): %u bytes');
-define('_ER_UP_FILEWIDTHTOOLARGE', 'File width too large (Maximum %u px): %u px');
-define('_ER_UP_FILEHEIGHTTOOLARGE', 'File height too large (Maximum %u px): %u px');
-define('_ER_UP_MIMETYPENOTALLOWED', 'File of MIME type: %s is not allowed');
-define('_ER_UP_FAILEDUPLOADFILE', 'Failed uploading file: %s');
-define('_ER_UP_FAILEDFETCHIMAGESIZE', 'Failed fetching image size of %s, skipping max dimension check..');
-define('_ER_UP_UNKNOWNFILETYPEREJECTED', "The file you're trying to upload is not supported by this site/server.");
-define('_ER_UP_ERRORSRETURNED', 'Errors Returned While Uploading file: %s');
-define('_ER_UP_INVALIDIMAGEFILE', 'Invalid image file');
-define('_ER_UP_SUSPICIOUSREFUSED', 'Suspicious image upload refused');
-define('_ER_UP_INVALIDFILENAME', 'Invalid file name');
-define('_ER_UP_FAILEDSAVEFILE', 'Failed to save file to %s');
-define('_ER_UP_INISIZE', 'The uploaded file exceeds the upload_max_filesize directive in php.ini');
-define('_ER_UP_FORMSIZE', 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form');
-define('_ER_UP_PARTIAL', 'The uploaded file was only partially uploaded');
-define('_ER_UP_NOFILE', 'No file was uploaded');
-define('_ER_UP_NOTMPDIR', 'Missing a temporary folder');
-define('_ER_UP_CANTWRITE', 'Failed to write file to disk');
-define('_ER_UP_EXTENSION', 'File upload stopped by extension. View phpinfo()');
-define('_ER_UP_UNKNOWN', 'Unknown upload error');
-define('_ER_UP_INDEXNOTSET', 'Attempt to fetch multiple file, index must be set.');
+// Translator: XOOPS Magyar Fordító Csapat
+define('_ER_UP_MIMETYPELOAD', 'Hiba történt a MIME típusok definíciójának betöltésekor');
+define('_ER_UP_FILENOTFOUND', 'A fájl nem található');
+define('_ER_UP_INVALIDFILESIZE', 'Érvénytelen fájlméret');
+define('_ER_UP_FILENAMEEMPTY', 'A fájlnév hiányzik');
+define('_ER_UP_NOFILEUPLOADED', 'Nem töltöttél fel fájlt');
+define('_ER_UP_ERROROCCURRED', 'Váratlan hiba történt #%s');
+define('_ER_UP_UPLOADDIRNOTSET', 'A feltöltési könyvtár nincs beállítva');
+define('_ER_UP_FAILEDOPENDIR', 'Nem sikerült megnyitni a könyvtárat: %s');
+define('_ER_UP_FAILEDOPENDIRWRITE', 'Nem sikerült megnyitni a könyvtárat írási engedéllyel: %s');
+define('_ER_UP_FILESIZETOOLARGE', 'A fájlméret túl nagy (Maximum %u bájt): %u bájt');
+define('_ER_UP_FILEWIDTHTOOLARGE', 'A fájl szélessége túl nagy (Maximum %u px): %u px');
+define('_ER_UP_FILEHEIGHTTOOLARGE', 'A fájl magassága túl nagy (Maximum %u px): %u px');
+define('_ER_UP_MIMETYPENOTALLOWED', 'A(z) %s MIME típusú fájl nem engedélyezett');
+define('_ER_UP_FAILEDUPLOADFILE', 'Nem sikerült feltölteni a fájlt: %s');
+define('_ER_UP_FAILEDFETCHIMAGESIZE', 'Nem sikerült lekérni a(z) %s képméretét, a maximális dimenzió ellenőrzése kimarad..');
+define('_ER_UP_UNKNOWNFILETYPEREJECTED', 'Az a fájl, amit fel akarsz tölteni, nem támogatott ezen a webhelyen/szerveren.');
+define('_ER_UP_ERRORSRETURNED', 'Hibák adódtak a fájl feltöltése során: %s');
+define('_ER_UP_INVALIDIMAGEFILE', 'Érvénytelen képfájl');
+define('_ER_UP_SUSPICIOUSREFUSED', 'Gyanús képfeltöltés elutasítva');
+define('_ER_UP_INVALIDFILENAME', 'Érvénytelen fájlnév');
+define('_ER_UP_FAILEDSAVEFILE', 'Nem sikerült menteni a fájlt ide: %s');
+define('_ER_UP_INISIZE', 'A feltöltött fájl meghaladja a php.ini-ben megadott upload_max_filesize direktívát');
+define('_ER_UP_FORMSIZE', 'A feltöltött fájl meghaladja a HTML űrlapon megadott MAX_FILE_SIZE direktívát');
+define('_ER_UP_PARTIAL', 'A fájl csak részben lett feltöltve');
+define('_ER_UP_NOFILE', 'Nem lett fájl feltöltve');
+define('_ER_UP_NOTMPDIR', 'Hiányzik az ideiglenes mappa');
+define('_ER_UP_CANTWRITE', 'Nem sikerült a fájlt lemezre írni');
+define('_ER_UP_EXTENSION', 'A fájlfeltöltést egy bővítmény leállította. Nézd meg a phpinfo()-t');
+define('_ER_UP_UNKNOWN', 'Ismeretlen feltöltési hiba');
+define('_ER_UP_INDEXNOTSET', 'Több fájl lekérésének kísérlete, az indexet be kell állítani.');
 // 2.5.11
-define('_ER_UP_MODE_NOT_CHANGED', 'File uploaded, but could not change the mode');
+define('_ER_UP_MODE_NOT_CHANGED', 'Fájl feltöltve, de nem sikerült a módot megváltoztatni');
