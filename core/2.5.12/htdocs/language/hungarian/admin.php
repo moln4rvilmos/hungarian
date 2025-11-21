8b2ef92dd3b9ff52569e95f1f55ca1799d8a824b
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:22:51 +0100

    Translate admin.php strings to Hungarian

--- core/2.5.12/htdocs/language/hungarian/admin.php
+++ core/2.5.12/htdocs/language/hungarian/admin.php
@@ -1,23 +1,23 @@
 <?php
 //
-// _LANGCODE: en
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
+// Translator: XOOPS Magyar Fordító Csapat
 
-define('_AD_NORIGHT', 'You don\'t have the permission to access this area');
-define('_AD_ACTION', 'Action');
-define('_AD_EDIT', 'Edit');
-define('_AD_DELETE', 'Delete');
-define('_AD_LASTTENUSERS', 'Last 10 registered users');
-define('_AD_NICKNAME', 'Username');
-define('_AD_EMAIL', 'Email');
+define('_AD_NORIGHT', 'Nincs jogosultságod ezen a területen hozzáférni');
+define('_AD_ACTION', 'Művelet');
+define('_AD_EDIT', 'Szerkesztés');
+define('_AD_DELETE', 'Törlés');
+define('_AD_LASTTENUSERS', 'Utolsó 10 regisztrált felhasználó');
+define('_AD_NICKNAME', 'Felhasználónév');
+define('_AD_EMAIL', 'E-mail');
 define('_AD_AVATAR', 'Avatar');
-define('_AD_REGISTERED', 'Registered'); //Registered Date
+define('_AD_REGISTERED', 'Regisztrált'); //Regisztráció Dátuma
 // define('_AD_PRESSGEN','This is your first time to enter the administration section. Press the button below to proceed.');
-define('_AD_LOGINADMIN', 'Logging you in..');
-define('_AD_WARNINGINSTALL', 'WARNING: Directory %s exists on your server. <br>Please remove this directory for security reasons.');
-define('_AD_WARNINGWRITEABLE', 'WARNING: File %s is writeable by the server. <br>Please change the permission of this file for security reasons.<br> in Unix (444), in Win32 (read-only)');
-define('_AD_WARNINGNOTWRITEABLE', 'WARNING: Folder %s is not writeable by the server. <br>Please change the permission of this folder.<br> in Unix (777), in Win32 (writable)');
-define('_AD_WARNINGXOOPSLIBINSIDE', 'WARNING: Folder %s is inside DocumentRoot! <br>For security considerations it is highly suggested to move it out of DocumentRoot.');
-define('_AD_WARNING_OLD_PHP', 'WARNING: Consider upgrading to a newer version of PHP. Version %s or newer is recommended and will be required in future XOOPS versions.');
-define('_AD_WARNING_NO_XML', 'The PHP XML Extension is required for this function.');
+define('_AD_LOGINADMIN', 'Bejelentkezés folyamatban...');
+define('_AD_WARNINGINSTALL', 'FIGYELEM: A(z) %s könyvtár létezik a szervereden. <br>Kérlek, távolítsd el ezt a könyvtárat biztonsági okokból.');
+define('_AD_WARNINGWRITEABLE', 'FIGYELEM: A(z) %s fájl írható a szerver számára. <br>Kérlek, változtasd meg a fájl jogosultságát biztonsági okokból.<br> Unix alatt (444), Win32 alatt (csak olvasható)');
+define('_AD_WARNINGNOTWRITEABLE', 'FIGYELEM: A(z) %s mappa nem írható a szerver számára. <br>Kérlek, változtasd meg a mappa jogosultságát.<br> Unix alatt (777), Win32 alatt (írható)');
+define('_AD_WARNINGXOOPSLIBINSIDE', 'FIGYELEM: A(z) %s mappa a DocumentRoot-on belül van! <br>Biztonsági okokból erősen ajánlott áthelyezni a DocumentRoot-on kívülre.');
+define('_AD_WARNING_OLD_PHP', 'FIGYELEM: Fontold meg, hogy frissítesz egy újabb PHP verzióra. A(z) %s vagy újabb verzió ajánlott, és a jövőbeni XOOPS verziókban kötelező lesz.');
+define('_AD_WARNING_NO_XML', 'A PHP XML kiterjesztés szükséges ehhez a funkcióhoz.');
