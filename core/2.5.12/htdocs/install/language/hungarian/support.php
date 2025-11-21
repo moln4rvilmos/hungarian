72f66502b73ff7c27b4ead0934cef2790933988c
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:17:26 +0100

    Update Hungarian support site information

--- core/2.5.12/htdocs/install/language/hungarian/support.php
+++ core/2.5.12/htdocs/install/language/hungarian/support.php
@@ -1,23 +1,30 @@
 <?php
 
 //
-// _LANGCODE: en
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
+// Translator: XOOPS Magyar Fordító Csapat
 
 $supports = [];
 
-// Support site for English
-$supports['english'] = [
-    'url' => 'https://xoops.org/',
-    'title' => 'English support',
+// Support site for Hungarian
+$supports['hungarian'] = [
+    'url' => 'https://www.xoops.hu/', // Ha van hivatalos magyar XOOPS oldal
+    'title' => 'Magyar nyelvű támogatás',
 ];
+
+// Support site for English (Az eredeti angol link megtartva, de a magyar leírással)
+$supports['english'] = [
+    'url' => 'https://xoops.org/',
+    'title' => 'Angol nyelvű támogatás',
+];
+
 
 // Add extra support sites, use corresponding language folder name as key, for instance:
 /*
 $supports["french"] = array(
-    "url"   => "https://www.frxoops.org/",
-    "title" => "Support francophone"
+    "url"   => "https://www.frxoops.org/",
+    "title" => "Support francophone"
 );
 */
 
