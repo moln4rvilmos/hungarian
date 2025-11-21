3702dca048471c9491de443e238db262cbbdf3a7
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:29:41 +0100

    Translate user search phrases to Hungarian

--- core/2.5.12/htdocs/language/hungarian/findusers.php
+++ core/2.5.12/htdocs/language/hungarian/findusers.php
@@ -1,65 +1,64 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_MA_USER_MORE', 'Search users');
-define('_MA_USER_REMOVE', 'Remove unselected users');
+// Translator: XOOPS Magyar Fordító Csapat
+define('_MA_USER_MORE', 'Felhasználók keresése');
+define('_MA_USER_REMOVE', 'Kiválasztatlan felhasználók eltávolítása');
 
-//%%%%%%    File Name findusers.php     %%%%%
-define('_MA_USER_ADD_SELECTED', 'Add selected users');
-define('_MA_USER_GROUP', 'Group');
-define('_MA_USER_LEVEL', 'Level');
-define('_MA_USER_LEVEL_ACTIVE', 'Active');
-define('_MA_USER_LEVEL_INACTIVE', 'Inactive');
-define('_MA_USER_LEVEL_DISABLED', 'Disabled');
-define('_MA_USER_RANK', 'Rank');
-define('_MA_USER_FINDUS', 'Find Users');
+//%%%%%%     File Name findusers.php     %%%%%
+define('_MA_USER_ADD_SELECTED', 'Kiválasztott felhasználók hozzáadása');
+define('_MA_USER_GROUP', 'Csoport');
+define('_MA_USER_LEVEL', 'Szint');
+define('_MA_USER_LEVEL_ACTIVE', 'Aktív');
+define('_MA_USER_LEVEL_INACTIVE', 'Inaktív');
+define('_MA_USER_LEVEL_DISABLED', 'Letiltva');
+define('_MA_USER_RANK', 'Rang');
+define('_MA_USER_FINDUS', 'Felhasználók keresése');
 define('_MA_USER_AVATAR', 'Avatar');
-define('_MA_USER_REALNAME', 'Real Name');
-define('_MA_USER_REGDATE', 'Joined Date');
-define('_MA_USER_EMAIL', 'Email');
-define('_MA_USER_PREVIOUS', 'Previous');
-define('_MA_USER_NEXT', 'Next');
-define('_MA_USER_USERSFOUND', '%s user(s) found');
-define('_MA_USER_ACTUS', 'Active Users: %s');
-define('_MA_USER_INACTUS', 'Inactive Users: %s');
-define('_MA_USER_NOFOUND', 'No Users Found');
-define('_MA_USER_UNAME', 'User Name');
-define('_MA_USER_ICQ', 'ICQ Number');
-define('_MA_USER_AIM', 'AIM Handle');
+define('_MA_USER_REALNAME', 'Valódi Név');
+define('_MA_USER_REGDATE', 'Csatlakozás Dátuma');
+define('_MA_USER_EMAIL', 'E-mail');
+define('_MA_USER_PREVIOUS', 'Előző');
+define('_MA_USER_NEXT', 'Következő');
+define('_MA_USER_USERSFOUND', '%s felhasználó(k) található(k)');
+define('_MA_USER_ACTUS', 'Aktív felhasználók: %s');
+define('_MA_USER_INACTUS', 'Inaktív felhasználók: %s');
+define('_MA_USER_NOFOUND', 'Nincs Található Felhasználó');
+define('_MA_USER_UNAME', 'Felhasználónév');
+define('_MA_USER_ICQ', 'ICQ Szám');
+define('_MA_USER_AIM', 'AIM Azonosító');
 define('_MA_USER_YIM', 'YIM Link');
 define('_MA_USER_MSNM', 'MSNM Link');
-define('_MA_USER_LOCATION', 'Location contains');
-define('_MA_USER_OCCUPATION', 'Occupation contains');
-define('_MA_USER_INTEREST', 'Interest contains');
-define('_MA_USER_URLC', 'URL contains');
-define('_MA_USER_SORT', 'Sort by');
-define('_MA_USER_ORDER', 'Order');
-define('_MA_USER_LASTLOGIN', 'Last login');
-define('_MA_USER_POSTS', 'Number of posts');
-define('_MA_USER_ASC', 'Ascending order');
-define('_MA_USER_DESC', 'Descending order');
-define('_MA_USER_LIMIT', 'Number of users per page');
-define('_MA_USER_RESULTS', 'Search results');
-define('_MA_USER_SHOWMAILOK', 'Type of users to show');
-define('_MA_USER_MAILOK', 'Only users that accept email');
-define('_MA_USER_MAILNG', 'Only users that don\'t accept email');
-define('_MA_USER_BOTH', 'All');
-define('_MA_USER_RANGE_LAST_LOGIN', 'Logged in past <span style=\'color:#ff0000;\'>X</span>days');
-define('_MA_USER_RANGE_USER_REGDATE', 'Registered in past <span style=\'color:#ff0000;\'>X</span>days');
-define('_MA_USER_RANGE_POSTS', 'Posts');
-define('_MA_USER_HASAVATAR', 'Has avatar');
-define('_MA_USER_MODE_SIMPLE', 'Simple mode');
-define('_MA_USER_MODE_ADVANCED', 'Advanced mode');
-define('_MA_USER_MODE_QUERY', 'Query mode');
-define('_MA_USER_QUERY', 'Query');
-define('_MA_USER_SEARCHAGAIN', 'Search again');
-define('_MA_USER_NOUSERSELECTED', 'No user selected');
-define('_MA_USER_USERADDED', 'Users have been added');
-define('_MA_USER_SENDMAIL', 'Send Email');
+define('_MA_USER_LOCATION', 'Hely tartalmazza');
+define('_MA_USER_OCCUPATION', 'Foglalkozás tartalmazza');
+define('_MA_USER_INTEREST', 'Érdeklődési kör tartalmazza');
+define('_MA_USER_URLC', 'URL tartalmazza');
+define('_MA_USER_SORT', 'Rendezés');
+define('_MA_USER_ORDER', 'Sorrend');
+define('_MA_USER_LASTLOGIN', 'Utolsó bejelentkezés');
+define('_MA_USER_POSTS', 'Hozzászólások száma');
+define('_MA_USER_ASC', 'Növekvő sorrend');
+define('_MA_USER_DESC', 'Csökkenő sorrend');
+define('_MA_USER_LIMIT', 'Felhasználók száma oldalanként');
+define('_MA_USER_RESULTS', 'Keresési eredmények');
+define('_MA_USER_SHOWMAILOK', 'Megjelenítendő felhasználók típusa');
+define('_MA_USER_MAILOK', 'Csak azok a felhasználók, akik elfogadják az e-mailt');
+define('_MA_USER_MAILNG', 'Csak azok a felhasználók, akik nem fogadják el az e-mailt');
+define('_MA_USER_BOTH', 'Összes');
+define('_MA_USER_RANGE_LAST_LOGIN', 'Bejelentkezve az elmúlt <span style=\'color:#ff0000;\'>X</span> napban');
+define('_MA_USER_RANGE_USER_REGDATE', 'Regisztrált az elmúlt <span style=\'color:#ff0000;\'>X</span> napban');
+define('_MA_USER_RANGE_POSTS', 'Hozzászólások');
+define('_MA_USER_HASAVATAR', 'Van avatarja');
+define('_MA_USER_MODE_SIMPLE', 'Egyszerű mód');
+define('_MA_USER_MODE_ADVANCED', 'Haladó mód');
+define('_MA_USER_MODE_QUERY', 'Lekérdezési mód');
+define('_MA_USER_QUERY', 'Lekérdezés');
+define('_MA_USER_SEARCHAGAIN', 'Újra keresés');
+define('_MA_USER_NOUSERSELECTED', 'Nincs kiválasztott felhasználó');
+define('_MA_USER_USERADDED', 'A felhasználók hozzáadva');
+define('_MA_USER_SENDMAIL', 'E-mail küldése');
 
 //2.5.4
 define('_MA_USER_FACEBOOK', 'Facebook Link'); //TO DO
 define('_MA_USER_SKYPE', 'Skype Link');//TO DO
-
