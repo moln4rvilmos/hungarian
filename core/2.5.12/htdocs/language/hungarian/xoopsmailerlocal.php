9d1f50fd0f170ac77d5bce163d8704112ee5dbb0
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:43:09 +0100

    Update language settings to Hungarian in xoopsmailerlocal

--- core/2.5.12/htdocs/language/hungarian/xoopsmailerlocal.php
+++ core/2.5.12/htdocs/language/hungarian/xoopsmailerlocal.php
@@ -1,22 +1,22 @@
 <?php
 /*
- You may not change or alter any portion of this comment or credits
- of supporting developers from this source code or any supporting source code
- which is considered copyrighted (c) material of the original comment or credit authors.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 /**
- *  Xoops Language
+ * Xoops Language
  *
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
- * @license             GNU GPL 2 (https://www.gnu.org/licenses/gpl-2.0.html)
- * @package             kernel
- * @subpackage          Xoops Mailer Local Language
- * @since               2.3.0
- * @author              Taiwen Jiang <phppp@users.sourceforge.net>
+ * @license         GNU GPL 2 (https://www.gnu.org/licenses/gpl-2.0.html)
+ * @package         kernel
+ * @subpackage      Xoops Mailer Local Language
+ * @since           2.3.0
+ * @author          Taiwen Jiang <phppp@users.sourceforge.net>
  */
 defined('XOOPS_ROOT_PATH') || exit('Restricted access');
 /**
@@ -36,7 +36,8 @@ class XoopsMailerLocal extends XoopsMailer
         // It is supposed no need to change the charset
         $this->charSet = strtolower(_CHARSET);
         // You MUST specify the language code value so that the file exists: XOOPS_ROOT_PAT/class/mail/phpmailer/language/lang-["your-language-code"].php
-        $this->multimailer->setLanguage('en');
+        // ÁTÁLLÍTVA 'en'-ről 'hu'-ra
+        $this->multimailer->setLanguage('hu');
     }
 
     /**
@@ -48,8 +49,8 @@ class XoopsMailerLocal extends XoopsMailer
      */
     public function encodeFromName($text)
     {
-        // Activate the following line if needed
-        // $text = "=?{$this->charSet}?B?".base64_encode($text)."?=";
+        // Aktiválva többbájtos (pl. magyar) karakterek kódolásához
+        $text = "=?{$this->charSet}?B?".base64_encode($text)."?=";
         return $text;
     }
 
@@ -63,8 +64,8 @@ class XoopsMailerLocal extends XoopsMailer
      */
     public function encodeSubject($text)
     {
-        // Activate the following line if needed
-        // $text = "=?{$this->charSet}?B?".base64_encode($text)."?=";
+        // Aktiválva többbájtos (pl. magyar) karakterek kódolásához
+        $text = "=?{$this->charSet}?B?".base64_encode($text)."?=";
         return $text;
     }
 }
