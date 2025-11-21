3a518e3edbb2bc870ee284d64f124e42d90cfe55
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:18:10 +0100

    Translate finish.php content to Hungarian

--- core/2.5.12/htdocs/install/language/hungarian/finish.php
+++ core/2.5.12/htdocs/install/language/hungarian/finish.php
@@ -1,24 +1,24 @@
 <?php
 //
-// _LANGCODE: en
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
+// Translator: XOOPS Magyar Fordító Csapat
 
-$content .= "<h3>Your site</h3>
-<p>You can now access the <a href='../index.php'>home page of your site</a>.</p>
-<h3>Support</h3>
-<p>Visit <a href='https://xoops.org/' rel='external'>The XOOPS Project</a></p>
-<p><strong>ATTENTION :</strong> Your site currently contains the minimum functionality. 
-Please visit <a href='https://xoops.org/' rel='external' title='XOOPS Web Application System'>xoops.org</a> 
-to learn more about extending XOOPS to present text pages, photo galleries, forums, and more, 
-with <em>modules</em> as well as customizing the look of your XOOPS with <em>themes</em>.</p>
+$content .= "<h3>Az Ön webhelye</h3>
+<p>Most már elérheti <a href='../index.php'>webhelye főoldalát</a>.</p>
+<h3>Támogatás</h3>
+<p>Látogassa meg <a href='https://xoops.org/' rel='external'>A XOOPS Projekt</a> oldalát</p>
+<p><strong>FIGYELEM:</strong> Webhelye jelenleg a minimális funkcionalitást tartalmazza.
+Kérjük, látogasson el a <a href='https://xoops.org/' rel='external' title='XOOPS Web Application System'>xoops.org</a> oldalra,
+hogy többet megtudjon arról, hogyan bővítheti a XOOPS-ot szöveges oldalak, fotógalériák, fórumok és egyebek megjelenítésére
+különböző <em>modulok</em> segítségével, valamint hogyan szabhatja testre a XOOPS megjelenését <em>témákkal</em>.</p>
 ";
 
-$content .= "<h3>Security configuration</h3>
-<p>The installer will try to configure your site for security considerations. Please double-check to make sure:
+$content .= "<h3>Biztonsági konfiguráció</h3>
+<p>A telepítő megpróbálja konfigurálni webhelyét a biztonsági szempontoknak megfelelően. Kérjük, ellenőrizze, hogy:
 <div class='confirmMsg'>
-The <em>mainfile.php</em> is readonly.<br>
-Remove the folder <em>{$installer_modified}</em> (or <em>install</em> if it was not renamed automatically by the installer)  from your server.
+A <em>mainfile.php</em> írásvédett.<br>
+Távolítsa el a <em>{$installer_modified}</em> mappát (vagy az <em>install</em> mappát, ha a telepítő nem nevezte át automatikusan) a szerverről.
 </div>
 </p>
 ";
