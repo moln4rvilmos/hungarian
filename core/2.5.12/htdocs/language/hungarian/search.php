289676c3fba0f6929000324a4ff0bf7174122dd1
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:37:11 +0100

    Translate search.php to Hungarian

--- core/2.5.12/htdocs/language/hungarian/search.php
+++ core/2.5.12/htdocs/language/hungarian/search.php
@@ -1,25 +1,25 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-//%%%%%%    File Name search.php    %%%%%
-define('_SR_SEARCH', 'Search');
-define('_SR_PLZENTER', 'Please enter all required data!');
-define('_SR_SEARCHRESULTS', 'Search Results');
-define('_SR_NOMATCH', 'No Match Found for your Query');
-define('_SR_FOUND', 'Found <strong>%s</strong> match(es)');
-define('_SR_SHOWING', '(Showing %d - %d)');
-define('_SR_ANY', 'Any (OR)');
-define('_SR_ALL', 'All (AND)');
-define('_SR_EXACT', 'Exact Match');
-define('_SR_SHOWALLR', 'Show all results');
-define('_SR_NEXT', 'Next >>');
-define('_SR_PREVIOUS', '<< Previous');
-define('_SR_KEYWORDS', 'Keyword(s)');
-define('_SR_TYPE', 'Type');
-define('_SR_SEARCHIN', 'Search in');
-define('_SR_KEYTOOSHORT', 'Keywords must be at least <strong>%s</strong> characters long');
-define('_SR_KEYIGNORE', 'Keywords shorter than <strong>%s</strong> characters will be ignored');
-define('_SR_SEARCHRULE', 'Search Rule');
-define('_SR_IGNOREDWORDS', 'The following words are shorter than allowed minimum length (%u chars) and were not included in your search:');
+// Translator: XOOPS Magyar Fordító Csapat
+//%%%%%%     File Name search.php    %%%%%
+define('_SR_SEARCH', 'Keresés');
+define('_SR_PLZENTER', 'Kérlek, add meg az összes szükséges adatot!');
+define('_SR_SEARCHRESULTS', 'Keresési Eredmények');
+define('_SR_NOMATCH', 'Nincs Találat a Lekérdezésedre');
+define('_SR_FOUND', 'Találat(ok) száma: <strong>%s</strong>');
+define('_SR_SHOWING', '(Megjelenítve: %d - %d)');
+define('_SR_ANY', 'Bármelyik (VAGY)');
+define('_SR_ALL', 'Mind (ÉS)');
+define('_SR_EXACT', 'Pontos Egyezés');
+define('_SR_SHOWALLR', 'Minden eredmény megjelenítése');
+define('_SR_NEXT', 'Következő >>');
+define('_SR_PREVIOUS', '<< Előző');
+define('_SR_KEYWORDS', 'Kulcsszó(k)');
+define('_SR_TYPE', 'Típus');
+define('_SR_SEARCHIN', 'Keresés itt');
+define('_SR_KEYTOOSHORT', 'A kulcsszavaknak legalább <strong>%s</strong> karakter hosszúnak kell lenniük');
+define('_SR_KEYIGNORE', 'A(z) <strong>%s</strong> karakternél rövidebb kulcsszavak figyelmen kívül lesznek hagyva');
+define('_SR_SEARCHRULE', 'Keresési Szabály');
+define('_SR_IGNOREDWORDS', 'A következő szavak rövidebbek az engedélyezett minimális hossznál (%u karakter), és nem lettek bevonva a keresésbe:');
