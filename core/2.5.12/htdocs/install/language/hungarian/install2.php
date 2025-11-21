e8cf2e805119c8bf7d6436a2dd3b10bb4c2f0133
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:14:16 +0100

    Translate install2.php to Hungarian

--- core/2.5.12/htdocs/install/language/hungarian/install2.php
+++ core/2.5.12/htdocs/install/language/hungarian/install2.php
@@ -1,55 +1,57 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
+// Translator: XOOPS Magyar Fordító Csapat
 // License
-define('LICENSE_NOT_WRITEABLE', 'License is %s . <br><span style="color:#ff0000;">Make ../include/license.php Writable</span>');
-define('LICENSE_IS_WRITEABLE', 'License is %s');
-define('_INSTALL_WEBMASTER', 'Webmasters');
-define('_INSTALL_WEBMASTERD', 'Webmasters of this site');
-define('_INSTALL_REGUSERS', 'Registered Users');
-define('_INSTALL_REGUSERSD', 'Registered Users Group');
-define('_INSTALL_ANONUSERS', 'Anonymous Users');
-define('_INSTALL_ANONUSERSD', 'Anonymous Users Group');
+define('LICENSE_NOT_WRITEABLE', 'A licenc státusza: %s . <br><span style="color:#ff0000;">Tegye írhatóvá a ../include/license.php fájlt!</span>');
+define('LICENSE_IS_WRITEABLE', 'A licenc státusza: %s');
+define('_INSTALL_WEBMASTER', 'Webmesterek');
+define('_INSTALL_WEBMASTERD', 'A webhely webmesterei');
+define('_INSTALL_REGUSERS', 'Regisztrált felhasználók');
+define('_INSTALL_REGUSERSD', 'Regisztrált felhasználók csoportja');
+define('_INSTALL_ANONUSERS', 'Névtelen felhasználók');
+define('_INSTALL_ANONUSERSD', 'Névtelen felhasználók csoportja');
 /**
  * New Group types
  */
-define('_INSTALL_BANNEDUSERS', 'Banned Users');
-define('_INSTALL_BANNEDUSERSD', 'Banned user group');
-define('_INSTALL_MODERATORUSERS', 'Moderators');
-define('_INSTALL_MODERATORUSERSD', 'These are Moderators for your website');
-define('_INSTALL_SUBMITTERUSERS', 'Submitters');
-define('_INSTALL_SUBMITTERUSERSD', 'This group can submit articles to your website');
-define('_INSTALL_DEVELOPEUSERS', 'Developer');
-define('_INSTALL_DEVELOPEUSERSD', 'This user has developer privileges and can see developer debugging messages.');
-define('_INSTALL_L165', 'The site is currently closed for maintenance. Please come back later.');
-define('_INSTALL_ANON', 'Anonymous');
-define('_INSTALL_DISCLMR', 'While the administrators and moderators of this site will attempt to remove
-or edit any generally objectionable material as quickly as possible, it is
-impossible to review every message. Therefore you acknowledge that all posts
-made to this site express the views and opinions of the author and not the
-administrators, moderators or webmaster (except for posts by these people)
-and hence will not be held liable.
+define('_INSTALL_BANNEDUSERS', 'Kitiltott felhasználók');
+define('_INSTALL_BANNEDUSERSD', 'Kitiltott felhasználói csoport');
+define('_INSTALL_MODERATORUSERS', 'Moderátorok');
+define('_INSTALL_MODERATORUSERSD', 'Ezek a webhely moderátorai');
+define('_INSTALL_SUBMITTERUSERS', 'Tartalomküldők');
+define('_INSTALL_SUBMITTERUSERSD', 'Ez a csoport küldhet be cikkeket a webhelyre');
+define('_INSTALL_DEVELOPEUSERS', 'Fejlesztő');
+define('_INSTALL_DEVELOPEUSERSD', 'Ez a felhasználó fejlesztői jogosultságokkal rendelkezik, és láthatja a fejlesztői hibakeresési üzeneteket.');
+define('_INSTALL_L165', 'A webhely jelenleg karbantartás miatt zárva van. Kérjük, térjen vissza később.');
+define('_INSTALL_ANON', 'Névtelen');
+define('_INSTALL_DISCLMR', 'Bár a webhely adminisztrátorai és moderátorai megpróbálnak minden
+általánosan kifogásolható anyagot a lehető leggyorsabban eltávolítani vagy szerkeszteni,
+lehetetlen minden üzenetet áttekinteni. Ezért Ön tudomásul veszi, hogy a
+webhelyre feltöltött összes bejegyzés a szerző nézeteit és véleményét fejezi ki,
+és nem az adminisztrátorok, moderátorok vagy a webmester véleményét (kivéve, ha
+azok e személyek bejegyzései), és így nem vonható felelősségre.
 
-You agree not to post any abusive, obscene, vulgar, slanderous, hateful,
-threatening, sexually-orientated or any other material that may violate any
-applicable laws. Doing so may lead to you being immediately and permanently
-banned (and your service provider being informed). The IP address of all
-posts is recorded to aid in enforcing these conditions. Creating multiple
-accounts for a single user is not allowed. You agree that the webmaster,
-administrator and moderators of this site have the right to remove, edit,
-move or close any topic at any time should they see fit. As a user you agree
-to any information you have entered above being stored in a database. While
-this information will not be disclosed to any third party without your
-consent the webmaster, administrator and moderators cannot be held
-responsible for any hacking attempt that may lead to the data being
-compromised.
+Ön beleegyezik, hogy nem tesz közzé semmilyen sértő, obszcén, vulgáris,
+rágalmazó, gyűlöletkeltő, fenyegető, szexuális jellegű vagy bármely más
+anyagot, amely sérthet bármely hatályos jogszabályt. Ennek elmulasztása
+azonnali és végleges kitiltásához (és a szolgáltató értesítéséhez) vezethet.
+Minden bejegyzés IP-címe rögzítésre kerül e feltételek betartatásának
+elősegítésére. Több fiók létrehozása egyetlen felhasználó számára nem
+megengedett. Ön beleegyezik abba, hogy a webhely webmestere, adminisztrátora és
+moderátorai jogosultak bármely témát eltávolítani, szerkeszteni, áthelyezni
+vagy lezárni, ha azt szükségesnek látják. Felhasználóként Ön beleegyezik, hogy
+az Ön által fent megadott információk egy adatbázisban tárolásra kerüljenek.
+Bár ezeket az információkat az Ön hozzájárulása nélkül nem adjuk ki harmadik
+félnek, a webmester, az adminisztrátor és a moderátorok nem vonhatók
+felelősségre semmilyen hackelési kísérletért, amely az adatok
+kompromittálásához vezethet.
 
-This site system uses cookies to store information on your local computer.
-These cookies do not contain any of the information you have entered above,
-they serve only to improve your viewing pleasure. The email address is used
-only for confirming your registration details and password (and for sending
-new passwords should you forget your current one).
+Ez a webhelyrendszer sütiket (cookie-kat) használ az információk helyi
+számítógépén történő tárolására. Ezek a sütik nem tartalmazzák az Ön által
+fent megadott információkat, csupán a böngészési élmény javítását szolgálják.
+Az e-mail címet csak a regisztrációs adatok és a jelszó megerősítésére
+(és új jelszavak küldésére, ha elfelejtené a jelenlegit) használjuk.
 
-By clicking Register below you agree to be bound by these conditions.');
+Az alábbi "Regisztráció" gombra kattintva Ön elfogadja, hogy kötelező érvényű
+ezekre a feltételekre.
