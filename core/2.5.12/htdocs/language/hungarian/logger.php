5d201b122e42624f817a35e0785c90c180e1a451
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:33:20 +0100

    Translate logger messages to Hungarian

--- core/2.5.12/htdocs/language/hungarian/logger.php
+++ core/2.5.12/htdocs/language/hungarian/logger.php
@@ -1,55 +1,55 @@
 <?php
 /*
- You may not change or alter any portion of this comment or credits
- of supporting developers from this source code or any supporting source code
- which is considered copyrighted (c) material of the original comment or credit authors.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*/
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
 /**
- *  Xoops Language
+ * Xoops Language
  *
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
- * @license             GNU GPL 2 (https://www.gnu.org/licenses/gpl-2.0.html)
- * @package             kernel
- * @subpackage          Xoops Logger Language
- * @since               2.4.0
- * @author              trabis <lusopoemas@gmail.com>
+ * @license         GNU GPL 2 (https://www.gnu.org/licenses/gpl-2.0.html)
+ * @package         kernel
+ * @subpackage      Xoops Logger Language
+ * @since           2.4.0
+ * @author          trabis <lusopoemas@gmail.com>
  */
 defined('XOOPS_ROOT_PATH') || exit('Restricted access');
 
-// _LANGCODE: en
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_LOGGER_DEBUG', 'Debug');
-define('_LOGGER_INCLUDED_FILES', 'Included files');
-define('_LOGGER_FILES', '%s files');
-define('_LOGGER_MEM_USAGE', 'Memory usage');
-define('_LOGGER_MEM_ESTIMATED', '%s [Estimated]');
-define('_LOGGER_NONE', 'None');
-define('_LOGGER_ALL', 'All');
-define('_LOGGER_ERRORS', 'Errors');
-define('_LOGGER_DEPRECATED', 'Deprecated');
-define('_LOGGER_QUERIES', 'Queries');
-define('_LOGGER_BLOCKS', 'Blocks');
+// Translator: XOOPS Magyar Fordító Csapat
+define('_LOGGER_DEBUG', 'Hibakeresés (Debug)');
+define('_LOGGER_INCLUDED_FILES', 'Behúzott fájlok');
+define('_LOGGER_FILES', '%s fájl');
+define('_LOGGER_MEM_USAGE', 'Memóriahasználat');
+define('_LOGGER_MEM_ESTIMATED', '%s [Becsült]');
+define('_LOGGER_NONE', 'Nincs');
+define('_LOGGER_ALL', 'Mind');
+define('_LOGGER_ERRORS', 'Hibák');
+define('_LOGGER_DEPRECATED', 'Elavult (Deprecated)');
+define('_LOGGER_QUERIES', 'Lekérdezések');
+define('_LOGGER_BLOCKS', 'Blokkok');
 define('_LOGGER_EXTRA', 'Extra');
-define('_LOGGER_TIMERS', 'Timers');
+define('_LOGGER_TIMERS', 'Időzítők');
 
-define('_LOGGER_TIMETOLOAD', '%s took %s seconds to load.');
+define('_LOGGER_TIMETOLOAD', 'A(z) %s betöltése %s másodpercet vett igénybe.');
 
-define('_LOGGER_TOTAL', 'Total');
+define('_LOGGER_TOTAL', 'Összesen');
 
-define('_LOGGER_NOT_CACHED', 'Not cached');
-define('_LOGGER_CACHED', 'Cached (regenerates every %s seconds)');
+define('_LOGGER_NOT_CACHED', 'Nincs gyorsítótárazva');
+define('_LOGGER_CACHED', 'Gyorsítótárazva (újragenerálás %s másodpercenként)');
 
-define('_LOGGER_UNKNOWN', 'Unknown');
-define('_LOGGER_E_USER_NOTICE', 'Notice');
-define('_LOGGER_E_USER_WARNING', 'Warning');
-define('_LOGGER_E_USER_ERROR', 'Error');
-define('_LOGGER_E_NOTICE', 'Notice');
-define('_LOGGER_E_WARNING', 'Warning');
-define('_LOGGER_E_STRICT', 'Strict');
-define('_LOGGER_FILELINE', '%s in file %s line %s');
+define('_LOGGER_UNKNOWN', 'Ismeretlen');
+define('_LOGGER_E_USER_NOTICE', 'Értesítés (Notice)');
+define('_LOGGER_E_USER_WARNING', 'Figyelmeztetés (Warning)');
+define('_LOGGER_E_USER_ERROR', 'Hiba (Error)');
+define('_LOGGER_E_NOTICE', 'Értesítés (Notice)');
+define('_LOGGER_E_WARNING', 'Figyelmeztetés (Warning)');
+define('_LOGGER_E_STRICT', 'Szigorú (Strict)');
+define('_LOGGER_FILELINE', '%s a(z) %s fájlban, %s. sorban');
