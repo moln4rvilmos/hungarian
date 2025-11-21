ede53620d837c44c5f75c4de043e6fba43103cdd
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:36:27 +0100

    Translate private message strings to Hungarian

--- core/2.5.12/htdocs/language/hungarian/pmsg.php
+++ core/2.5.12/htdocs/language/hungarian/pmsg.php
@@ -1,46 +1,46 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-//%%%%%%    File Name readpmsg.php  %%%%%
-define('_PM_DELETED', 'Your message(s) has been deleted');
-define('_PM_PRIVATEMESSAGE', 'Private Messages');
-define('_PM_INBOX', 'Inbox');
-define('_PM_FROM', 'From');
-define('_PM_YOUDONTHAVE', 'You don\'t have any private messages');
-define('_PM_FROMC', 'From: ');
-define('_PM_SENTC', 'Sent: '); // The date of message sent
-define('_PM_PROFILE', 'Profile');
+// Translator: XOOPS Magyar Fordító Csapat
+//%%%%%%     File Name readpmsg.php  %%%%%
+define('_PM_DELETED', 'Az üzeneted(id) törölve lett');
+define('_PM_PRIVATEMESSAGE', 'Privát Üzenetek');
+define('_PM_INBOX', 'Bejövő (Inbox)');
+define('_PM_FROM', 'Tőle');
+define('_PM_YOUDONTHAVE', 'Nincs privát üzeneted');
+define('_PM_FROMC', 'Feladó: ');
+define('_PM_SENTC', 'Elküldve: '); // Az üzenet küldésének dátuma
+define('_PM_PROFILE', 'Profil');
 // %s is a username
-define('_PM_PREVIOUS', 'Previous Message');
-define('_PM_NEXT', 'Next Message');
-//%%%%%%    File Name pmlite.php    %%%%%
-define('_PM_SORRY', 'Sorry! You are not a registered user.');
-define('_PM_REGISTERNOW', 'Register Now!');
-define('_PM_GOBACK', 'Go Back');
-define('_PM_USERNOEXIST', 'The selected user doesn\'t exist in the database.');
-define('_PM_PLZTRYAGAIN', 'Please check the name and try again.');
-define('_PM_MESSAGEPOSTED', 'Your message has been posted');
-define('_PM_CLICKHERE', 'You can click here to view your private messages');
-define('_PM_ORCLOSEWINDOW', 'Or click here to close this window.');
-define('_PM_USERWROTE', '%s wrote:');
-define('_PM_TO', 'To: ');
-define('_PM_SUBJECTC', 'Subject: ');
-define('_PM_MESSAGEC', 'Message: ');
-define('_PM_CLEAR', 'Clear');
-define('_PM_CANCELSEND', 'Cancel Send');
-define('_PM_SUBMIT', 'Submit');
-//%%%%%%    File Name viewpmsg.php  %%%%%
-define('_PM_SUBJECT', 'Subject');
-define('_PM_DATE', 'Date');
-define('_PM_NOTREAD', 'Not Read');
-define('_PM_SEND', 'Send');
-define('_PM_DELETE', 'Delete');
-define('_PM_REPLY', 'Reply');
-define('_PM_PLZREG', 'Please register first to send private messages!');
+define('_PM_PREVIOUS', 'Előző Üzenet');
+define('_PM_NEXT', 'Következő Üzenet');
+//%%%%%%     File Name pmlite.php    %%%%%
+define('_PM_SORRY', 'Sajnálom! Nem vagy regisztrált felhasználó.');
+define('_PM_REGISTERNOW', 'Regisztrálj Most!');
+define('_PM_GOBACK', 'Vissza');
+define('_PM_USERNOEXIST', 'A kiválasztott felhasználó nem létezik az adatbázisban.');
+define('_PM_PLZTRYAGAIN', 'Kérlek, ellenőrizd a nevet, és próbáld újra.');
+define('_PM_MESSAGEPOSTED', 'Az üzeneted elküldve');
+define('_PM_CLICKHERE', 'Kattinthatsz ide a privát üzeneteid megtekintéséhez');
+define('_PM_ORCLOSEWINDOW', 'Vagy kattints ide az ablak bezárásához.');
+define('_PM_USERWROTE', '%s írta:');
+define('_PM_TO', 'Címzett: ');
+define('_PM_SUBJECTC', 'Tárgy: ');
+define('_PM_MESSAGEC', 'Üzenet: ');
+define('_PM_CLEAR', 'Törlés');
+define('_PM_CANCELSEND', 'Küldés Mégse');
+define('_PM_SUBMIT', 'Küldés');
+//%%%%%%     File Name viewpmsg.php  %%%%%
+define('_PM_SUBJECT', 'Tárgy');
+define('_PM_DATE', 'Dátum');
+define('_PM_NOTREAD', 'Nincs Olvasva');
+define('_PM_SEND', 'Küldés');
+define('_PM_DELETE', 'Törlés');
+define('_PM_REPLY', 'Válasz');
+define('_PM_PLZREG', 'Kérlek, először regisztrálj, hogy privát üzeneteket küldhess!');
 define('_PM_ONLINE', 'Online');
 //XOOPS 2.5.2
-define('_PM_SURE_TO_DELETE', 'Are you sure you want to delete these message(s)?');
+define('_PM_SURE_TO_DELETE', 'Biztosan törölni szeretnéd ezeket az üzeneteket?');
 //XOOPS 2.5.5
-define('_PM_READ', 'Already Read');
+define('_PM_READ', 'Már Olvasva');
