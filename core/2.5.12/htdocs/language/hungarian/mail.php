6ab76e1b0b89780d9e5bd1033a40a035ef83e456
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:34:04 +0100

    Translate mail.php messages to Hungarian

--- core/2.5.12/htdocs/language/hungarian/mail.php
+++ core/2.5.12/htdocs/language/hungarian/mail.php
@@ -1,13 +1,13 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_MAIL_MSGBODY', 'Message body is not set.');
-define('_MAIL_FAILOPTPL', 'Failed opening template file.');
-define('_MAIL_FNAMENG', 'From Name is not set.');
-define('_MAIL_FEMAILNG', 'From Email is not set.');
-define('_MAIL_SENDMAILNG', 'Could not send mail to %s.');
-define('_MAIL_MAILGOOD', 'Mail sent to %s.');
-define('_MAIL_SENDPMNG', 'Could not send private message to %s.');
-define('_MAIL_PMGOOD', 'Private message sent to %s.');
+// Translator: XOOPS Magyar Fordító Csapat
+define('_MAIL_MSGBODY', 'Az üzenet törzse nincs beállítva.');
+define('_MAIL_FAILOPTPL', 'Nem sikerült megnyitni a sablonfájlt.');
+define('_MAIL_FNAMENG', 'A Küldő neve nincs beállítva.');
+define('_MAIL_FEMAILNG', 'A Küldő e-mail címe nincs beállítva.');
+define('_MAIL_SENDMAILNG', 'Nem sikerült elküldeni az e-mailt neki: %s.');
+define('_MAIL_MAILGOOD', 'Az e-mail elküldve neki: %s.');
+define('_MAIL_SENDPMNG', 'Nem sikerült elküldeni a privát üzenetet neki: %s.');
+define('_MAIL_PMGOOD', 'A privát üzenet elküldve neki: %s.');
