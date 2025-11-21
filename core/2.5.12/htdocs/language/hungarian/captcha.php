1bbdcea530800f0b5728bc66b811dd7933607c99
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-21 23:26:38 +0100

    Translate CAPTCHA messages to Hungarian

--- core/2.5.12/htdocs/language/hungarian/captcha.php
+++ core/2.5.12/htdocs/language/hungarian/captcha.php
@@ -1,21 +1,21 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_CAPTCHA_CAPTION', 'Confirmation Code');
-define('_CAPTCHA_INVALID_CODE', 'Invalid confirmation code!');
-define('_CAPTCHA_TOOMANYATTEMPTS', 'Too many attempts!');
-define('_CAPTCHA_MAXATTEMPTS', 'Maximum attempts you can try: %d');
+// Translator: XOOPS Magyar Fordító Csapat
+define('_CAPTCHA_CAPTION', 'Megerősítő Kód');
+define('_CAPTCHA_INVALID_CODE', 'Érvénytelen megerősítő kód!');
+define('_CAPTCHA_TOOMANYATTEMPTS', 'Túl sok próbálkozás!');
+define('_CAPTCHA_MAXATTEMPTS', 'Maximális próbálkozások száma: %d');
 // For image mode
-define('_CAPTCHA_RULE_IMAGE', 'Input letters in the image');
-define('_CAPTCHA_RULE_CASESENSITIVE', 'The code is case-sensitive');
-define('_CAPTCHA_RULE_CASEINSENSITIVE', 'The code is case-insensitive');
-define('_CAPTCHA_REFRESH', 'Click to refresh the image if it is not clear enough.');
+define('_CAPTCHA_RULE_IMAGE', 'Írd be a képen látható betűket');
+define('_CAPTCHA_RULE_CASESENSITIVE', 'A kód kis- és nagybetű érzékeny');
+define('_CAPTCHA_RULE_CASEINSENSITIVE', 'A kód nem kis- és nagybetű érzékeny');
+define('_CAPTCHA_REFRESH', 'Kattints ide a kép frissítéséhez, ha nem elég tiszta.');
 // For text mode
-define('_CAPTCHA_RULE_TEXT', 'Input the result from the expression');
+define('_CAPTCHA_RULE_TEXT', 'Írd be a kifejezés eredményét');
 
 /**
  * Error defines
  */
-define('_CAPTCHA_LOADFILEERROR', 'Error: Could not load file %u in file %s at line %s. ');
+define('_CAPTCHA_LOADFILEERROR', 'Hiba: Nem sikerült betölteni a(z) %u fájlt, itt: %s, a(z) %s sorban.');
