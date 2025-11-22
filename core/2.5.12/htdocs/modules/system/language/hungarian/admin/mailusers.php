028da065b48bd2e9f1bcd8ab0c7fe00e0dde486f
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:56:36 +0100

    Update mailusers.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/admin/mailusers.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/admin/mailusers.php
@@ -2,45 +2,46 @@
 /**
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
  * @license         GNU GPL 2.0 or later (https://www.gnu.org/licenses/gpl-2.0.html)
- * _LANGCODE    en
- * _CHARSET     UTF-8
+ * _LANGCODE        hu
+ * _CHARSET         UTF-8
+ * Translator: Molnár Vilmos (XOOPS Translation Team)
  */
-define('_AM_SYSTEM_MAILUSERS_AMIFCHECKD', 'If this is checked, all the above plus private messaging will be ignored');
-define('_AM_SYSTEM_MAILUSERS_EMAIL', 'Email');
-define('_AM_SYSTEM_MAILUSERS_GROUPIS', ' - Group:');
-define('_AM_SYSTEM_MAILUSERS_DAY', ' - Last Login days ago:');
-define('_AM_SYSTEM_MAILUSERS_IDLEMORE', 'more than X');
-define('_AM_SYSTEM_MAILUSERS_IDLELESS', 'less than X');
-define('_AM_SYSTEM_MAILUSERS_DATE', ' - Last Login date:');
-define('_AM_SYSTEM_MAILUSERS_LASTLOGMAX', 'before');
-define('_AM_SYSTEM_MAILUSERS_LASTLOGMIN', 'after');
-define('_AM_SYSTEM_MAILUSERS_REGDATE', ' - Registered date:');
-define('_AM_SYSTEM_MAILUSERS_REGDMIN', 'after');
-define('_AM_SYSTEM_MAILUSERS_REGDMAX', 'before');
-define('_AM_SYSTEM_MAILUSERS_INACTIVE', 'Send message to inactive users only');
-define('_AM_SYSTEM_MAILUSERS_MAILOK', 'Send message only to users that accept notification messages');
-define('_AM_SYSTEM_MAILUSERS_OPTIONAL', 'Optional value');
-define('_AM_SYSTEM_MAILUSERS_LIST', 'Send Message to Users');
-define('_AM_SYSTEM_MAILUSERS_MAILBODY', 'Body');
-define('_AM_SYSTEM_MAILUSERS_MAILFNAME', 'From Name (email only)');
-define('_AM_SYSTEM_MAILUSERS_MAILFMAIL', 'From Email (email only)');
-define('_AM_SYSTEM_MAILUSERS_MAILTAGS', 'Useful Tags:');
-define('_AM_SYSTEM_MAILUSERS_MAILTAGS1', '{X_UID} will print user id');
-define('_AM_SYSTEM_MAILUSERS_MAILTAGS2', '{X_UNAME} will print user name');
-define('_AM_SYSTEM_MAILUSERS_MAILTAGS3', '{X_UEMAIL} will print user email');
-define('_AM_SYSTEM_MAILUSERS_MAILTAGS4', '{X_UACTLINK} will print user activation link');
-define('_AM_SYSTEM_MAILUSERS_MAILSUBJECT', 'Subject');
-define('_AM_SYSTEM_MAILUSERS_MANAGER', 'User Message Manager');
-define('_AM_SYSTEM_MAILUSERS_SENDNEXT', 'Next');
-define('_AM_SYSTEM_MAILUSERS_NOUSERMATCH', 'No user matched');
-define('_AM_SYSTEM_MAILUSERS_PM', 'Private Message');
-define('_AM_SYSTEM_MAILUSERS_SENDCOMP', 'Sending message completed.');
-define('_AM_SYSTEM_MAILUSERS_SENDTO', 'Send to');
-define('_AM_SYSTEM_MAILUSERS_SENDTOUSERS', 'Send message to users whose:');
-define('_AM_SYSTEM_MAILUSERS_SENDTOUSERS2', 'Send to:');
-define('_AM_SYSTEM_MAILUSERS_SENT', 'Sent Users');
-define('_AM_SYSTEM_MAILUSERS_SENTNUM', '%s - %s (total: %s users)');
-define('_AM_SYSTEM_MAILUSERS_TIMEFORMAT', '(Format yyyy-mm-dd, optional)');
+define('_AM_SYSTEM_MAILUSERS_AMIFCHECKD', 'Ha ez be van jelölve, minden fenti feltételt figyelmen kívül hagyunk, beleértve a privát üzenetküldést is.');
+define('_AM_SYSTEM_MAILUSERS_EMAIL', 'E-mail');
+define('_AM_SYSTEM_MAILUSERS_GROUPIS', ' - Csoport:');
+define('_AM_SYSTEM_MAILUSERS_DAY', ' - Utolsó bejelentkezés napokkal ezelőtt:');
+define('_AM_SYSTEM_MAILUSERS_IDLEMORE', 'több mint X');
+define('_AM_SYSTEM_MAILUSERS_IDLELESS', 'kevesebb mint X');
+define('_AM_SYSTEM_MAILUSERS_DATE', ' - Utolsó bejelentkezés dátuma:');
+define('_AM_SYSTEM_MAILUSERS_LASTLOGMAX', 'előtt');
+define('_AM_SYSTEM_MAILUSERS_LASTLOGMIN', 'után');
+define('_AM_SYSTEM_MAILUSERS_REGDATE', ' - Regisztráció dátuma:');
+define('_AM_SYSTEM_MAILUSERS_REGDMIN', 'után');
+define('_AM_SYSTEM_MAILUSERS_REGDMAX', 'előtt');
+define('_AM_SYSTEM_MAILUSERS_INACTIVE', 'Üzenet küldése csak inaktív felhasználóknak');
+define('_AM_SYSTEM_MAILUSERS_MAILOK', 'Üzenet küldése csak azoknak a felhasználóknak, akik elfogadják az értesítő üzeneteket');
+define('_AM_SYSTEM_MAILUSERS_OPTIONAL', 'Opcionális érték');
+define('_AM_SYSTEM_MAILUSERS_LIST', 'Üzenet Küldése Felhasználóknak');
+define('_AM_SYSTEM_MAILUSERS_MAILBODY', 'Szövegtörzs');
+define('_AM_SYSTEM_MAILUSERS_MAILFNAME', 'Feladó Neve (csak e-mail)');
+define('_AM_SYSTEM_MAILUSERS_MAILFMAIL', 'Feladó E-mail címe (csak e-mail)');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS', 'Hasznos Címkék (Tags):');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS1', '{X_UID} kiírja a felhasználó azonosítóját');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS2', '{X_UNAME} kiírja a felhasználó nevét');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS3', '{X_UEMAIL} kiírja a felhasználó e-mail címét');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS4', '{X_UACTLINK} kiírja a felhasználó aktiválási linkjét');
+define('_AM_SYSTEM_MAILUSERS_MAILSUBJECT', 'Tárgy');
+define('_AM_SYSTEM_MAILUSERS_MANAGER', 'Felhasználói Üzenet Kezelő');
+define('_AM_SYSTEM_MAILUSERS_SENDNEXT', 'Tovább');
+define('_AM_SYSTEM_MAILUSERS_NOUSERMATCH', 'Nincs megfelelő felhasználó');
+define('_AM_SYSTEM_MAILUSERS_PM', 'Privát Üzenet');
+define('_AM_SYSTEM_MAILUSERS_SENDCOMP', 'Az üzenetküldés befejeződött.');
+define('_AM_SYSTEM_MAILUSERS_SENDTO', 'Küldés');
+define('_AM_SYSTEM_MAILUSERS_SENDTOUSERS', 'Üzenet küldése azoknak a felhasználóknak, akik:');
+define('_AM_SYSTEM_MAILUSERS_SENDTOUSERS2', 'Küldés ide:');
+define('_AM_SYSTEM_MAILUSERS_SENT', 'Elküldött Felhasználók');
+define('_AM_SYSTEM_MAILUSERS_SENTNUM', '%s - %s (összesen: %s felhasználó)');
+define('_AM_SYSTEM_MAILUSERS_TIMEFORMAT', '(Formátum: éééé-hh-nn, opcionális)');
 
 //XOOPS 2.5.11
-define('_AM_SYSTEM_MAILUSERS_MAILTAGS5', '{X_NAME} will print name or uname if empty');
+define('_AM_SYSTEM_MAILUSERS_MAILTAGS5', '{X_NAME} kiírja a nevet vagy a felhasználónevet, ha a név üres');
