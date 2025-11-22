4c1ed12b3e7c067806e62362e4aedc4008ff52e8
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:44:59 +0100

    Update admin.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/admin.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/admin.php
@@ -1,76 +1,77 @@
 <?php
 /**
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
- * @license             GNU GPL 2 (https://www.gnu.org/licenses/gpl-2.0.html)
- * _LANGCODE    en
- * _CHARSET     UTF-8
+ * @license         GNU GPL 2 (https://www.gnu.org/licenses/gpl-2.0.html)
+ * _LANGCODE        hu
+ * _CHARSET         UTF-8
+ * Translator: Molnár Vilmos (XOOPS Translation Team)
  */
 // Main
-define('_AM_SYSTEM_CONFIG', 'System Configuration');
-define('_AM_SYSTEM_CPANEL', 'Control Panel');
-define('_AM_SYSTEM_UPDATE', 'Update');
-define('_AM_SYSTEM_GOTOMODULE', 'Go to module');
-define('_AM_SYSTEM_HELP', 'Help');
-define('_AM_SYSTEM_HELP_VIEW', 'Display Help');
-define('_AM_SYSTEM_HELP_HIDE', 'Hide Help');
-define('_AM_SYSTEM_TIPS', 'Tips');
-define('_AM_SYSTEM_SECTION', 'Section');
-define('_AM_SYSTEM_DESC', 'Description');
-define('_AM_SYSTEM_GO', 'Access to this section');
-define('_AM_SYSTEM_STATUS', 'Change status section');
-define('_AM_SYSTEM_LOADING', 'Loading');
-define('_AM_SYSTEM_ALL', 'All');
-define('_AM_SYSTEM_TIPS_MAIN', '<ul><li>Enable or disable sections of system module or just access to it.</li></ul>');
-define('_AM_SYSTEM_AVATAR_INFO', "<ul><li><span class='bold red'>%s</span> avatars.</li></ul>");
-define('_AM_SYSTEM_BANNER_INFO', "<ul><li><span class='bold red'>%s</span> banners.</li></ul>");
-define('_AM_SYSTEM_COMMENT_INFO', "<ul><li><span class='bold red'>%s</span> comments.</li></ul>");
-define('_AM_SYSTEM_GROUP_INFO', "<ul><li><span class='bold red'>%s</span> groups.</li></ul>");
-define('_AM_SYSTEM_IMG_INFO', "<ul><li><span class='bold red'>%s</span> images.</li></ul>");
-define('_AM_SYSTEM_SMILIES_INFO', "<ul><li><span class='bold red'>%s</span> smilies.</li></ul>");
-define('_AM_SYSTEM_RANKS_INFO', "<ul><li><span class='bold red'>%s</span> user ranks.</li></ul>");
-define('_AM_SYSTEM_USERS_INFO', "<ul><li><span class='bold red'>%s</span> users.</li></ul>");
+define('_AM_SYSTEM_CONFIG', 'Rendszerkonfiguráció');
+define('_AM_SYSTEM_CPANEL', 'Vezérlőpult');
+define('_AM_SYSTEM_UPDATE', 'Frissítés');
+define('_AM_SYSTEM_GOTOMODULE', 'Ugrás a modulhoz');
+define('_AM_SYSTEM_HELP', 'Súgó');
+define('_AM_SYSTEM_HELP_VIEW', 'Súgó Megjelenítése');
+define('_AM_SYSTEM_HELP_HIDE', 'Súgó Elrejtése');
+define('_AM_SYSTEM_TIPS', 'Tippek');
+define('_AM_SYSTEM_SECTION', 'Szekció');
+define('_AM_SYSTEM_DESC', 'Leírás');
+define('_AM_SYSTEM_GO', 'Hozzáférés ehhez a szekcióhoz');
+define('_AM_SYSTEM_STATUS', 'Szekció állapotának megváltoztatása');
+define('_AM_SYSTEM_LOADING', 'Betöltés');
+define('_AM_SYSTEM_ALL', 'Összes');
+define('_AM_SYSTEM_TIPS_MAIN', '<ul><li>Engedélyezd vagy tiltsd le a Rendszer modul szekcióit, vagy egyszerűen csak lépj be oda.</li></ul>');
+define('_AM_SYSTEM_AVATAR_INFO', "<ul><li><span class='bold red'>%s</span> avatar.</li></ul>");
+define('_AM_SYSTEM_BANNER_INFO', "<ul><li><span class='bold red'>%s</span> reklámszalag (banner).</li></ul>");
+define('_AM_SYSTEM_COMMENT_INFO', "<ul><li><span class='bold red'>%s</span> hozzászólás.</li></ul>");
+define('_AM_SYSTEM_GROUP_INFO', "<ul><li><span class='bold red'>%s</span> csoport.</li></ul>");
+define('_AM_SYSTEM_IMG_INFO', "<ul><li><span class='bold red'>%s</span> kép.</li></ul>");
+define('_AM_SYSTEM_SMILIES_INFO', "<ul><li><span class='bold red'>%s</span> hangulatjel (smilies).</li></ul>");
+define('_AM_SYSTEM_RANKS_INFO', "<ul><li><span class='bold red'>%s</span> felhasználói rang.</li></ul>");
+define('_AM_SYSTEM_USERS_INFO', "<ul><li><span class='bold red'>%s</span> felhasználó.</li></ul>");
 // Admin Module Names and description
-define('_AM_SYSTEM_ADGS', 'Groups');
-define('_AM_SYSTEM_ADGS_DESC', 'Manage module access permissions for users and groups.');
-define('_AM_SYSTEM_BANS', 'Banners');
-define('_AM_SYSTEM_BANS_DESC', 'Manage XOOPS advertising banner feature.');
-define('_AM_SYSTEM_BLOCKS', 'Blocks');
-define('_AM_SYSTEM_BLOCKS_DESC', 'Blocks can display module content on any page. Manage blocks here.');
-define('_AM_SYSTEM_MODULES', 'Modules');
-define('_AM_SYSTEM_MODULES_DESC', 'Install and un-install XOOPS modules.');
-define('_AM_SYSTEM_SMLS', 'Smilies');
-define('_AM_SYSTEM_SMLS_DESC', 'Hide, change and add custom smilies for people to use in posts and comments.');
-define('_AM_SYSTEM_RANK', 'User Ranks');
-define('_AM_SYSTEM_RANK_DESC', 'Edit and create named ranks for users and moderators.');
-define('_AM_SYSTEM_USER', 'Users');
-define('_AM_SYSTEM_USER_DESC', 'Manually add users, edit user profiles and change passwords.');
-define('_AM_SYSTEM_PREF', 'Preferences');
-define('_AM_SYSTEM_PREF_DESC', 'Change the global preferences for your XOOPS website.');
-define('_AM_SYSTEM_MLUS', 'Email Users');
-define('_AM_SYSTEM_MLUS_DESC', 'Send emails or private messages to one user, or many users from here.');
-define('_AM_SYSTEM_IMAGES', 'Image Manager');
-define('_AM_SYSTEM_IMAGES_DESC', 'Create categories for the image manager and upload images here.');
-define('_AM_SYSTEM_AVATARS', 'Avatars');
-define('_AM_SYSTEM_AVATARS_DESC', 'Upload custom avatars for users to display in their profiles.');
-define('_AM_SYSTEM_TPLSETS', 'Templates');
-define('_AM_SYSTEM_TPLSETS_DESC', 'Edit module templates directly without editing the files on disc.');
-define('_AM_SYSTEM_COMMENTS', 'Comments');
-define('_AM_SYSTEM_COMMENTS_DESC', 'Many modules allow users to post comments. You can delete or edit them here.');
-define('_AM_SYSTEM_FILEMANAGER', 'File Manager');
-define('_AM_SYSTEM_FILEMANAGER_DESC', 'XOOPS file manager.');
-define('_AM_SYSTEM_MAINTENANCE', 'Maintenance');
-define('_AM_SYSTEM_MAINTENANCE_DESC', 'Maintenance tools for database tables, cache folders and session table.');
+define('_AM_SYSTEM_ADGS', 'Csoportok');
+define('_AM_SYSTEM_ADGS_DESC', 'Kezeld a modul hozzáférési jogosultságait a felhasználók és csoportok számára.');
+define('_AM_SYSTEM_BANS', 'Reklámszalagok (Bannerek)');
+define('_AM_SYSTEM_BANS_DESC', 'Kezeld a XOOPS hirdetési bannereit.');
+define('_AM_SYSTEM_BLOCKS', 'Blokkok');
+define('_AM_SYSTEM_BLOCKS_DESC', 'A blokkok bármely oldalon megjeleníthetik a modul tartalmát. Itt kezelheted a blokkokat.');
+define('_AM_SYSTEM_MODULES', 'Modulok');
+define('_AM_SYSTEM_MODULES_DESC', 'Telepítsd és távolítsd el a XOOPS modulokat.');
+define('_AM_SYSTEM_SMLS', 'Hangulatjelek (Smilies)');
+define('_AM_SYSTEM_SMLS_DESC', 'Rejtsd el, változtasd meg, és adj hozzá egyedi hangulatjeleket, amelyeket a felhasználók használhatnak a bejegyzésekben és hozzászólásokban.');
+define('_AM_SYSTEM_RANK', 'Felhasználói Rangok');
+define('_AM_SYSTEM_RANK_DESC', 'Szerkeszd és hozd létre a névvel ellátott rangokat a felhasználók és moderátorok számára.');
+define('_AM_SYSTEM_USER', 'Felhasználók');
+define('_AM_SYSTEM_USER_DESC', 'Manuálisan adj hozzá felhasználókat, szerkeszd a felhasználói profilokat és változtasd meg a jelszavakat.');
+define('_AM_SYSTEM_PREF', 'Beállítások');
+define('_AM_SYSTEM_PREF_DESC', 'Változtasd meg a XOOPS weboldalad globális beállításait.');
+define('_AM_SYSTEM_MLUS', 'E-mail Küldés Felhasználóknak');
+define('_AM_SYSTEM_MLUS_DESC', 'E-mailt vagy privát üzenetet küldhetsz egy vagy több felhasználónak innen.');
+define('_AM_SYSTEM_IMAGES', 'Képkezelő');
+define('_AM_SYSTEM_IMAGES_DESC', 'Készíts kategóriákat a képkezelőhöz, és tölts fel ide képeket.');
+define('_AM_SYSTEM_AVATARS', 'Avatárok');
+define('_AM_SYSTEM_AVATARS_DESC', 'Tölts fel egyedi avatárokat, amelyeket a felhasználók megjeleníthetnek profiljukban.');
+define('_AM_SYSTEM_TPLSETS', 'Sablonok (Templates)');
+define('_AM_SYSTEM_TPLSETS_DESC', 'Szerkeszd a modul sablonjait közvetlenül anélkül, hogy a lemezen lévő fájlokat szerkesztenéd.');
+define('_AM_SYSTEM_COMMENTS', 'Hozzászólások');
+define('_AM_SYSTEM_COMMENTS_DESC', 'Sok modul engedélyezi a felhasználóknak a hozzászólások feladását. Itt törölheted vagy szerkesztheted őket.');
+define('_AM_SYSTEM_FILEMANAGER', 'Fájlkezelő');
+define('_AM_SYSTEM_FILEMANAGER_DESC', 'XOOPS fájlkezelő.');
+define('_AM_SYSTEM_MAINTENANCE', 'Karbantartás');
+define('_AM_SYSTEM_MAINTENANCE_DESC', 'Karbantartási eszközök az adatbázis táblákhoz, gyorsítótár mappákhoz és a munkamenet (session) táblához.');
 // Messages
-define('_AM_SYSTEM_DBUPDATED', 'Database Updated Successfully!');
-define('_AM_SYSTEM_DBERROR', 'Database was not updated due to some error!');
-define('_AM_SYSTEM_NOTACTIVE', 'This section is not active!');
+define('_AM_SYSTEM_DBUPDATED', 'Az Adatbázis Sikeresen Frissítve!');
+define('_AM_SYSTEM_DBERROR', 'Az adatbázis nem frissült hiba miatt!');
+define('_AM_SYSTEM_NOTACTIVE', 'Ez a szekció nem aktív!');
 // Group permission phrases
-define('_MD_AM_PERMADDNG', 'Could not add %s permission to %s for group %s');
-define('_MD_AM_PERMADDOK', 'Added %s permission to %s for group %s');
-define('_MD_AM_PERMRESETNG', 'Could not reset group permission for module %s');
-define('_MD_AM_PERMADDNGP', 'All parent items must be selected.');
-define('_AM_SYSTEM_UNINSTALL', 'Uninstall');
+define('_MD_AM_PERMADDNG', 'Nem sikerült hozzáadni a(z) %s jogosultságot a(z) %s elemhez a(z) %s csoport számára');
+define('_MD_AM_PERMADDOK', 'A(z) %s jogosultság sikeresen hozzáadva a(z) %s elemhez a(z) %s csoport számára');
+define('_MD_AM_PERMRESETNG', 'Nem sikerült visszaállítani a csoport jogosultságát a(z) %s modulhoz');
+define('_MD_AM_PERMADDNGP', 'Minden szülőelemet ki kell választani.');
+define('_AM_SYSTEM_UNINSTALL', 'Eltávolítás');
 
 //2.5.7
-define('_AM_SYSTEM_USAGE', 'Usage');
-define('_AM_SYSTEM_ACTIVE', 'Active');
+define('_AM_SYSTEM_USAGE', 'Használat');
+define('_AM_SYSTEM_ACTIVE', 'Aktív');
