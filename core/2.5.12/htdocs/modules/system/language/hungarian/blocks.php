4337f34868df59a7ab1370549ac0a16a7b6c086e
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:45:40 +0100

    Update blocks.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/blocks.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/blocks.php
@@ -1,50 +1,50 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
+// Translator: Molnár Vilmos (XOOPS Translation Team)
 // Blocks
-define('_MB_SYSTEM_ADMENU', 'Administration Menu');
-define('_MB_SYSTEM_RNOW', 'Register now!');
-define('_MB_SYSTEM_LPASS', 'Lost Password?');
-define('_MB_SYSTEM_SEARCH', 'Search');
-define('_MB_SYSTEM_ADVS', 'Advanced Search');
-define('_MB_SYSTEM_VACNT', 'View Account');
-define('_MB_SYSTEM_EACNT', 'Edit Account');
+define('_MB_SYSTEM_ADMENU', 'Adminisztrációs Menü');
+define('_MB_SYSTEM_RNOW', 'Regisztrálj most!');
+define('_MB_SYSTEM_LPASS', 'Elfelejtetted a jelszavad?');
+define('_MB_SYSTEM_SEARCH', 'Keresés');
+define('_MB_SYSTEM_ADVS', 'Részletes Keresés');
+define('_MB_SYSTEM_VACNT', 'Fiók Megtekintése');
+define('_MB_SYSTEM_EACNT', 'Fiók Szerkesztése');
 // RMV-NOTIFY
-define('_MB_SYSTEM_NOTIF', 'Notifications');
-define('_MB_SYSTEM_LOUT', 'Logout');
-define('_MB_SYSTEM_INBOX', 'Inbox');
-define('_MB_SYSTEM_SUBMS', 'Submitted News');
-define('_MB_SYSTEM_WLNKS', 'Waiting Links');
-define('_MB_SYSTEM_BLNK', 'Broken Links');
-define('_MB_SYSTEM_MLNKS', 'Modified Links');
-define('_MB_SYSTEM_WDLS', 'Waiting Downloads');
-define('_MB_SYSTEM_BFLS', 'Broken Files');
-define('_MB_SYSTEM_MFLS', 'Modified Downloads');
-define('_MB_SYSTEM_TDMDOWNLOADS', 'Waiting Downloads');
-define('_MB_SYSTEM_EXTGALLERY', 'Waiting Photos');
-define('_MB_SYSTEM_SMARTSECTION', 'Articles');
-define('_MB_SYSTEM_HOME', 'Home'); // link to home page in main menu block
-define('_MB_SYSTEM_RECO', 'Recommend Us');
-define('_MB_SYSTEM_PWWIDTH', 'Pop-Up Window Width');
-define('_MB_SYSTEM_PWHEIGHT', 'Pop-Up Window Height');
-define('_MB_SYSTEM_LOGO', 'Logo image is located %s directory');  // %s is your root image directory name
-define('_MB_SYSTEM_COMPEND', 'Comments');
+define('_MB_SYSTEM_NOTIF', 'Értesítések');
+define('_MB_SYSTEM_LOUT', 'Kijelentkezés');
+define('_MB_SYSTEM_INBOX', 'Bejövő (Inbox)');
+define('_MB_SYSTEM_SUBMS', 'Elküldött Hírek');
+define('_MB_SYSTEM_WLNKS', 'Várakozó Hivatkozások');
+define('_MB_SYSTEM_BLNK', 'Hibás Hivatkozások');
+define('_MB_SYSTEM_MLNKS', 'Módosított Hivatkozások');
+define('_MB_SYSTEM_WDLS', 'Várakozó Letöltések');
+define('_MB_SYSTEM_BFLS', 'Hibás Fájlok');
+define('_MB_SYSTEM_MFLS', 'Módosított Letöltések');
+define('_MB_SYSTEM_TDMDOWNLOADS', 'Várakozó Letöltések');
+define('_MB_SYSTEM_EXTGALLERY', 'Várakozó Fotók');
+define('_MB_SYSTEM_SMARTSECTION', 'Cikkek');
+define('_MB_SYSTEM_HOME', 'Kezdőlap'); // link to home page in main menu block
+define('_MB_SYSTEM_RECO', 'Ajánlj minket');
+define('_MB_SYSTEM_PWWIDTH', 'Felugró Ablak Szélessége');
+define('_MB_SYSTEM_PWHEIGHT', 'Felugró Ablak Magassága');
+define('_MB_SYSTEM_LOGO', 'A logó kép a %s könyvtárban található'); // %s is your root image directory name
+define('_MB_SYSTEM_COMPEND', 'Hozzászólások');
 //define('_MB_SYSTEM_LOGGEDINAS',"Logged in as");
-define('_MB_SYSTEM_SADMIN', 'Show admin groups');
-define('_MB_SYSTEM_SPMTO', 'Send Private Message to %s');
-define('_MB_SYSTEM_SEMTO', 'Send Email to %s');
-define('_MB_SYSTEM_DISPLAY', 'Display %s members');
-define('_MB_SYSTEM_DISPLAYA', 'Display member avatars');
-define('_MB_SYSTEM_NODISPGR', 'Do not display users whose rank is:');
-define('_MB_SYSTEM_DISPLAYC', 'Display %s comments');
-define('_MB_SYSTEM_SECURE', 'Secure Login');
-define('_MB_SYSTEM_NUMTHEME', '%s themes');
-define('_MB_SYSTEM_THSHOW', 'Display screenshot image');
-define('_MB_SYSTEM_THWIDTH', 'Screenshot image width');
-define('_MB_SYSTEM_REMEMBERME', 'Remember me');
+define('_MB_SYSTEM_SADMIN', 'Admin csoportok mutatása');
+define('_MB_SYSTEM_SPMTO', 'Privát Üzenet Küldése neki: %s');
+define('_MB_SYSTEM_SEMTO', 'E-mail Küldése neki: %s');
+define('_MB_SYSTEM_DISPLAY', 'Megjelenítés %s tagot');
+define('_MB_SYSTEM_DISPLAYA', 'Tag avatárok megjelenítése');
+define('_MB_SYSTEM_NODISPGR', 'Ne jelenítse meg azokat a felhasználókat, akiknek a rangja:');
+define('_MB_SYSTEM_DISPLAYC', 'Megjelenítés %s hozzászólást');
+define('_MB_SYSTEM_SECURE', 'Biztonságos Bejelentkezés');
+define('_MB_SYSTEM_NUMTHEME', '%s téma');
+define('_MB_SYSTEM_THSHOW', 'Képernyőkép (screenshot) megjelenítése');
+define('_MB_SYSTEM_THWIDTH', 'Képernyőkép szélessége');
+define('_MB_SYSTEM_REMEMBERME', 'Emlékezz rám');
 //2.5.8
-define('_MB_SYSTEM_BLOCK_HEIGHT', 'Block Height (lines)');
+define('_MB_SYSTEM_BLOCK_HEIGHT', 'Blokk Magassága (sor)');
 //2.5.12
-define('_MB_SYSTEM_WAITING_CONTENT_DEPRECATED', "Block 'Waiting Contents' is deprecated since XOOPS 2.5.11, please use Waiting module");
+define('_MB_SYSTEM_WAITING_CONTENT_DEPRECATED', "A 'Várakozó Tartalmak' (Waiting Contents) blokk elavult (deprecated) a XOOPS 2.5.11 óta, kérlek, használd a Waiting modult");
