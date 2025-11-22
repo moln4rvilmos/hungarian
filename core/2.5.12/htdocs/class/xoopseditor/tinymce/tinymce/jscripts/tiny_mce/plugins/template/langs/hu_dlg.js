6238b72b4a9888c929f7ca12fa73d9bd10fb2935
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:06:53 +0100

    Create hu_dlg.js

---
+++ core/2.5.12/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/template/langs/hu_dlg.js
@@ -0,0 +1,15 @@
+tinyMCE.addI18n('hu.template_dlg',{
+    title:"Sablonok",
+    label:"Sablon",
+    "desc_label":"Leírás",
+    desc:"Előre Meghatározott Sablon Tartalom Beillesztése",
+    select:"Sablon Kiválasztása",
+    preview:"Előnézet",
+    warning:"Figyelem: Egy sablon frissítése egy másikkal adatvesztést okozhat.",
+    "mdate_format":"%Y-%m-%d %H:%M:%S",
+    "cdate_format":"%Y-%m-%d %H:%M:%S",
+    "months_long":"Január,Február,Március,Április,Május,Június,Július,Augusztus,Szeptember,Október,November,December",
+    "months_short":"Jan,Feb,Már,Ápr,Máj,Jún,Júl,Aug,Sze,Okt,Nov,Dec",
+    "day_long":"Vasárnap,Hétfő,Kedd,Szerda,Csütörtök,Péntek,Szombat,Vasárnap",
+    "day_short":"V,H,K,Sze,Cs,P,Szo,V"
+});
