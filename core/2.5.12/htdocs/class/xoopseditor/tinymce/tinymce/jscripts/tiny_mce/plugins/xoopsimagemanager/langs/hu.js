f8955f23a5fc45379320fa5a59d0b8bb97a5f9bc
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:14:23 +0100

    Create hu.js

---
+++ core/2.5.12/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/xoopsimagemanager/langs/hu.js
@@ -0,0 +1,13 @@
+/**
+ */
+
+tinyMCE.addI18n('hu.xoopsimagemanager',{
+    desc : 'Xoops Haladó Képkezelő',
+    delta_width : '0',
+    delta_height : '0'
+});
+tinyMCE.addI18n('hu.xoopsimagebrowser',{
+    desc : 'Xoops Képböngésző',
+    delta_width : '0',
+    delta_height : '0'
+});
