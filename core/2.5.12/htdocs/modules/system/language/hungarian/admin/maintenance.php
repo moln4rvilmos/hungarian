1c03682b7ccd9f4e136be72551e7f89c50d702ce
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:57:34 +0100

    Update maintenance.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/admin/maintenance.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/admin/maintenance.php
@@ -2,61 +2,62 @@
 /**
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
  * @license         GNU GPL 2.0 or later (https://www.gnu.org/licenses/gpl-2.0.html)
- * _LANGCODE    en
- * _CHARSET     UTF-8
+ * _LANGCODE        hu
+ * _CHARSET         UTF-8
+ * Translator: Molnár Vilmos (XOOPS Translation Team)
  */
 //Nav
-define('_AM_SYSTEM_MAINTENANCE_NAV_MANAGER', 'Maintenance');
-define('_AM_SYSTEM_MAINTENANCE_NAV_LIST', 'All maintenance');
-define('_AM_SYSTEM_MAINTENANCE_NAV_DUMP', 'Dump');
-define('_AM_SYSTEM_MAINTENANCE_SESSION', 'Empty the sessions table');
-define('_AM_SYSTEM_MAINTENANCE_SESSION_OK', 'Session maintenance : OK');
-define('_AM_SYSTEM_MAINTENANCE_SESSION_NOTOK', 'Session maintenance : Error');
-define('_AM_SYSTEM_MAINTENANCE_AVATAR', 'Purge unused custom avatars');
-define('_AM_SYSTEM_MAINTENANCE_CACHE', 'Clean cache folder');
-define('_AM_SYSTEM_MAINTENANCE_CACHE_OK', 'Cache maintenance : OK');
-define('_AM_SYSTEM_MAINTENANCE_CACHE_NOTOK', 'Cache maintenance : Error');
-define('_AM_SYSTEM_MAINTENANCE_TABLES', 'Tables maintenance');
-define('_AM_SYSTEM_MAINTENANCE_TABLES_OK', 'Tables maintenance : OK');
-define('_AM_SYSTEM_MAINTENANCE_TABLES_NOTOK', 'Tables maintenance : Error');
-define('_AM_SYSTEM_MAINTENANCE_QUERY_DESC', 'Optimize, Check, Repair and Analyze your tables');
-define('_AM_SYSTEM_MAINTENANCE_QUERY_OK', 'Maintain database : OK');
-define('_AM_SYSTEM_MAINTENANCE_QUERY_NOTOK', 'Maintain database : Error');
-define('_AM_SYSTEM_MAINTENANCE_CHOICE1', 'Optimize table(s)');
-define('_AM_SYSTEM_MAINTENANCE_CHOICE2', 'Check table(s)');
-define('_AM_SYSTEM_MAINTENANCE_CHOICE3', 'Repair table(s)');
-define('_AM_SYSTEM_MAINTENANCE_CHOICE4', 'Analyze table(s)');
-define('_AM_SYSTEM_MAINTENANCE_TABLES_DESC', 'ANALYZE TABLE analyzes and stores the key distribution for a table. During the analysis, the table is locked with a read lock.<br>
-CHECK TABLE checks a table or tables for errors.<br>
-OPTIMIZE TABLE to reclaim the unused space and to defragment the data file.<br>
-REPAIR TABLE repairs a possibly corrupted table.');
-define('_AM_SYSTEM_MAINTENANCE_RESULT', 'Result');
-define('_AM_SYSTEM_MAINTENANCE_RESULT_NO_RESULT', 'Not Result');
-define('_AM_SYSTEM_MAINTENANCE_RESULT_CACHE', 'Clean Cache task');
-define('_AM_SYSTEM_MAINTENANCE_RESULT_SESSION', 'Clean sessions table task');
-define('_AM_SYSTEM_MAINTENANCE_RESULT_QUERY', 'Database task');
-define('_AM_SYSTEM_MAINTENANCE_RESULT_AVATAR', 'Purge unused avatars task');
-define('_AM_SYSTEM_MAINTENANCE_ERROR_MAINTENANCE', 'No choice for maintenance');
-define('_AM_SYSTEM_MAINTENANCE_TABLES1', 'Tables');
-define('_AM_SYSTEM_MAINTENANCE_TABLES_OPTIMIZE', 'Optimize');
-define('_AM_SYSTEM_MAINTENANCE_TABLES_CHECK', 'Check');
-define('_AM_SYSTEM_MAINTENANCE_TABLES_REPAIR', 'Repair');
-define('_AM_SYSTEM_MAINTENANCE_TABLES_ANALYZE', 'Analyze');
+define('_AM_SYSTEM_MAINTENANCE_NAV_MANAGER', 'Karbantartás');
+define('_AM_SYSTEM_MAINTENANCE_NAV_LIST', 'Minden karbantartás');
+define('_AM_SYSTEM_MAINTENANCE_NAV_DUMP', 'Adatbázis Mentés (Dump)');
+define('_AM_SYSTEM_MAINTENANCE_SESSION', 'Munkamenet (session) tábla ürítése');
+define('_AM_SYSTEM_MAINTENANCE_SESSION_OK', 'Munkamenet karbantartás: OK');
+define('_AM_SYSTEM_MAINTENANCE_SESSION_NOTOK', 'Munkamenet karbantartás: Hiba');
+define('_AM_SYSTEM_MAINTENANCE_AVATAR', 'Nem használt egyedi avatárok tisztítása');
+define('_AM_SYSTEM_MAINTENANCE_CACHE', 'Gyorsítótár mappa tisztítása (Cache)');
+define('_AM_SYSTEM_MAINTENANCE_CACHE_OK', 'Gyorsítótár karbantartás: OK');
+define('_AM_SYSTEM_MAINTENANCE_CACHE_NOTOK', 'Gyorsítótár karbantartás: Hiba');
+define('_AM_SYSTEM_MAINTENANCE_TABLES', 'Táblák karbantartása');
+define('_AM_SYSTEM_MAINTENANCE_TABLES_OK', 'Táblák karbantartása: OK');
+define('_AM_SYSTEM_MAINTENANCE_TABLES_NOTOK', 'Táblák karbantartása: Hiba');
+define('_AM_SYSTEM_MAINTENANCE_QUERY_DESC', 'Optimalizálás, Ellenőrzés, Javítás és Elemzés a táblákon');
+define('_AM_SYSTEM_MAINTENANCE_QUERY_OK', 'Adatbázis karbantartása: OK');
+define('_AM_SYSTEM_MAINTENANCE_QUERY_NOTOK', 'Adatbázis karbantartása: Hiba');
+define('_AM_SYSTEM_MAINTENANCE_CHOICE1', 'Tábla(k) Optimalizálása');
+define('_AM_SYSTEM_MAINTENANCE_CHOICE2', 'Tábla(k) Ellenőrzése');
+define('_AM_SYSTEM_MAINTENANCE_CHOICE3', 'Tábla(k) Javítása');
+define('_AM_SYSTEM_MAINTENANCE_CHOICE4', 'Tábla(k) Elemzése (Analyze)');
+define('_AM_SYSTEM_MAINTENANCE_TABLES_DESC', 'Az **ANALYZE TABLE** elemzi és tárolja a kulcselosztást egy táblához. Az elemzés során a tábla olvasási zárral zárolva van.<br>
+A **CHECK TABLE** ellenőrzi egy vagy több táblában a hibákat.<br>
+Az **OPTIMIZE TABLE** visszanyeri a fel nem használt helyet és töredezettségmentesíti az adatfájlt.<br>
+A **REPAIR TABLE** megjavít egy esetlegesen sérült táblát.');
+define('_AM_SYSTEM_MAINTENANCE_RESULT', 'Eredmény');
+define('_AM_SYSTEM_MAINTENANCE_RESULT_NO_RESULT', 'Nincs Eredmény');
+define('_AM_SYSTEM_MAINTENANCE_RESULT_CACHE', 'Gyorsítótár tisztítási feladat');
+define('_AM_SYSTEM_MAINTENANCE_RESULT_SESSION', 'Munkamenet tábla tisztítási feladat');
+define('_AM_SYSTEM_MAINTENANCE_RESULT_QUERY', 'Adatbázis feladat');
+define('_AM_SYSTEM_MAINTENANCE_RESULT_AVATAR', 'Nem használt avatárok tisztítási feladat');
+define('_AM_SYSTEM_MAINTENANCE_ERROR_MAINTENANCE', 'Nincs karbantartási opció kiválasztva');
+define('_AM_SYSTEM_MAINTENANCE_TABLES1', 'Táblák');
+define('_AM_SYSTEM_MAINTENANCE_TABLES_OPTIMIZE', 'Optimalizálás');
+define('_AM_SYSTEM_MAINTENANCE_TABLES_CHECK', 'Ellenőrzés');
+define('_AM_SYSTEM_MAINTENANCE_TABLES_REPAIR', 'Javítás');
+define('_AM_SYSTEM_MAINTENANCE_TABLES_ANALYZE', 'Elemzés');
 //Dump
-define('_AM_SYSTEM_MAINTENANCE_DUMP', 'Dump');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_TABLES_OR_MODULES', 'Select tables or modules');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_DROP', "Add command DROP TABLE IF EXISTS 'tables' in the dump");
-define('_AM_SYSTEM_MAINTENANCE_DUMP_OR', 'OR');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_AND', 'AND');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_ERROR_TABLES_OR_MODULES', 'You must select the tables or modules');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_NO_TABLES', 'No tables');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_TABLES', 'Tables');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_STRUCTURES', 'Structures');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_NB_RECORDS', 'Numbers of records');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_FILE_CREATED', 'File created');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_RESULT', 'Result');
-define('_AM_SYSTEM_MAINTENANCE_DUMP_RECORDS', 'record(s)');
+define('_AM_SYSTEM_MAINTENANCE_DUMP', 'Adatbázis Mentés (Dump)');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_TABLES_OR_MODULES', 'Válaszd ki a táblákat vagy modulokat');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_DROP', "Adja hozzá a 'DROP TABLE IF EXISTS [táblák]' parancsot a mentéshez");
+define('_AM_SYSTEM_MAINTENANCE_DUMP_OR', 'VAGY');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_AND', 'ÉS');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_ERROR_TABLES_OR_MODULES', 'Ki kell választanod a táblákat vagy a modulokat');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_NO_TABLES', 'Nincs tábla');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_TABLES', 'Táblák');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_STRUCTURES', 'Szerkezetek');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_NB_RECORDS', 'Rekordok száma');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_FILE_CREATED', 'Fájl létrehozva');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_RESULT', 'Eredmény');
+define('_AM_SYSTEM_MAINTENANCE_DUMP_RECORDS', 'rekord');
 // Tips
 define('_AM_SYSTEM_MAINTENANCE_TIPS', '<ul>
-<li>You can do a simple maintenance of your XOOPS Installation: clear your cache and session table, and do maintenance of your tables</li>
+<li>Végezhetsz egy egyszerű karbantartást a XOOPS telepítéseden: törölheted a gyorsítótáradat és a munkamenet táblát, és karbantarthatod a tábláidat.</li>
 </ul>');
