55fdcf04d4c1d85ae9fbf21548dbbd836482be86
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:16:46 +0100

    Create hu_dlg.js

---
+++ core/2.5.12/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/xoopsmlcontent/langs/hu_dlg.js
@@ -0,0 +1,13 @@
+/**
+ */
+
+tinyMCE.addI18n('hu.xoopsmlcontent_dlg',{
+    title : 'Többnyelvű Tartalom Beillesztése',
+    subtitle : 'Válassz nyelvet, majd írd/illeszd be a tartalmat az alábbi mezőbe:',
+    sellang : 'Nyelv kiválasztása',
+    chooselang : 'Választanod kell egy nyelvet',
+    maxstring : ' karakter van beírva, %maxchar% engedélyezett',
+    alertmaxstring : 'Elérted a maximálisan engedélyezett karakterszámot',
+    delta_width : 100,
+    delta_height : 100
+});
