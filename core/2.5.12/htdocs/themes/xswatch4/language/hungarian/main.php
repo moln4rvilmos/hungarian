f8f436ab9372a2a32e5b53081bf885ac9d439bf2
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 09:36:03 +0100

    Update main.php

--- core/2.5.12/htdocs/themes/xswatch4/language/hungarian/main.php
+++ core/2.5.12/htdocs/themes/xswatch4/language/hungarian/main.php
@@ -1,113 +1,113 @@
 <?php
 
-define('THEME_INFO', 'Info');
-define('THEME_CLOSE', 'Close');
+define('THEME_INFO', 'Infó');
+define('THEME_CLOSE', 'Bezár');
 
 //Nav Menu
-define('THEME_HOME', 'Home');
-define('THEME_MODULE1', 'About');
-define('THEME_MODULE2', 'News');
-define('THEME_MODULE3', 'Forum');
-define('THEME_MODULE4', 'Contact');
-define('THEME_SEARCH_TEXT', 'Search');
-define('THEME_SEARCH_BUTTON', 'Go');
+define('THEME_HOME', 'Kezdőlap');
+define('THEME_MODULE1', 'Rólunk');
+define('THEME_MODULE2', 'Hírek');
+define('THEME_MODULE3', 'Fórum');
+define('THEME_MODULE4', 'Kapcsolat');
+define('THEME_SEARCH_TEXT', 'Keresés');
+define('THEME_SEARCH_BUTTON', 'Indít');
 
 //Account tab
-define('THEME_ACCOUNT', 'Account');
-define('THEME_ACCOUNT_EDIT', 'Edit Account');
-define('THEME_ACCOUNT_LOGIN', 'Login');
-define('THEME_ACCOUNT_LOGOUT', 'Logout');
-define('THEME_ACCOUNT_MESSAGES', 'Messages');
-define('THEME_ACCOUNT_NOTIFICATIONS', 'Notifications');
-define('THEME_ACCOUNT_REGISTER', 'Sign Up');
-define('THEME_ACCOUNT_TOOLBAR', 'Toolbar');
+define('THEME_ACCOUNT', 'Fiók');
+define('THEME_ACCOUNT_EDIT', 'Fiók szerkesztése');
+define('THEME_ACCOUNT_LOGIN', 'Bejelentkezés');
+define('THEME_ACCOUNT_LOGOUT', 'Kijelentkezés');
+define('THEME_ACCOUNT_MESSAGES', 'Üzenetek');
+define('THEME_ACCOUNT_NOTIFICATIONS', 'Értesítések');
+define('THEME_ACCOUNT_REGISTER', 'Regisztráció');
+define('THEME_ACCOUNT_TOOLBAR', 'Eszköztár');
 
 //Slider
-define('THEME_READMORE', 'Read More');
+define('THEME_READMORE', 'Tovább olvasom');
 
 //Home Message
-define('THEME_ABOUTUS', 'About Us');
-define('THEME_LEARNMORE', 'Learn More...');
+define('THEME_ABOUTUS', 'Rólunk');
+define('THEME_LEARNMORE', 'Tudj meg többet...');
 
 //NewBB
-define('THEME_NEWBB_TOPIC', 'Toggle');
-define('THEME_FORUM_SPONSORBY', 'Sponsor By.: ');
-define('THEME_GOTOTHEFORUM', 'Go to Forum');
-define('THEME_FORUM_DESCRIPTION', 'Description');
-define('THEME_NEWBB_SEARCH_FORUM', 'Search in forum...');
-define('THEME_NEWBB_SEARCH_TOPIC', 'Search in topic...');
-define('THEME_FORUM_DESC', 'About This Forum');
-define('THEME_FORUM_NEWTOPIC', 'New Topic');
-define('THEME_FORUM_REGISTER', 'Register');
-define('THEME_FORUM_SEARCH', 'Search');
-define('THEME_FORUM_ADVSEARCH', 'Advanced Search');
+define('THEME_NEWBB_TOPIC', 'Váltás');
+define('THEME_FORUM_SPONSORBY', 'Szponzorálja: ');
+define('THEME_GOTOTHEFORUM', 'Ugrás a Fórumra');
+define('THEME_FORUM_DESCRIPTION', 'Leírás');
+define('THEME_NEWBB_SEARCH_FORUM', 'Keresés a fórumban...');
+define('THEME_NEWBB_SEARCH_TOPIC', 'Keresés a témában...');
+define('THEME_FORUM_DESC', 'Erről a fórumról');
+define('THEME_FORUM_NEWTOPIC', 'Új Téma');
+define('THEME_FORUM_REGISTER', 'Regisztráció');
+define('THEME_FORUM_SEARCH', 'Keresés');
+define('THEME_FORUM_ADVSEARCH', 'Haladó Keresés');
 if (!defined ('THEME_FORUM_REPLY')) {
-    define('THEME_FORUM_REPLY', 'Reply');
+    define('THEME_FORUM_REPLY', 'Válasz');
 }
-define('THEME_ADD_POLL', 'Add Poll');
+define('THEME_ADD_POLL', 'Szavazás Hozzáadása');
 
 //Block login
-define('THEME_LOGIN', 'Your Username');
-define('THEME_PASS', 'Your Password');
-define('THEME_EMAIL', 'Your Email');
+define('THEME_LOGIN', 'Felhasználóneved');
+define('THEME_PASS', 'Jelszavad');
+define('THEME_EMAIL', 'E-mail címed');
 
 //Cookie consent -- escape for javascript if needed
-define('THEME_COOKIE_MESSAGE', 'This website uses cookies to ensure you get the best experience on our website');
-define('THEME_COOKIE_DISMISS', 'Got it!');
-define('THEME_COOKIE_LEARNMORE', 'More info');
+define('THEME_COOKIE_MESSAGE', 'Ez a weboldal cookie-kat használ, hogy a legjobb élményt biztosítsa webhelyünkön');
+define('THEME_COOKIE_DISMISS', 'Rendben!');
+define('THEME_COOKIE_LEARNMORE', 'Több infó');
 
-define('THEME_SELECT_ALL', 'Select All');
+define('THEME_SELECT_ALL', 'Mindet kijelöl');
 
 // Toolbar
-define('THEME_TOOLBAR_HOME', 'Home');
-define('THEME_TOOLBAR_SETTINGS', 'Settings');
-define('THEME_TOOLBAR_CONTROL_PANEL', 'Control Panel');
-define('THEME_TOOLBAR_SYSTEM_CONFIG', 'System Configuration');
-define('THEME_TOOLBAR_PREFERENCES', 'Preferences');
-define('THEME_TOOLBAR_SYSTEM_MODULE', 'System Module');
-define('THEME_TOOLBAR_GENERAL_SETTINGS', 'General Settings');
-define('THEME_TOOLBAR_TOOLS', 'Tools');
-define('THEME_TOOLBAR_SYSTEM_OPTIONS', 'System Options');
-define('THEME_TOOLBAR_DEBUGMODE', 'Debug Mode');
-define('THEME_TOOLBAR_THIS_MODULE', 'This Module');
-define('THEME_TOOLBAR_MODULES', 'Modules');
-define('THEME_TOOLBAR_BLOCKS', 'Blocks');
-define('THEME_TOOLBAR_TEMPLATES', 'Templates');
-define('THEME_TOOLBAR_MAINTENANCE', 'Maintenance');
-define('THEME_TOOLBAR_USER_TOOLS', 'User');
-define('THEME_TOOLBAR_USERS', 'Users');
-define('THEME_TOOLBAR_GROUPS', 'Groups');
-define('THEME_TOOLBAR_RANKS', 'Ranks');
-define('THEME_TOOLBAR_FIND', 'Find');
-define('THEME_TOOLBAR_MAIL', 'Mail');
-define('THEME_TOOLBAR_COMMENTS', 'Comments');
-define('THEME_TOOLBAR_IMAGE_TOOLS', 'Image');
-define('THEME_TOOLBAR_IMAGES', 'Images');
-define('THEME_TOOLBAR_AVATARS', 'Avatars');
-define('THEME_TOOLBAR_SMILIES', 'Smilies');
-define('THEME_TOOLBAR_BANNERS', 'Banners');
-define('THEME_TOOLBAR_CLOSE', 'Close');
+define('THEME_TOOLBAR_HOME', 'Kezdőlap');
+define('THEME_TOOLBAR_SETTINGS', 'Beállítások');
+define('THEME_TOOLBAR_CONTROL_PANEL', 'Vezérlőpult');
+define('THEME_TOOLBAR_SYSTEM_CONFIG', 'Rendszerkonfiguráció');
+define('THEME_TOOLBAR_PREFERENCES', 'Beállítások');
+define('THEME_TOOLBAR_SYSTEM_MODULE', 'Rendszer Modul');
+define('THEME_TOOLBAR_GENERAL_SETTINGS', 'Általános Beállítások');
+define('THEME_TOOLBAR_TOOLS', 'Eszközök');
+define('THEME_TOOLBAR_SYSTEM_OPTIONS', 'Rendszeropciók');
+define('THEME_TOOLBAR_DEBUGMODE', 'Hibakeresési Mód');
+define('THEME_TOOLBAR_THIS_MODULE', 'Ez a Modul');
+define('THEME_TOOLBAR_MODULES', 'Modulok');
+define('THEME_TOOLBAR_BLOCKS', 'Blokkok');
+define('THEME_TOOLBAR_TEMPLATES', 'Sablonok');
+define('THEME_TOOLBAR_MAINTENANCE', 'Karbantartás');
+define('THEME_TOOLBAR_USER_TOOLS', 'Felhasználó');
+define('THEME_TOOLBAR_USERS', 'Felhasználók');
+define('THEME_TOOLBAR_GROUPS', 'Csoportok');
+define('THEME_TOOLBAR_RANKS', 'Rangok');
+define('THEME_TOOLBAR_FIND', 'Keresés');
+define('THEME_TOOLBAR_MAIL', 'E-mail');
+define('THEME_TOOLBAR_COMMENTS', 'Hozzászólások');
+define('THEME_TOOLBAR_IMAGE_TOOLS', 'Kép');
+define('THEME_TOOLBAR_IMAGES', 'Képek');
+define('THEME_TOOLBAR_AVATARS', 'Avatárok');
+define('THEME_TOOLBAR_SMILIES', 'Hangulatjelek');
+define('THEME_TOOLBAR_BANNERS', 'Bannerek');
+define('THEME_TOOLBAR_CLOSE', 'Bezár');
 
-define('THEME_TOOLBAR_SHOW_BLOCK_EDIT', 'Block Edits');
-define('THEME_TOOLBAR_EDIT_THIS_BLOCK', 'Edit this block');
+define('THEME_TOOLBAR_SHOW_BLOCK_EDIT', 'Blokk Szerkesztés');
+define('THEME_TOOLBAR_EDIT_THIS_BLOCK', 'Ezt a blokkot szerkesztem');
 
-define('THEME_COMMENT_ADD', 'Add Your Comment');
-define('THEME_COMMENT_OPTIONS', 'Comment View Options');
+define('THEME_COMMENT_ADD', 'Hagyjon Hozzászólást');
+define('THEME_COMMENT_OPTIONS', 'Hozzászólás Nézet Opciói');
 
-define('THEME_PERMISSIONS_LEGEND', 'Permissions &amp; Legend');
+define('THEME_PERMISSIONS_LEGEND', 'Engedélyek &amp; Jelmagyarázat');
 
-define('THEME_INBOX_ALERT', 'PM Alert');
-define('THEME_INBOX_LINK', 'Click here to view your Inbox!');
+define('THEME_INBOX_ALERT', 'Privát Üzenet Értesítés');
+define('THEME_INBOX_LINK', 'Kattintson ide a postaládája megtekintéséhez!');
 
-define('THEME_EVENT_DOWNLOADS', 'Event Downloads');
-define('THEME_FILE_NAME', 'File');
-define('THEME_FILE_SIZE', 'Size');
+define('THEME_EVENT_DOWNLOADS', 'Esemény Letöltések');
+define('THEME_FILE_NAME', 'Fájl');
+define('THEME_FILE_SIZE', 'Méret');
 
-define('THEME_CONTROL_NEXT', 'Next');
-define('THEME_CONTROL_PREVIOUS', 'Previous');
+define('THEME_CONTROL_NEXT', 'Következő');
+define('THEME_CONTROL_PREVIOUS', 'Előző');
 
 // Bloc online
-define('THEME_OL_MEMBERS', 'Members');
-define('THEME_OL_MEMBER', 'Member');
-define('THEME_OL_GUESTS', 'Guests');
-define('THEME_OL_GUEST', 'Guest');
+define('THEME_OL_MEMBERS', 'Tagok');
+define('THEME_OL_MEMBER', 'Tag');
+define('THEME_OL_GUESTS', 'Vendégek');
+define('THEME_OL_GUEST', 'Vendég');
