d11547d9e2e0e927a88e2772e6272ac2b31489bf
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:02:45 +0100

    Create hu_dlg.js

---
+++ core/2.5.12/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/paste/langs/hu_dlg.js
@@ -0,0 +1,5 @@
+tinyMCE.addI18n('hu.paste_dlg',{
+    "word_title":"Használd a Ctrl+V kombinációt a billentyűzeteden a szöveg ablakba illesztéséhez.",
+    "text_linebreaks":"Sortörések Megtartása",
+    "text_title":"Használd a Ctrl+V kombinációt a billentyűzeteden a szöveg ablakba illesztéséhez."
+});
