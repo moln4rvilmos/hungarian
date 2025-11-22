1194bde979b7b64d2708d4bb813e71e9fbc12f3a
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 09:02:22 +0100

    Update userrank.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/admin/userrank.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/admin/userrank.php
@@ -2,34 +2,35 @@
 /**
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
  * @license         GNU GPL 2.0 or later (https://www.gnu.org/licenses/gpl-2.0.html)
- * _LANGCODE    en
- * _CHARSET     UTF-8
+ * _LANGCODE        hu
+ * _CHARSET         UTF-8
+ * Translator: Molnár Vilmos (XOOPS Translation Team)
  */
 //Nav
-define('_AM_SYSTEM_USERRANK_NAV_MANAGER', 'Ranks Manager');
-define('_AM_SYSTEM_USERRANK_NAV_MAIN', 'Ranks list');
-define('_AM_SYSTEM_USERRANK_NAV_ADD', 'Add a new rank');
-define('_AM_SYSTEM_USERRANK_NAV_EDIT', 'Edit rank');
-define('_AM_SYSTEM_USERRANK_NAV_DELETE', 'Delete rank');
-define('_AM_SYSTEM_USERRANK_ACTION', 'Action');
-define('_AM_SYSTEM_USERRANK_ADD', 'Add a new rank');
-define('_AM_SYSTEM_USERRANK_DELETE', 'Delete rank');
-define('_AM_SYSTEM_USERRANK_EDIT', 'Edit rank');
-define('_AM_SYSTEM_USERRANK_IMAGE', 'Image');
-define('_AM_SYSTEM_USERRANK_IMAGE_PATH', 'Files exist in: %s');
-define('_AM_SYSTEM_USERRANK_MAXPOST', 'Max. Posts');
-define('_AM_SYSTEM_USERRANK_MINPOST', 'Min. Posts');
-define('_AM_SYSTEM_USERRANK_OFF', 'Special rank');
-define('_AM_SYSTEM_USERRANK_ON', 'No special rank');
+define('_AM_SYSTEM_USERRANK_NAV_MANAGER', 'Rang Kezelő');
+define('_AM_SYSTEM_USERRANK_NAV_MAIN', 'Rangok listája');
+define('_AM_SYSTEM_USERRANK_NAV_ADD', 'Új rang hozzáadása');
+define('_AM_SYSTEM_USERRANK_NAV_EDIT', 'Rang szerkesztése');
+define('_AM_SYSTEM_USERRANK_NAV_DELETE', 'Rang törlése');
+define('_AM_SYSTEM_USERRANK_ACTION', 'Művelet');
+define('_AM_SYSTEM_USERRANK_ADD', 'Új rang hozzáadása');
+define('_AM_SYSTEM_USERRANK_DELETE', 'Rang törlése');
+define('_AM_SYSTEM_USERRANK_EDIT', 'Rang szerkesztése');
+define('_AM_SYSTEM_USERRANK_IMAGE', 'Kép');
+define('_AM_SYSTEM_USERRANK_IMAGE_PATH', 'A fájlok itt találhatók: %s');
+define('_AM_SYSTEM_USERRANK_MAXPOST', 'Max. Bejegyzés');
+define('_AM_SYSTEM_USERRANK_MINPOST', 'Min. Bejegyzés');
+define('_AM_SYSTEM_USERRANK_OFF', 'Speciális rang');
+define('_AM_SYSTEM_USERRANK_ON', 'Nem speciális rang');
 define('_AM_SYSTEM_USERRANK_SAVE', _AM_SYSTEM_DBUPDATED);
-define('_AM_SYSTEM_USERRANK_SPECIAL', 'Special Ranks');
-define('_AM_SYSTEM_USERRANK_SPECIAL_CAN', '(Special ranks can be assigned to users irrespective of the number of user posts)');
-define('_AM_SYSTEM_USERRANK_SUREDEL', 'Are you sure you want to delete this ranking?');
-define('_AM_SYSTEM_USERRANK_TITLE', 'Title');
-define('_AM_SYSTEM_USERRANK_UPLOAD', 'Upload');
+define('_AM_SYSTEM_USERRANK_SPECIAL', 'Speciális Rangok');
+define('_AM_SYSTEM_USERRANK_SPECIAL_CAN', '(A speciális rangok a felhasználói bejegyzések számától függetlenül is hozzárendelhetők a felhasználókhoz)');
+define('_AM_SYSTEM_USERRANK_SUREDEL', 'Biztosan törölni szeretnéd ezt a rangot?');
+define('_AM_SYSTEM_USERRANK_TITLE', 'Cím');
+define('_AM_SYSTEM_USERRANK_UPLOAD', 'Feltöltés');
 // Tips
-define('_AM_SYSTEM_USERRANK_TIPS', '<ul><li>Add, update or delete user rank</li></ul>');
-define('_AM_SYSTEM_USERRANK_TIPS_FORM1', '<ul><li>Authorized mime types: %s</li>');
-define('_AM_SYSTEM_USERRANK_TIPS_FORM2', '<li>Max uploaded files size: %s KB</li></ul>');
+define('_AM_SYSTEM_USERRANK_TIPS', '<ul><li>Felhasználói rang hozzáadása, frissítése vagy törlése</li></ul>');
+define('_AM_SYSTEM_USERRANK_TIPS_FORM1', '<ul><li>Engedélyezett mime típusok: %s</li>');
+define('_AM_SYSTEM_USERRANK_TIPS_FORM2', '<li>Feltöltött fájlok maximális mérete: %s KB</li></ul>');
 // Errors
-define('_AM_SYSTEM_USERRANK_ERROR', 'Errors');
+define('_AM_SYSTEM_USERRANK_ERROR', 'Hibák');
