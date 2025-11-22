a2b90d4191360a8652c4c46365eca2bae2c475cd
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:53:49 +0100

    Update groups.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/admin/groups.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/admin/groups.php
@@ -1,33 +1,33 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-// %%%%%%    Admin Module Name  AdminGroup     %%%%%
+// Translator: Molnár Vilmos (XOOPS Translation Team)
+// %%%%%%     Admin Module Name  AdminGroup      %%%%%
 //Nav
-define('_AM_SYSTEM_GROUPS_NAV_MANAGER', 'Groups Manager');
-define('_AM_SYSTEM_GROUPS_NAV_LIST', 'Groups list');
-define('_AM_SYSTEM_GROUPS_NAV_ADD', 'Add a new group');
-define('_AM_SYSTEM_GROUPS_NAV_EDIT', 'Edit group');
-define('_AM_SYSTEM_GROUPS_NAV_DELETE', 'Delete group');
+define('_AM_SYSTEM_GROUPS_NAV_MANAGER', 'Csoportok Kezelése');
+define('_AM_SYSTEM_GROUPS_NAV_LIST', 'Csoportok listája');
+define('_AM_SYSTEM_GROUPS_NAV_ADD', 'Új csoport hozzáadása');
+define('_AM_SYSTEM_GROUPS_NAV_EDIT', 'Csoport szerkesztése');
+define('_AM_SYSTEM_GROUPS_NAV_DELETE', 'Csoport törlése');
 // Tips
-define('_AM_SYSTEM_GROUPS_NAV_TIPS_1', '<ul><li>Create a new group with their own permissions.</li><li>Edit group for change permissions.</li></ul>');
-define('_AM_SYSTEM_GROUPS_NAV_TIPS_2', '<ul><li>Change or create permission for this group, all modification will affect users of this group.</li></ul>');
+define('_AM_SYSTEM_GROUPS_NAV_TIPS_1', '<ul><li>Hozz létre egy új csoportot saját jogosultságokkal.</li><li>Szerkeszd a csoportot a jogosultságok megváltoztatásához.</li></ul>');
+define('_AM_SYSTEM_GROUPS_NAV_TIPS_2', '<ul><li>Módosítsd vagy hozd létre az engedélyeket ehhez a csoporthoz, minden módosítás hatással lesz a csoport felhasználóira.</li></ul>');
 //Infos
-define('_AM_SYSTEM_GROUPS_ACCESSRIGHTS', 'Module Access rights');
-define('_AM_SYSTEM_GROUPS_ACTION', 'Action');
-define('_AM_SYSTEM_GROUPS_ACTIVERIGHTS', 'Module Admin rights');
-define('_AM_SYSTEM_GROUPS_ADD', 'Add a new group');
-define('_AM_SYSTEM_GROUPS_BLOCKRIGHTS', 'Block Access Rights');
-define('_AM_SYSTEM_GROUPS_CUSTOMBLOCK', 'Custom Block');
-define('_AM_SYSTEM_GROUPS_DELETE', 'Delete group');
-define('_AM_SYSTEM_GROUPS_DESCRIPTION', 'Group Description');
-define('_AM_SYSTEM_GROUPS_EDIT', 'Edit group');
-define('_AM_SYSTEM_GROUPS_ERROR_DELETE', 'You can not remove this group');
+define('_AM_SYSTEM_GROUPS_ACCESSRIGHTS', 'Modul Hozzáférési Jogok');
+define('_AM_SYSTEM_GROUPS_ACTION', 'Művelet');
+define('_AM_SYSTEM_GROUPS_ACTIVERIGHTS', 'Modul Adminisztrációs Jogok');
+define('_AM_SYSTEM_GROUPS_ADD', 'Új csoport hozzáadása');
+define('_AM_SYSTEM_GROUPS_BLOCKRIGHTS', 'Blokk Hozzáférési Jogok');
+define('_AM_SYSTEM_GROUPS_CUSTOMBLOCK', 'Egyedi Blokk');
+define('_AM_SYSTEM_GROUPS_DELETE', 'Csoport törlése');
+define('_AM_SYSTEM_GROUPS_DESCRIPTION', 'Csoport Leírása');
+define('_AM_SYSTEM_GROUPS_EDIT', 'Csoport szerkesztése');
+define('_AM_SYSTEM_GROUPS_ERROR_DELETE', 'Ezt a csoportot nem távolíthatod el');
 define('_AM_SYSTEM_GROUPS_ID', 'ID');
-define('_AM_SYSTEM_GROUPS_NAME', 'Group Name');
-define('_AM_SYSTEM_GROUPS_NB_USERS_BY_GROUPS', 'Number users by group');
-define('_AM_SYSTEM_GROUPS_NB_USERS_BY_GROUPS_USERS', '%s user(s)');
-define('_AM_SYSTEM_GROUPS_SUREDEL', 'Are you sure you want to delete this group?');
-define('_AM_SYSTEM_GROUPS_SYSTEMRIGHTS', 'System Admin rights');
+define('_AM_SYSTEM_GROUPS_NAME', 'Csoport Neve');
+define('_AM_SYSTEM_GROUPS_NB_USERS_BY_GROUPS', 'Felhasználók száma csoportonként');
+define('_AM_SYSTEM_GROUPS_NB_USERS_BY_GROUPS_USERS', '%s felhasználó');
+define('_AM_SYSTEM_GROUPS_SUREDEL', 'Biztosan törölni szeretnéd ezt a csoportot?');
+define('_AM_SYSTEM_GROUPS_SYSTEMRIGHTS', 'Rendszer Adminisztrációs Jogok');
 define('_AM_SYSTEM_GROUPS_DBUPDATED', _AM_SYSTEM_DBUPDATED);
