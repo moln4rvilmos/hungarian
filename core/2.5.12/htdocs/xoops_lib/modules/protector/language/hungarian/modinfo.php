44e5cc24538e51d60645b47ecc90a963bb36d6ac
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 09:40:49 +0100

    Update modinfo.php

--- core/2.5.12/htdocs/xoops_lib/modules/protector/language/hungarian/modinfo.php
+++ core/2.5.12/htdocs/xoops_lib/modules/protector/language/hungarian/modinfo.php
@@ -12,116 +12,116 @@ if (defined('FOR_XOOPS_LANG_CHECKER') || !defined($constpref . '_LOADED')) {
     define($constpref . '_NAME', 'Protector');
 
     // A brief description of this module
-    define($constpref . '_DESC', 'This module protects your XOOPS site from various attacks like DoS, SQL Injection, and Variables contamination.');
+    define($constpref . '_DESC', 'Ez a modul megvédi a XOOPS webhelyet különféle támadásoktól, mint például a DoS, SQL Injection és Változó szennyezés.');
 
     // Menu
-    define($constpref . '_ADMININDEX', 'Protector Center');
-    define($constpref . '_ADVISORY', 'Security Advisory');
-    define($constpref . '_PREFIXMANAGER', 'Prefix Manager');
-    define($constpref . '_ADMENU_MYBLOCKSADMIN', 'Permissions');
+    define($constpref . '_ADMININDEX', 'Protector Központ');
+    define($constpref . '_ADVISORY', 'Biztonsági Tanácsadás');
+    define($constpref . '_PREFIXMANAGER', 'Előtag (Prefix) Kezelő');
+    define($constpref . '_ADMENU_MYBLOCKSADMIN', 'Jogosultságok');
 
     // Configs
-    define($constpref . '_GLOBAL_DISBL', 'Temporary disabled');
-    define($constpref . '_GLOBAL_DISBLDSC', 'All protections are disabled in temporary.<br>Don\'t forget turn this off after shooting the trouble');
+    define($constpref . '_GLOBAL_DISBL', 'Ideiglenesen letiltva');
+    define($constpref . '_GLOBAL_DISBLDSC', 'Minden védelem ideiglenesen le van tiltva.<br>Ne felejtsd el kikapcsolni, miután a hibát elhárítottad');
 
-    define($constpref . '_DEFAULT_LANG', 'Default language');
-    define($constpref . '_DEFAULT_LANGDSC', 'Specify the language set to display messages before processing common.php');
+    define($constpref . '_DEFAULT_LANG', 'Alapértelmezett nyelv');
+    define($constpref . '_DEFAULT_LANGDSC', 'Add meg a nyelv beállítást, amely a common.php feldolgozása előtt megjelenő üzeneteket jeleníti meg');
 
-    define($constpref . '_RELIABLE_IPS', 'Reliable IPs');
-    define($constpref . '_RELIABLE_IPSDSC', 'set IPs you can rely separated with | . ^ matches the head of string, $ matches the tail of string.');
+    define($constpref . '_RELIABLE_IPS', 'Megbízható IP-k');
+    define($constpref . '_RELIABLE_IPSDSC', 'Állítsd be a megbízható IP-ket, | jellel elválasztva. A ^ megegyezik a sztring elejével, a $ a sztring végével.');
 
-    define($constpref . '_LOG_LEVEL', 'Logging level');
+    define($constpref . '_LOG_LEVEL', 'Naplózási szint');
     define($constpref . '_LOG_LEVELDSC', '');
 
-    define($constpref . '_BANIP_TIME0', 'Banned IP suspension time (sec)');
+    define($constpref . '_BANIP_TIME0', 'Tiltott IP felfüggesztési idő (másodperc)');
 
-    define($constpref . '_LOGLEVEL0', 'none');
-    define($constpref . '_LOGLEVEL15', 'Quiet');
-    define($constpref . '_LOGLEVEL63', 'quiet');
-    define($constpref . '_LOGLEVEL255', 'full');
+    define($constpref . '_LOGLEVEL0', 'nincs');
+    define($constpref . '_LOGLEVEL15', 'Csendes');
+    define($constpref . '_LOGLEVEL63', 'csendes');
+    define($constpref . '_LOGLEVEL255', 'teljes');
 
-    define($constpref . '_HIJACK_TOPBIT', 'Protected IP bits for the session');
-    define($constpref . '_HIJACK_TOPBITDSC', 'Anti Session Hi-Jacking:<br>Default 24/56 (netmask for IPV4/IPV6). (All bits are protected)<br>When your IP is not stable, set the IP range by number of the bits.<br>(eg) If your IP can move in the range of 192.168.0.0-192.168.0.255, set 24(bit) here');
-    define($constpref . '_HIJACK_DENYGP', 'Groups disallowed IP moving in a session');
-    define($constpref . '_HIJACK_DENYGPDSC', 'Anti Session Hi-Jacking:<br>Select groups which is disallowed to move their IP in a session.<br>(I recommend to turn Administrator on.)');
-    define($constpref . '_SAN_NULLBYTE', 'Sanitizing null-bytes');
-    define($constpref . '_SAN_NULLBYTEDSC', 'The terminating character "\\0" is often used in malicious attacks.<br>a null-byte will be changed to a space.<br>(highly recommended as On)');
-    define($constpref . '_DIE_NULLBYTE', 'Exit if null bytes are found');
-    define($constpref . '_DIE_NULLBYTEDSC', 'The terminating character "\\0" is often used in malicious attacks.<br>(highly recommended as On)');
-    define($constpref . '_DIE_BADEXT', 'Exit if bad files are uploaded');
-    define($constpref . '_DIE_BADEXTDSC', 'If someone tries to upload files which have bad extensions like .php , this module exits your XOOPS.<br>If you often attach php files into B-Wiki or PukiWikiMod, turn this off.');
-    define($constpref . '_CONTAMI_ACTION', 'Action if a contamination is found');
-    define($constpref . '_CONTAMI_ACTIONDS', 'Select the action when someone tries to contaminate system global variables into your XOOPS.<br>(recommended option is blank screen)');
-    define($constpref . '_ISOCOM_ACTION', 'Action if an isolated comment-in is found');
-    define($constpref . '_ISOCOM_ACTIONDSC', 'Anti SQL Injection:<br>Select the action when an isolated "/*" is found.<br>"Sanitizing" means adding another "*/" in tail.<br>(recommended option is Sanitizing)');
-    define($constpref . '_UNION_ACTION', 'Action if a UNION is found');
-    define($constpref . '_UNION_ACTIONDSC', 'Anti SQL Injection:<br>Select the action when some syntax like UNION of SQL.<br>"Sanitizing" means changing "union" to "uni-on".<br>(recommended option is Sanitizing)');
-    define($constpref . '_ID_INTVAL', 'Force intval to variables like id');
-    define($constpref . '_ID_INTVALDSC', 'All requests named "*id" will be treated as integer.<br>This option protects you from some kind of XSS and SQL Injections.<br>Though I recommend to turn this option on, it can cause problems with some modules.');
-    define($constpref . '_FILE_DOTDOT', 'Protection from Directory Traversals');
-    define($constpref . '_FILE_DOTDOTDSC', 'It eliminates ".." from all requests looks like Directory Traversals');
+    define($constpref . '_HIJACK_TOPBIT', 'Védett IP bitek a munkamenethez');
+    define($constpref . '_HIJACK_TOPBITDSC', 'Munkamenet eltérítés elleni védelem (Anti Session Hi-Jacking):<br>Alapértelmezett 24/56 (netmaszk IPV4/IPV6-hoz). (Minden bit védett)<br>Ha az IP-címed nem stabil, állítsd be az IP-tartományt a bitek számával.<br>(pl.) Ha az IP-d 192.168.0.0-192.168.0.255 tartományban mozoghat, állítsd 24-re (bit) itt');
+    define($constpref . '_HIJACK_DENYGP', 'Csoportok, amelyeknek nem engedélyezett az IP-cím változtatása egy munkamenetben');
+    define($constpref . '_HIJACK_DENYGPDSC', 'Munkamenet eltérítés elleni védelem:<br>Válaszd ki azokat a csoportokat, amelyeknek nem engedélyezett az IP-címük megváltoztatása egy munkamenetben.<br>(Javaslom, hogy az Adminisztrátort kapcsold be.)');
+    define($constpref . '_SAN_NULLBYTE', 'Null-bájtok szanálása (tisztítása)');
+    define($constpref . '_SAN_NULLBYTEDSC', 'A lezáró "\\0" karaktert gyakran használják rosszindulatú támadásoknál.<br>A null-bájt szóközre változik.<br>(Erősen ajánlott bekapcsolva tartani)');
+    define($constpref . '_DIE_NULLBYTE', 'Kilépés, ha null-bájtot talál');
+    define($constpref . '_DIE_NULLBYTEDSC', 'A lezáró "\\0" karaktert gyakran használják rosszindulatú támadásoknál.<br>(Erősen ajánlott bekapcsolva tartani)');
+    define($constpref . '_DIE_BADEXT', 'Kilépés, ha rossz fájl kiterjesztésű fájlokat töltenek fel');
+    define($constpref . '_DIE_BADEXTDSC', 'Ha valaki megpróbál rossz kiterjesztésű fájlokat feltölteni, mint pl. .php, ez a modul kilép a XOOPS-ból.<br>Ha gyakran csatolsz php fájlokat a B-Wiki-hez vagy PukiWikiMod-hoz, kapcsold ki.');
+    define($constpref . '_CONTAMI_ACTION', 'Művelet, ha változó szennyeződést talál');
+    define($constpref . '_CONTAMI_ACTIONDS', 'Válaszd ki a műveletet, ha valaki megpróbálja szennyezni a rendszer globális változóit a XOOPS-odban.<br>(Az ajánlott opció az üres képernyő)');
+    define($constpref . '_ISOCOM_ACTION', 'Művelet, ha elszigetelt komment-kezdetet talál');
+    define($constpref . '_ISOCOM_ACTIONDSC', 'Anti SQL Injection:<br>Válaszd ki a műveletet, ha elszigetelt "/*" található.<br>A "Szanálás" azt jelenti, hogy hozzáad egy másik "*/"-t a végén.<br>(Az ajánlott opció a Szanálás)');
+    define($constpref . '_UNION_ACTION', 'Művelet, ha UNION-t talál');
+    define($constpref . '_UNION_ACTIONDSC', 'Anti SQL Injection:<br>Válaszd ki a műveletet, ha SQL-szerű UNION szintaxist talál.<br>A "Szanálás" azt jelenti, hogy "union"-t "uni-on"-ra cserél.<br>(Az ajánlott opció a Szanálás)');
+    define($constpref . '_ID_INTVAL', 'Kényszerített intval (egész számmá alakítás) változókra, mint pl. id');
+    define($constpref . '_ID_INTVALDSC', 'Minden "*id" nevű kérés egész számként lesz kezelve.<br>Ez az opció védelmet nyújt néhány XSS és SQL Injection ellen.<br>Bár javaslom ennek az opciónak a bekapcsolását, problémákat okozhat néhány modulnál.');
+    define($constpref . '_FILE_DOTDOT', 'Védelem a Könyvtár Bejárás (Directory Traversal) ellen');
+    define($constpref . '_FILE_DOTDOTDSC', 'Eltávolítja a ".." karaktert minden olyan kérésből, amely Könyvtár Bejárásnak tűnik');
 
-    define($constpref . '_BF_COUNT', 'Anti Brute Force');
-    define($constpref . '_BF_COUNTDSC', "Set the maximum number of times a guest is allowed to try and login within 10 minutes. If the failed attempts to login exceed this, the guest's IP address will be banned.");
+    define($constpref . '_BF_COUNT', 'Anti Brute Force (Brutális Erő elleni védelem)');
+    define($constpref . '_BF_COUNTDSC', "Állítsd be, hányszor próbálkozhat egy vendég bejelentkezni 10 percen belül. Ha a sikertelen kísérletek meghaladják ezt a számot, a vendég IP-címe tiltva lesz.");
 
-    define($constpref . '_BWLIMIT_COUNT', 'Bandwidth limitation');
-    define($constpref . '_BWLIMIT_COUNTDSC', 'Specify the max access to mainfile.php during watching time. This value should be 0 for normal environments which have enough CPU bandwidth. The number fewer than 10 will be ignored.');
+    define($constpref . '_BWLIMIT_COUNT', 'Sávszélesség korlátozás');
+    define($constpref . '_BWLIMIT_COUNTDSC', 'Add meg a mainfile.php maximális hozzáférését a figyelési idő alatt. Ennek az értéknek 0-nak kell lennie a normál környezetekben, ahol elegendő a CPU sávszélesség. A 10-nél kisebb számokat a rendszer figyelmen kívül hagyja.');
 
-    define($constpref . '_DOS_SKIPMODS', 'Modules out of DoS/Crawler checker');
-    define($constpref . '_DOS_SKIPMODSDSC', 'set the dirnames of the modules separated with |. This option will be useful with chatting module etc.');
+    define($constpref . '_DOS_SKIPMODS', 'Modulok a DoS/Crawler ellenőrzőn kívül');
+    define($constpref . '_DOS_SKIPMODSDSC', 'Állítsd be a modulok könyvtárnevét | jellel elválasztva. Ez az opció hasznos lehet csevegő moduloknál, stb.');
 
-    define($constpref . '_DOS_EXPIRE', 'Watch time for high loadings (sec)');
-    define($constpref . '_DOS_EXPIREDSC', 'This value specifies the watch time for high-frequent reloading (F5 attack) and high loading crawlers.');
+    define($constpref . '_DOS_EXPIRE', 'Figyelési idő magas terhelések esetén (másodperc)');
+    define($constpref . '_DOS_EXPIREDSC', 'Ez az érték meghatározza a gyakori újratöltések (F5 támadás) és a magas terhelést okozó crawlerek figyelési idejét.');
 
-    define($constpref . '_DOS_F5COUNT', 'Bad counts for F5 Attack');
-    define($constpref . '_DOS_F5COUNTDSC', 'Preventing from DoS attacks.<br>This value specifies the reloading counts to be considered as a malicious attack.');
-    define($constpref . '_DOS_F5ACTION', 'Action against F5 Attack');
+    define($constpref . '_DOS_F5COUNT', 'Hibás szám az F5 Támadásra');
+    define($constpref . '_DOS_F5COUNTDSC', 'DoS támadások megelőzése.<br>Ez az érték határozza meg az újratöltési számot, amely már rosszindulatú támadásnak minősül.');
+    define($constpref . '_DOS_F5ACTION', 'Művelet F5 Támadás ellen');
 
-    define($constpref . '_DOS_CRCOUNT', 'Bad counts for Crawlers');
-    define($constpref . '_DOS_CRCOUNTDSC', 'Preventing from high loading crawlers.<br>This value specifies the access counts to be considered as a bad-manner crawler.');
-    define($constpref . '_DOS_CRACTION', 'Action against high loading Crawlers');
+    define($constpref . '_DOS_CRCOUNT', 'Hibás szám a Crawlerekre (robotokra)');
+    define($constpref . '_DOS_CRCOUNTDSC', 'Magas terhelést okozó crawlerek megelőzése.<br>Ez az érték határozza meg a hozzáférések számát, amelyet rosszindulatú crawlernek tekintünk.');
+    define($constpref . '_DOS_CRACTION', 'Művelet a magas terhelést okozó Crawlerek ellen');
 
-    define($constpref . '_DOS_CRSAFE', 'Welcomed User-Agent');
-    define($constpref . '_DOS_CRSAFEDSC', 'A perl regex pattern for User-Agent.<br>If it matches, the crawler is never considered as a high loading crawler.<br>eg) /(bingbot|Googlebot|Yahoo! Slurp)/i');
+    define($constpref . '_DOS_CRSAFE', 'Üdvözölt User-Agent');
+    define($constpref . '_DOS_CRSAFEDSC', 'Egy perl regex minta a User-Agent-hez.<br>Ha megegyezik, a crawler sosem minősül magas terhelést okozó crawlernek.<br>pl) /(bingbot|Googlebot|Yahoo! Slurp)/i');
 
-    define($constpref . '_OPT_NONE', 'None (only logging)');
-    define($constpref . '_OPT_SAN', 'Sanitizing');
-    define($constpref . '_OPT_EXIT', 'Blank Screen');
-    define($constpref . '_OPT_BIP', 'Ban the IP (No limit)');
-    define($constpref . '_OPT_BIPTIME0', 'Ban the IP (moratorium)');
+    define($constpref . '_OPT_NONE', 'Nincs (csak naplózás)');
+    define($constpref . '_OPT_SAN', 'Szanálás (Tisztítás)');
+    define($constpref . '_OPT_EXIT', 'Üres Képernyő');
+    define($constpref . '_OPT_BIP', 'IP letiltása (korlátlanul)');
+    define($constpref . '_OPT_BIPTIME0', 'IP letiltása (moratóriummal)');
 
-    define($constpref . '_DOSOPT_NONE', 'None (only logging)');
-    define($constpref . '_DOSOPT_SLEEP', 'Sleep');
-    define($constpref . '_DOSOPT_EXIT', 'Blank Screen');
-    define($constpref . '_DOSOPT_BIP', 'Ban the IP (No limit)');
-    define($constpref . '_DOSOPT_BIPTIME0', 'Ban the IP (moratorium)');
-    define($constpref . '_DOSOPT_HTA', 'DENY by .htaccess(Experimental)');
+    define($constpref . '_DOSOPT_NONE', 'Nincs (csak naplózás)');
+    define($constpref . '_DOSOPT_SLEEP', 'Alvás (Sleep)');
+    define($constpref . '_DOSOPT_EXIT', 'Üres Képernyő');
+    define($constpref . '_DOSOPT_BIP', 'IP letiltása (korlátlanul)');
+    define($constpref . '_DOSOPT_BIPTIME0', 'IP letiltása (moratóriummal)');
+    define('define(_DOSOPT_HTA', 'DENY by .htaccess (Kísérleti)');
 
-    define($constpref . '_BIP_EXCEPT', 'Groups never registered as Bad IP');
-    define($constpref . '_BIP_EXCEPTDSC', 'A user who belongs to the group specified here will never be banned.<br>(I recommend to turn Administrator on.)');
+    define($constpref . '_BIP_EXCEPT', 'Csoportok, amelyek soha nem kerülnek Rossz IP-ként regisztrálásra');
+    define($constpref . '_BIP_EXCEPTDSC', 'Az itt megadott csoporthoz tartozó felhasználó soha nem lesz letiltva.<br>(Javaslom, hogy az Adminisztrátort kapcsold be.)');
 
-    define($constpref . '_DISABLES', 'Disable dangerous features in XOOPS');
+    define($constpref . '_DISABLES', 'Veszélyes funkciók letiltása a XOOPS-ban');
 
-    define($constpref . '_DBLAYERTRAP', 'Enable DB Layer trapping anti-SQL-Injection');
-    define($constpref . '_DBLAYERTRAPDSC', 'Almost SQL Injection attacks will be canceled by this feature. This feature is required a support from databasefactory. You can check it on Security Advisory page. This setting must be on. Never turn it off casually.');
-    define($constpref . '_DBTRAPWOSRV', 'Never checking _SERVER for anti-SQL-Injection');
-    define($constpref . '_DBTRAPWOSRVDSC', 'Some servers always enable DB Layer trapping. It causes wrong detections as SQL Injection attack. If you got such errors, turn this option on. You should know this option weakens the security of DB Layer trapping anti-SQL-Injection.');
+    define($constpref . '_DBLAYERTRAP', 'DB Réteg csapdázás (trapping) engedélyezése az anti-SQL-Injection-höz');
+    define($constpref . '_DBLAYERTRAPDSC', 'Szinte minden SQL Injection támadás megszűnik ezzel a funkcióval. Ez a funkció támogatást igényel a databasefactory-tól. Ezt ellenőrizheted a Biztonsági Tanácsadás oldalon. Ennek a beállításnak bekapcsolva kell lennie. Soha ne kapcsold ki könnyelműen.');
+    define($constpref . '_DBTRAPWOSRV', 'Soha ne ellenőrizze a _SERVER-t az anti-SQL-Injection-höz');
+    define($constpref . '_DBTRAPWOSRVDSC', 'Néhány szerver mindig engedélyezi a DB Layer trapping-et. Ez hibás észlelést okoz SQL Injection támadásként. Ha ilyen hibákat tapasztalsz, kapcsold be ezt az opciót. Tudnod kell, hogy ez az opció gyengíti a DB Layer trapping anti-SQL-Injection biztonságát.');
 
-    define($constpref . '_BIGUMBRELLA', 'enable anti-XSS (BigUmbrella)');
-    define($constpref . '_BIGUMBRELLADSC', 'This protects you from almost attacks via XSS vulnerabilities. But it is not 100%');
+    define($constpref . '_BIGUMBRELLA', 'anti-XSS engedélyezése (BigUmbrella)');
+    define($constpref . '_BIGUMBRELLADSC', 'Ez megvéd szinte minden támadástól XSS sebezhetőségeken keresztül. De nem 100%-os.');
 
-    define($constpref . '_SPAMURI4U', 'anti-SPAM: URLs for normal users');
-    define($constpref . '_SPAMURI4UDSC', 'If this number of URLs are found in POST data from users other than admin, the POST is considered as SPAM. 0 means disabling this feature.');
-    define($constpref . '_SPAMURI4G', 'anti-SPAM: URLs for guests');
-    define($constpref . '_SPAMURI4GDSC', 'If this number of URLs are found in POST data from guests, the POST is considered as SPAM. 0 means disabling this feature.');
+    define($constpref . '_SPAMURI4U', 'anti-SPAM: URL-ek normál felhasználók számára');
+    define($constpref . '_SPAMURI4UDSC', 'Ha ennyi URL-t találnak a POST adatokban az adminisztrátoron kívüli felhasználóktól, a POST SPAM-nek minősül. A 0 azt jelenti, hogy a funkció ki van kapcsolva.');
+    define($constpref . '_SPAMURI4G', 'anti-SPAM: URL-ek vendégek számára');
+    define($constpref . '_SPAMURI4GDSC', 'Ha ennyi URL-t találnak a POST adatokban vendégektől, a POST SPAM-nek minősül. A 0 azt jelenti, hogy a funkció ki van kapcsolva.');
 
     //3.40b
-    define($constpref . '_ADMINHOME', 'Home');
-    define($constpref . '_ADMINABOUT', 'About');
+    define($constpref . '_ADMINHOME', 'Kezdőlap');
+    define($constpref . '_ADMINABOUT', 'Rólunk');
     //3.50
     define($constpref . '_STOPFORUMSPAM_ACTION', 'Stop Forum Spam');
-    define($constpref . '_STOPFORUMSPAM_ACTIONDSC', 'Checks POST data against spammers registered on www.stopforumspam.com database. Requires php CURL lib.');
+    define($constpref . '_STOPFORUMSPAM_ACTIONDSC', 'Ellenőrzi a POST adatokat a www.stopforumspam.com adatbázisban regisztrált spammerek ellen. Szükséges hozzá a php CURL könyvtár.');
     // 3.60
-    define($constpref . '_ADMINSTATS', 'Overview');
-    define($constpref . '_BANIP_TIME0DSC', 'Suspension time in seconds for automatic IP bans');
+    define($constpref . '_ADMINSTATS', 'Áttekintés');
+    define($constpref . '_BANIP_TIME0DSC', 'Felfüggesztési idő másodpercben az automatikus IP tiltásokhoz');
 }
