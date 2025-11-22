8c6663a455ae6d2cb193027b2168b14ebeb2da76
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:40:35 +0100

    Update modinfo.php

--- core/2.5.12/htdocs/modules/profile/language/hungarian/modinfo.php
+++ core/2.5.12/htdocs/modules/profile/language/hungarian/modinfo.php
@@ -1,47 +1,47 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_PROFILE_MI_NAME', 'User Profile');
-define('_PROFILE_MI_DESC', 'Module for managing custom user profile fields');
+// Translator: Molnár Vilmos (XOOPS Translation Team)
+define('_PROFILE_MI_NAME', 'Felhasználói Profil');
+define('_PROFILE_MI_DESC', 'Modul az egyedi felhasználói profil mezők kezelésére');
 //Main menu links
-define('_PROFILE_MI_EDITACCOUNT', 'Edit Profile');
-define('_PROFILE_MI_CHANGEPASS', 'Change Password');
-define('_PROFILE_MI_CHANGEMAIL', 'Change Email');
+define('_PROFILE_MI_EDITACCOUNT', 'Profil Szerkesztése');
+define('_PROFILE_MI_CHANGEPASS', 'Jelszó Megváltoztatása');
+define('_PROFILE_MI_CHANGEMAIL', 'E-mail Cím Megváltoztatása');
 //Admin links
 define('_PROFILE_MI_INDEX', 'Index');
-define('_PROFILE_MI_CATEGORIES', 'Categories');
-define('_PROFILE_MI_FIELDS', 'Fields');
-define('_PROFILE_MI_USERS', 'Users');
-define('_PROFILE_MI_STEPS', 'Registration Steps');
-define('_PROFILE_MI_PERMISSIONS', 'Permissions');
+define('_PROFILE_MI_CATEGORIES', 'Kategóriák');
+define('_PROFILE_MI_FIELDS', 'Mezők');
+define('_PROFILE_MI_USERS', 'Felhasználók');
+define('_PROFILE_MI_STEPS', 'Regisztrációs Lépések');
+define('_PROFILE_MI_PERMISSIONS', 'Jogosultságok');
 //User Profile Category
-define('_PROFILE_MI_CATEGORY_TITLE', 'User Profile');
-define('_PROFILE_MI_CATEGORY_DESC', 'For those user fields');
+define('_PROFILE_MI_CATEGORY_TITLE', 'Felhasználói Profil');
+define('_PROFILE_MI_CATEGORY_DESC', 'Ezeknek a felhasználói mezőknek');
 //User Profile Fields
-define('_PROFILE_MI_URL_TITLE', 'Website');
+define('_PROFILE_MI_URL_TITLE', 'Weboldal');
 //Configuration categories
-define('_PROFILE_MI_CAT_SETTINGS', 'General Settings');
+define('_PROFILE_MI_CAT_SETTINGS', 'Általános Beállítások');
 define('_PROFILE_MI_CAT_SETTINGS_DSC', '');
-define('_PROFILE_MI_CAT_USER', 'User Settings');
+define('_PROFILE_MI_CAT_USER', 'Felhasználói Beállítások');
 define('_PROFILE_MI_CAT_USER_DSC', '');
 //Configuration items
-define('_PROFILE_MI_PROFILE_SEARCH', 'Show latest activities on user profile');
+define('_PROFILE_MI_PROFILE_SEARCH', 'Legutóbbi tevékenységek mutatása a felhasználói profilon');
 //Pages
-define('_PROFILE_MI_PAGE_INFO', 'User Info');
-define('_PROFILE_MI_PAGE_EDIT', 'Edit User');
-define('_PROFILE_MI_PAGE_SEARCH', 'Search');
-define('_PROFILE_MI_STEP_BASIC', 'Basic');
-define('_PROFILE_MI_STEP_COMPLEMENTARY', 'Advanced');
-define('_PROFILE_MI_CATEGORY_PERSONAL', 'Personal');
-define('_PROFILE_MI_CATEGORY_MESSAGING', 'Messaging');
-define('_PROFILE_MI_CATEGORY_SETTINGS', 'Settings');
-define('_PROFILE_MI_CATEGORY_COMMUNITY', 'Community');
-define('_PROFILE_MI_NEVER_LOGGED_IN', 'Never logged in');
+define('_PROFILE_MI_PAGE_INFO', 'Felhasználói Adatok');
+define('_PROFILE_MI_PAGE_EDIT', 'Felhasználó Szerkesztése');
+define('_PROFILE_MI_PAGE_SEARCH', 'Keresés');
+define('_PROFILE_MI_STEP_BASIC', 'Alapvető');
+define('_PROFILE_MI_STEP_COMPLEMENTARY', 'Haladó');
+define('_PROFILE_MI_CATEGORY_PERSONAL', 'Személyes');
+define('_PROFILE_MI_CATEGORY_MESSAGING', 'Üzenetküldés');
+define('_PROFILE_MI_CATEGORY_SETTINGS', 'Beállítások');
+define('_PROFILE_MI_CATEGORY_COMMUNITY', 'Közösség');
+define('_PROFILE_MI_NEVER_LOGGED_IN', 'Soha nem jelentkezett be');
 //1.62
-define('_PROFILE_MI_ABOUT', 'About');
-define('_PROFILE_MI_HOME', 'Home');
+define('_PROFILE_MI_ABOUT', 'Névjegy');
+define('_PROFILE_MI_HOME', 'Kezdőlap');
 //1.86
-define('_PROFILE_MI_PROFILE_CAPTCHA_STEP1', 'Use Captcha after the second Registration step?');
-define('_PROFILE_MI_PROFILE_CAPTCHA_STEP1_DESC', "Select 'Yes' to add extra measure against Spam registration by bots");
+define('_PROFILE_MI_PROFILE_CAPTCHA_STEP1', 'Használjak Captcha-t a második regisztrációs lépés után?');
+define('_PROFILE_MI_PROFILE_CAPTCHA_STEP1_DESC', "Válaszd az 'Igen'-t a spambotok elleni védelem megerősítéséhez a regisztráció során");
