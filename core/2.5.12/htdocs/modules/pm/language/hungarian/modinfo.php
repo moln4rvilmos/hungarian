b23d0b7be98ffa91858aa7cda341e2666325caed
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:36:17 +0100

    Update modinfo.php

--- core/2.5.12/htdocs/modules/pm/language/hungarian/modinfo.php
+++ core/2.5.12/htdocs/modules/pm/language/hungarian/modinfo.php
@@ -1,24 +1,24 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_PM_MI_NAME', 'Private Messaging');
-define('_PM_MI_DESC', 'Module for private messaging between users');
-define('_PM_MI_INDEX', 'Home');
-define('_PM_MI_PRUNE', 'Prune Messages');
-define('_PM_MI_LINK_TITLE', 'PM Link');
-define('_PM_MI_LINK_DESCRIPTION', 'Shows a link to send a private message to the user');
-define('_PM_MI_MESSAGE', 'Write a message to');
-define('_PM_MI_PRUNESUBJECT', 'Prune PM subject line');
-define('_PM_MI_PRUNESUBJECT_DESC', 'This will be the subject of the PM to the user, received after a PM prune');
-define('_PM_MI_PRUNEMESSAGE', 'Prune PM body message');
-define('_PM_MI_PRUNEMESSAGE_DESC', "This message will be in the body of the message to users after one or more of their messages have been removed from their inbox during a PM prune. Use {PM_COUNT} in the text to be replaced with the number of messages removed from this user's inbox");
-define('_PM_MI_PRUNESUBJECTDEFAULT', 'Messages deleted during cleanup');
-define('_PM_MI_PRUNEMESSAGEDEFAULT', 'During a cleanup of the Private Messaging, XOOPS has deleted {PM_COUNT} messages from your inbox to save space and resources');
-define('_PM_MI_MAXSAVE', 'Maximum items in savebox');
+// Translator: Molnár Vilmos (XOOPS Translation Team)
+define('_PM_MI_NAME', 'Privát Üzenetküldés');
+define('_PM_MI_DESC', 'Modul privát üzenetek küldéséhez a felhasználók között');
+define('_PM_MI_INDEX', 'Kezdőlap');
+define('_PM_MI_PRUNE', 'Üzenetek Törlése/Takarítása');
+define('_PM_MI_LINK_TITLE', 'PM Hivatkozás');
+define('_PM_MI_LINK_DESCRIPTION', 'Megjelenít egy hivatkozást, amivel privát üzenetet küldhetsz a felhasználónak');
+define('_PM_MI_MESSAGE', 'Üzenet írása neki:');
+define('_PM_MI_PRUNESUBJECT', 'Törölt PM üzenet tárgya');
+define('_PM_MI_PRUNESUBJECT_DESC', 'Ez lesz annak a privát üzenetnek a tárgya, amit a felhasználó kap az üzenetek takarítása után');
+define('_PM_MI_PRUNEMESSAGE', 'Törölt PM üzenet törzse');
+define('_PM_MI_PRUNEMESSAGE_DESC', "Ez az üzenet lesz az üzenet törzsében, amit a felhasználók kapnak, miután egy vagy több üzenetük törölve lett a bejövő mappájukból a PM takarítás során. Használd a {PM_COUNT} helykitöltőt a törölt üzenetek számának beillesztéséhez.");
+define('_PM_MI_PRUNESUBJECTDEFAULT', 'Üzenetek törölve a tisztítás során');
+define('_PM_MI_PRUNEMESSAGEDEFAULT', 'A Privát Üzenetküldés tisztítása során a XOOPS törölt {PM_COUNT} üzenetet a bejövő mappádból, hogy helyet és erőforrást takarítson meg.');
+define('_PM_MI_MAXSAVE', 'Maximális mentett elemek száma a Mentett (Savebox) mappában');
 define('_PM_MI_MAXSAVE_DESC', '');
-define('_PM_MI_PERPAGE', 'Messages per page');
+define('_PM_MI_PERPAGE', 'Üzenetek száma oldalanként');
 define('_PM_MI_PERPAGE_DESC', '');
 //1.07
-define('_PM_MI_ABOUT', 'About');
+define('_PM_MI_ABOUT', 'Névjegy');
