23f9ebdb40c0fc1ba1f9e08998be14eb95f0a9ac
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:50:23 +0100

    Update banners.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/admin/banners.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/admin/banners.php
@@ -2,76 +2,77 @@
 /**
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
  * @license         GNU GPL 2.0 or later (https://www.gnu.org/licenses/gpl-2.0.html)
- * _LANGCODE    en
- * _CHARSET     UTF-8
+ * _LANGCODE        hu
+ * _CHARSET         UTF-8
+ * Translator: Molnár Vilmos (XOOPS Translation Team)
  */
-//%%%%%%        Admin Module Name  Banners         %%%%%
+//%%%%%%        Admin Module Name  Banners        %%%%%
 define('_AM_SYSTEM_BANNERS_DBUPDATED', _AM_SYSTEM_DBUPDATED);
 //Nav
-define('_AM_SYSTEM_BANNERS_NAV_MANAGER', 'Banners Management');
-define('_AM_SYSTEM_BANNERS_NAV_MAIN', 'Banners and clients list');
-define('_AM_SYSTEM_BANNERS_NAV_EDITBNR', 'Edit banner');
-define('_AM_SYSTEM_BANNERS_NAV_DELETEBNR', 'Delete banner');
-define('_AM_SYSTEM_BANNERS_NAV_DELETEFINISHBNR', 'Delete finish banner');
-define('_AM_SYSTEM_BANNERS_NAV_ADDBNR', 'Add a New Banner');
-define('_AM_SYSTEM_BANNERS_NAV_EDITADVCLI', 'Edit Advertising Client');
-define('_AM_SYSTEM_BANNERS_NAV_ADDNWCLI', 'Add a New Client');
-define('_AM_SYSTEM_BANNERS_NAV_DELETECLI', 'Delete Client');
-define('_AM_SYSTEM_BANNERS_CURACTBNR', 'Current Active Banners');
+define('_AM_SYSTEM_BANNERS_NAV_MANAGER', 'Bannerek Kezelése');
+define('_AM_SYSTEM_BANNERS_NAV_MAIN', 'Bannerek és ügyfelek listája');
+define('_AM_SYSTEM_BANNERS_NAV_EDITBNR', 'Banner szerkesztése');
+define('_AM_SYSTEM_BANNERS_NAV_DELETEBNR', 'Banner törlése');
+define('_AM_SYSTEM_BANNERS_NAV_DELETEFINISHBNR', 'Befejezett banner törlése');
+define('_AM_SYSTEM_BANNERS_NAV_ADDBNR', 'Új Banner Hozzáadása');
+define('_AM_SYSTEM_BANNERS_NAV_EDITADVCLI', 'Reklámügyfél szerkesztése');
+define('_AM_SYSTEM_BANNERS_NAV_ADDNWCLI', 'Új Ügyfél Hozzáadása');
+define('_AM_SYSTEM_BANNERS_NAV_DELETECLI', 'Ügyfél törlése');
+define('_AM_SYSTEM_BANNERS_CURACTBNR', 'Jelenlegi Aktív Bannerek');
 define('_AM_SYSTEM_BANNERS_BANNERID', 'Banner ID');
-define('_AM_SYSTEM_BANNERS_IMPRESION', 'Impressions');
-define('_AM_SYSTEM_BANNERS_IMPLEFT', 'Imp. Left');
-define('_AM_SYSTEM_BANNERS_CLICKS', 'Clicks');
-define('_AM_SYSTEM_BANNERS_NCLICKS', '% Clicks');
-define('_AM_SYSTEM_BANNERS_CLINAME', 'Client Name');
-define('_AM_SYSTEM_BANNERS_FUNCTION', 'Functions');
-define('_AM_SYSTEM_BANNERS_UNLIMIT', 'Unlimited');
-define('_AM_SYSTEM_BANNERS_VIEW', 'View banner');
-define('_AM_SYSTEM_BANNERS_EDIT', 'Edit');
-define('_AM_SYSTEM_BANNERS_DELETE', 'Delete');
-define('_AM_SYSTEM_BANNERS_FINISHBNR', 'Finished Banners');
-define('_AM_SYSTEM_BANNERS_IMPD', 'Imp.');
-define('_AM_SYSTEM_BANNERS_STARTDATE', 'Date Started');
-define('_AM_SYSTEM_BANNERS_ENDDATE', 'Date Ended');
-define('_AM_SYSTEM_BANNERS_ADVCLI', 'Advertising Clients');
-define('_AM_SYSTEM_BANNERS_ACTIVEBNR', 'Active Banners');
-define('_AM_SYSTEM_BANNERS_CONTNAME', 'Contact Name');
-define('_AM_SYSTEM_BANNERS_CONTMAIL', 'Contact Email');
-define('_AM_SYSTEM_BANNERS_CLINAMET', 'Client Name:');
-define('_AM_SYSTEM_BANNERS_ADDNWBNR', 'Add a New Banner');
-define('_AM_SYSTEM_BANNERS_IMPPURCHT', 'Impressions Purchased:');
-define('_AM_SYSTEM_BANNERS_IMGURLT', 'Image URL:');
-define('_AM_SYSTEM_BANNERS_CLICKURLT', 'Click URL:');
-define('_AM_SYSTEM_BANNERS_ADDBNR', 'Add Banner');
-define('_AM_SYSTEM_BANNERS_ADDNWCLI', 'Add a New Client');
-define('_AM_SYSTEM_BANNERS_CONTNAMET', 'Contact Name:');
-define('_AM_SYSTEM_BANNERS_CONTMAILT', 'Contact Email:');
-define('_AM_SYSTEM_BANNERS_CLILOGINT', 'Client Login:');
-define('_AM_SYSTEM_BANNERS_CLIPASST', 'Client Password:');
-define('_AM_SYSTEM_BANNERS_ADDCLI', 'Add Client');
-define('_AM_SYSTEM_BANNERS_DELEBNR', 'Delete Banner');
-define('_AM_SYSTEM_BANNERS_SUREDELE', 'Are you sure to delete this Banner?');
-define('_AM_SYSTEM_BANNERS_NO', 'No');
-define('_AM_SYSTEM_BANNERS_YES', 'Yes');
-define('_AM_SYSTEM_BANNERS_EDITBNR', 'Edit Banner');
-define('_AM_SYSTEM_BANNERS_ADDIMPT', 'Add More Impressions:');
-define('_AM_SYSTEM_BANNERS_PURCHT', 'Purchased:');
-define('_AM_SYSTEM_BANNERS_MADET', 'Made:');
-define('_AM_SYSTEM_BANNERS_CHGBNR', 'Change Banner');
-define('_AM_SYSTEM_BANNERS_DELEADC', 'Delete Advertising Client');
-define('_AM_SYSTEM_BANNERS_SUREDELCLI', 'You are about to delete client <strong>%s</strong> and all its Banners!!!');
-define('_AM_SYSTEM_BANNERS_NOBNRRUN', "The client doesn't have any banners running now.");
-define('_AM_SYSTEM_BANNERS_WARNING', 'WARNING!!!');
-define('_AM_SYSTEM_BANNERS_ACTBNRRUN', 'This client has the following ACTIVE BANNERS running on our site:');
-define('_AM_SYSTEM_BANNERS_SUREDELBNR', 'Are you sure you want to delete this Client and ALL its Banners?');
-define('_AM_SYSTEM_BANNERS_EDITADVCLI', 'Edit Advertising Client');
-define('_AM_SYSTEM_BANNERS_EXTINFO', 'Extra Info:');
-define('_AM_SYSTEM_BANNERS_CHGCLI', 'Change Client');
-define('_AM_SYSTEM_BANNERS_USEHTML', 'Use HTML code?');
-define('_AM_SYSTEM_BANNERS_CODEHTML', 'Enter HTML code:');
+define('_AM_SYSTEM_BANNERS_IMPRESION', 'Megjelenítések');
+define('_AM_SYSTEM_BANNERS_IMPLEFT', 'Hátra lévő Megj.');
+define('_AM_SYSTEM_BANNERS_CLICKS', 'Kattintások');
+define('_AM_SYSTEM_BANNERS_NCLICKS', 'Kattintások %-a');
+define('_AM_SYSTEM_BANNERS_CLINAME', 'Ügyfél Neve');
+define('_AM_SYSTEM_BANNERS_FUNCTION', 'Funkciók');
+define('_AM_SYSTEM_BANNERS_UNLIMIT', 'Korlátlan');
+define('_AM_SYSTEM_BANNERS_VIEW', 'Banner megtekintése');
+define('_AM_SYSTEM_BANNERS_EDIT', 'Szerkesztés');
+define('_AM_SYSTEM_BANNERS_DELETE', 'Törlés');
+define('_AM_SYSTEM_BANNERS_FINISHBNR', 'Befejezett Bannerek');
+define('_AM_SYSTEM_BANNERS_IMPD', 'Megj.');
+define('_AM_SYSTEM_BANNERS_STARTDATE', 'Kezdés Dátuma');
+define('_AM_SYSTEM_BANNERS_ENDDATE', 'Befejezés Dátuma');
+define('_AM_SYSTEM_BANNERS_ADVCLI', 'Reklámügyfelek');
+define('_AM_SYSTEM_BANNERS_ACTIVEBNR', 'Aktív Bannerek');
+define('_AM_SYSTEM_BANNERS_CONTNAME', 'Kapcsolattartó Neve');
+define('_AM_SYSTEM_BANNERS_CONTMAIL', 'Kapcsolattartó E-mail címe');
+define('_AM_SYSTEM_BANNERS_CLINAMET', 'Ügyfél Neve:');
+define('_AM_SYSTEM_BANNERS_ADDNWBNR', 'Új Banner Hozzáadása');
+define('_AM_SYSTEM_BANNERS_IMPPURCHT', 'Megvásárolt Megjelenítések:');
+define('_AM_SYSTEM_BANNERS_IMGURLT', 'Kép URL:');
+define('_AM_SYSTEM_BANNERS_CLICKURLT', 'Kattintás URL:');
+define('_AM_SYSTEM_BANNERS_ADDBNR', 'Banner Hozzáadása');
+define('_AM_SYSTEM_BANNERS_ADDNWCLI', 'Új Ügyfél Hozzáadása');
+define('_AM_SYSTEM_BANNERS_CONTNAMET', 'Kapcsolattartó Neve:');
+define('_AM_SYSTEM_BANNERS_CONTMAILT', 'Kapcsolattartó E-mail címe:');
+define('_AM_SYSTEM_BANNERS_CLILOGINT', 'Ügyfél Bejelentkezési Neve:');
+define('_AM_SYSTEM_BANNERS_CLIPASST', 'Ügyfél Jelszava:');
+define('_AM_SYSTEM_BANNERS_ADDCLI', 'Ügyfél Hozzáadása');
+define('_AM_SYSTEM_BANNERS_DELEBNR', 'Banner Törlése');
+define('_AM_SYSTEM_BANNERS_SUREDELE', 'Biztosan törölni szeretnéd ezt a Bannert?');
+define('_AM_SYSTEM_BANNERS_NO', 'Nem');
+define('_AM_SYSTEM_BANNERS_YES', 'Igen');
+define('_AM_SYSTEM_BANNERS_EDITBNR', 'Banner Szerkesztése');
+define('_AM_SYSTEM_BANNERS_ADDIMPT', 'További Megjelenítések Hozzáadása:');
+define('_AM_SYSTEM_BANNERS_PURCHT', 'Megvásárolva:');
+define('_AM_SYSTEM_BANNERS_MADET', 'Megtörtént:');
+define('_AM_SYSTEM_BANNERS_CHGBNR', 'Banner Megváltoztatása');
+define('_AM_SYSTEM_BANNERS_DELEADC', 'Reklámügyfél Törlése');
+define('_AM_SYSTEM_BANNERS_SUREDELCLI', 'Törölni készülsz a(z) <strong>%s</strong> ügyfelet és az összes hozzá tartozó Bannert!!!');
+define('_AM_SYSTEM_BANNERS_NOBNRRUN', 'Az ügyfélnek jelenleg nincs aktív bannere.');
+define('_AM_SYSTEM_BANNERS_WARNING', 'FIGYELMEZTETÉS!!!');
+define('_AM_SYSTEM_BANNERS_ACTBNRRUN', 'Ennek az ügyfélnek a következő AKTÍV BANNERJAI futnak az oldalunkon:');
+define('_AM_SYSTEM_BANNERS_SUREDELBNR', 'Biztosan törölni szeretnéd ezt az Ügyfelet és az ÖSSZES Bannerjét?');
+define('_AM_SYSTEM_BANNERS_EDITADVCLI', 'Reklámügyfél Szerkesztése');
+define('_AM_SYSTEM_BANNERS_EXTINFO', 'Extra Információ:');
+define('_AM_SYSTEM_BANNERS_CHGCLI', 'Ügyfél Megváltoztatása');
+define('_AM_SYSTEM_BANNERS_USEHTML', 'Használjon HTML kódot?');
+define('_AM_SYSTEM_BANNERS_CODEHTML', 'Adja meg a HTML kódot:');
 // Tips
 define('_AM_SYSTEM_BANNERS_NAV_TIPS', '
 <ul>
-<li>Add, modify and update category, banners and client.</li>
+<li>Kategória, bannerek és ügyfelek hozzáadása, módosítása és frissítése.</li>
 </ul>
 ');
