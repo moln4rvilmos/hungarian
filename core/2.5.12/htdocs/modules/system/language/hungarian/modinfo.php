45a39987cc641d80e0dc349afbc615401c744c42
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:47:00 +0100

    Update modinfo.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/modinfo.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/modinfo.php
@@ -1,84 +1,84 @@
 <?php
 //
-// _LANGCODE: en
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
+// Translator: Molnár Vilmos (XOOPS Translation Team)
 // Module Info
 // The name of this module
-define('_MI_SYSTEM_NAME', 'System');
+define('_MI_SYSTEM_NAME', 'Rendszer');
 // A brief description of this module
-define('_MI_SYSTEM_DESC', 'For administration of core settings of the site.');
+define('_MI_SYSTEM_DESC', 'A webhely alapbeállításainak adminisztrációjához.');
 // Names of blocks for this module (Not all module has blocks)
-define('_MI_SYSTEM_BNAME2', 'User Menu');
-define('_MI_SYSTEM_BNAME3', 'Login');
-define('_MI_SYSTEM_BNAME4', 'Search');
-define('_MI_SYSTEM_BNAME5', 'Waiting Contents');
-define('_MI_SYSTEM_BNAME6', 'Main Menu');
-define('_MI_SYSTEM_BNAME7', 'Site Info');
-define('_MI_SYSTEM_BNAME8', 'Who is Online');
-define('_MI_SYSTEM_BNAME9', 'Top Posters');
-define('_MI_SYSTEM_BNAME10', 'New Members');
-define('_MI_SYSTEM_BNAME11', 'Recent Comments');
+define('_MI_SYSTEM_BNAME2', 'Felhasználói Menü');
+define('_MI_SYSTEM_BNAME3', 'Bejelentkezés');
+define('_MI_SYSTEM_BNAME4', 'Keresés');
+define('_MI_SYSTEM_BNAME5', 'Várakozó Tartalmak');
+define('_MI_SYSTEM_BNAME6', 'Főmenü');
+define('_MI_SYSTEM_BNAME7', 'Oldal Info');
+define('_MI_SYSTEM_BNAME8', 'Ki van Online');
+define('_MI_SYSTEM_BNAME9', 'Legtöbbet Posztolók');
+define('_MI_SYSTEM_BNAME10', 'Új Tagok');
+define('_MI_SYSTEM_BNAME11', 'Legutóbbi Hozzászólások');
 // RMV-NOTIFY
-define('_MI_SYSTEM_BNAME12', 'Notification Options');
-define('_MI_SYSTEM_BNAME13', 'Themes');
+define('_MI_SYSTEM_BNAME12', 'Értesítési Opciók');
+define('_MI_SYSTEM_BNAME13', 'Témák (Themes)');
 // Names of admin menu items
-define('_MI_SYSTEM_ADMENU1', 'Banners');
-define('_MI_SYSTEM_ADMENU2', 'Blocks');
-define('_MI_SYSTEM_ADMENU3', 'Groups');
-define('_MI_SYSTEM_ADMENU5', 'Modules');
-define('_MI_SYSTEM_ADMENU6', 'Preferences');
-define('_MI_SYSTEM_ADMENU7', 'Smilies');
-define('_MI_SYSTEM_ADMENU9', 'User Ranks');
-define('_MI_SYSTEM_ADMENU10', 'Edit User');
-define('_MI_SYSTEM_ADMENU11', 'Email Users');
-define('_MI_SYSTEM_ADMENU12', 'Find Users');
-define('_MI_SYSTEM_ADMENU13', 'Images');
-define('_MI_SYSTEM_ADMENU14', 'Avatars');
-define('_MI_SYSTEM_ADMENU15', 'Templates');
-define('_MI_SYSTEM_ADMENU16', 'Comments');
+define('_MI_SYSTEM_ADMENU1', 'Reklámszalagok (Bannerek)');
+define('_MI_SYSTEM_ADMENU2', 'Blokkok');
+define('_MI_SYSTEM_ADMENU3', 'Csoportok');
+define('_MI_SYSTEM_ADMENU5', 'Modulok');
+define('_MI_SYSTEM_ADMENU6', 'Beállítások');
+define('_MI_SYSTEM_ADMENU7', 'Hangulatjelek (Smilies)');
+define('_MI_SYSTEM_ADMENU9', 'Felhasználói Rangok');
+define('_MI_SYSTEM_ADMENU10', 'Felhasználó Szerkesztése');
+define('_MI_SYSTEM_ADMENU11', 'E-mail Küldése Felhasználóknak');
+define('_MI_SYSTEM_ADMENU12', 'Felhasználók Keresése');
+define('_MI_SYSTEM_ADMENU13', 'Képek');
+define('_MI_SYSTEM_ADMENU14', 'Avatárok');
+define('_MI_SYSTEM_ADMENU15', 'Sablonok (Templates)');
+define('_MI_SYSTEM_ADMENU16', 'Hozzászólások');
 //Preference
-define('_MI_SYSTEM_PREFERENCE_BREAK_GENERAL', 'General Settings');
-define('_MI_SYSTEM_PREFERENCE_TIPS', 'Help online?');
-define('_MI_SYSTEM_PREFERENCE_TIPS_DSC', 'This gives you tips and online help');
-define('_MI_SYSTEM_PREFERENCE_ICONS', 'Icons');
-define('_MI_SYSTEM_PREFERENCE_BREADCRUMB', 'Breadcrumb');
-define('_MI_SYSTEM_PREFERENCE_BREAK_ACTIVE', 'Active Section');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_AVATARS', 'Active Avatars');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_BANNERS', 'Active Banners');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_BLOCKSADMIN', '');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_COMMENTS', 'Active Comments');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_FILEMANAGER', 'Active File Manager');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_GROUPS', '');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_IMAGES', 'Active Images Manager');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_MAILUSERS', 'Active Email Users');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_MODULESADMIN', '');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_PREFERENCES', '');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_SMILIES', 'Active Smilies');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_TPLSETS', '');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_USERRANK', 'Active User Ranks');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_USERS', 'Active Users');
-define('_MI_SYSTEM_PREFERENCE_ACTIVE_MAINTENANCE', 'Active Maintenance');
-define('_MI_SYSTEM_PREFERENCE_BREAK_PAGER', 'Number of rows to display in the administration');
-define('_MI_SYSTEM_PREFERENCE_AVATARS_PAGER', 'Number of avatars to display per page');
-define('_MI_SYSTEM_PREFERENCE_BANNERS_PAGER', 'Number of banners to display per page');
-define('_MI_SYSTEM_PREFERENCE_COMMENTS_PAGER', 'Number of comments to display per page');
-define('_MI_SYSTEM_PREFERENCE_GROUPS_PAGER', 'Number of groups to display per page');
-define('_MI_SYSTEM_PREFERENCE_IMAGES_PAGER', 'Number of images to display per page');
-define('_MI_SYSTEM_PREFERENCE_SMILIES_PAGER', 'Number of smilies to display per page');
-define('_MI_SYSTEM_PREFERENCE_USERRANKS_PAGER', 'Number of ranks to display per page');
-define('_MI_SYSTEM_PREFERENCE_USERS_PAGER', 'Number of users to display per page');
-define('_MI_SYSTEM_PREFERENCE_BREAK_EDITOR', 'Editor Settings');
-define('_MI_SYSTEM_PREFERENCE_BLOCKS_EDITOR', 'Editor for blocks:');
+define('_MI_SYSTEM_PREFERENCE_BREAK_GENERAL', 'Általános Beállítások');
+define('_MI_SYSTEM_PREFERENCE_TIPS', 'Online súgó?');
+define('_MI_SYSTEM_PREFERENCE_TIPS_DSC', 'Tippeket és online súgót biztosít');
+define('_MI_SYSTEM_PREFERENCE_ICONS', 'Ikonok');
+define('_MI_SYSTEM_PREFERENCE_BREADCRUMB', 'Morzsamenü (Breadcrumb)');
+define('_MI_SYSTEM_PREFERENCE_BREAK_ACTIVE', 'Aktív Szekció');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_AVATARS', 'Aktív Avatárok');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_BANNERS', 'Aktív Reklámszalagok');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_BLOCKSADMIN', 'Aktív Blokkok Admin');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_COMMENTS', 'Aktív Hozzászólások');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_FILEMANAGER', 'Aktív Fájlkezelő');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_GROUPS', 'Aktív Csoportok');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_IMAGES', 'Aktív Képkezelő');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_MAILUSERS', 'Aktív E-mail Küldés Felhasználóknak');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_MODULESADMIN', 'Aktív Modulok Admin');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_PREFERENCES', 'Aktív Beállítások');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_SMILIES', 'Aktív Hangulatjelek');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_TPLSETS', 'Aktív Sablonok');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_USERRANK', 'Aktív Felhasználói Rangok');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_USERS', 'Aktív Felhasználók');
+define('_MI_SYSTEM_PREFERENCE_ACTIVE_MAINTENANCE', 'Aktív Karbantartás');
+define('_MI_SYSTEM_PREFERENCE_BREAK_PAGER', 'Megjelenítendő sorok száma az adminisztrációban');
+define('_MI_SYSTEM_PREFERENCE_AVATARS_PAGER', 'Avatárok száma oldalanként');
+define('_MI_SYSTEM_PREFERENCE_BANNERS_PAGER', 'Bannerek száma oldalanként');
+define('_MI_SYSTEM_PREFERENCE_COMMENTS_PAGER', 'Hozzászólások száma oldalanként');
+define('_MI_SYSTEM_PREFERENCE_GROUPS_PAGER', 'Csoportok száma oldalanként');
+define('_MI_SYSTEM_PREFERENCE_IMAGES_PAGER', 'Képek száma oldalanként');
+define('_MI_SYSTEM_PREFERENCE_SMILIES_PAGER', 'Hangulatjelek száma oldalanként');
+define('_MI_SYSTEM_PREFERENCE_USERRANKS_PAGER', 'Rangok száma oldalanként');
+define('_MI_SYSTEM_PREFERENCE_USERS_PAGER', 'Felhasználók száma oldalanként');
+define('_MI_SYSTEM_PREFERENCE_BREAK_EDITOR', 'Szerkesztő Beállítások');
+define('_MI_SYSTEM_PREFERENCE_BLOCKS_EDITOR', 'Szerkesztő a blokkokhoz:');
 define('_MI_SYSTEM_PREFERENCE_BLOCKS_EDITOR_DSC', '');
-define('_MI_SYSTEM_PREFERENCE_COMMENTS_EDITOR', 'Editor for comments:');
+define('_MI_SYSTEM_PREFERENCE_COMMENTS_EDITOR', 'Szerkesztő a hozzászólásokhoz:');
 define('_MI_SYSTEM_PREFERENCE_COMMENTS_EDITOR_DSC', '');
-define('_MI_SYSTEM_PREFERENCE_GENERAL_EDITOR', 'Editor for all modules:');
+define('_MI_SYSTEM_PREFERENCE_GENERAL_EDITOR', 'Szerkesztő minden modulhoz:');
 define('_MI_SYSTEM_PREFERENCE_GENERAL_EDITOR_DSC', '');
-define('_MI_SYSTEM_PREFERENCE_ANONPOST', '');
-define('_MI_SYSTEM_PREFERENCE_REDIRECT', '');
-define('_MI_SYSTEM_PREFERENCE_JQUERY_THEME', 'jQuery theme');
+define('_MI_SYSTEM_PREFERENCE_ANONPOST', 'Anonim posztolás');
+define('_MI_SYSTEM_PREFERENCE_REDIRECT', 'Átirányítás');
+define('_MI_SYSTEM_PREFERENCE_JQUERY_THEME', 'jQuery téma');
 //2.5.11
-define('_MI_DEFAULT_NOTIFICATION_METHOD_DISABLE', 'Temporarily disable');
-define('_MI_DEFAULT_NOTIFICATION_METHOD_EMAIL', 'Email');
-define('_MI_DEFAULT_NOTIFICATION_METHOD_PM', 'Private message');
+define('_MI_DEFAULT_NOTIFICATION_METHOD_DISABLE', 'Ideiglenes letiltás');
+define('_MI_DEFAULT_NOTIFICATION_METHOD_EMAIL', 'E-mail');
+define('_MI_DEFAULT_NOTIFICATION_METHOD_PM', 'Privát üzenet');
