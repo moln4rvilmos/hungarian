5400695b2af615c55c669edbb4d27802e7d88305
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:58:20 +0100

    Update modulesadmin.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/admin/modulesadmin.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/admin/modulesadmin.php
@@ -2,169 +2,63 @@
 /**
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
  * @license         GNU GPL 2.0 or later (https://www.gnu.org/licenses/gpl-2.0.html)
- * _LANGCODE    en
- * _CHARSET     UTF-8
+ * _LANGCODE        hu
+ * _CHARSET         UTF-8
+ * Translator: Molnár Vilmos (XOOPS Translation Team)
  */
 // Navigation
-define('_AM_SYSTEM_MODULES_ADMIN', 'XOOPS Module Administration');
-define('_AM_SYSTEM_MODULES_LIST', 'Module List');
-define('_AM_SYSTEM_MODULES_TOINSTALL', 'Install module');
-define('_AM_SYSTEM_MODULES_VALIDATE', 'Validate changes');
-define('_AM_SYSTEM_MODULES_SUBMITRES', 'Submit Result');
+define('_AM_SYSTEM_MODULES_ADMIN', 'XOOPS Modul Adminisztráció');
+define('_AM_SYSTEM_MODULES_LIST', 'Modul Lista');
+define('_AM_SYSTEM_MODULES_TOINSTALL', 'Modul telepítése');
+define('_AM_SYSTEM_MODULES_VALIDATE', 'Változások jóváhagyása');
+define('_AM_SYSTEM_MODULES_SUBMITRES', 'Eredmény Küldése');
 // Messages
-define('_AM_SYSTEM_MODULES_RUSUREINS', 'Press the button below to install this module');
-define('_AM_SYSTEM_MODULES_RUSUREUNINS', 'Are you sure you would like to uninstall this module?');
-define('_AM_SYSTEM_MODULES_RUSUREUPD', 'Press the button below to update this module');
-define('_AM_SYSTEM_MODULES_BTOMADMIN', 'XOOPS Module Administration');
-define('_AM_SYSTEM_MODULES_INSTALLING', 'Installing ');
-define('_AM_SYSTEM_MODULES_DEACTIVATE', 'Deactivate ');
-define('_AM_SYSTEM_MODULES_ACTIVATE', 'Activate ');
-define('_AM_SYSTEM_MODULES_UPDATING', 'Updating ');
+define('_AM_SYSTEM_MODULES_RUSUREINS', 'Nyomd meg az alábbi gombot a modul telepítéséhez');
+define('_AM_SYSTEM_MODULES_RUSUREUNINS', 'Biztosan el szeretnéd távolítani ezt a modult?');
+define('_AM_SYSTEM_MODULES_RUSUREUPD', 'Nyomd meg az alábbi gombot a modul frissítéséhez');
+define('_AM_SYSTEM_MODULES_BTOMADMIN', 'XOOPS Modul Adminisztráció');
+define('_AM_SYSTEM_MODULES_INSTALLING', 'Telepítés ');
+define('_AM_SYSTEM_MODULES_DEACTIVATE', 'Deaktiválás ');
+define('_AM_SYSTEM_MODULES_ACTIVATE', 'Aktiválás ');
+define('_AM_SYSTEM_MODULES_UPDATING', 'Frissítés ');
 // Main
-define('_AM_SYSTEM_MODULES_INSTALL', 'Install');
-define('_AM_SYSTEM_MODULES_UNINSTALL', 'Uninstall');
-define('_AM_SYSTEM_MODULES_UPDATE', 'Update');
-define('_AM_SYSTEM_MODULES_VIEWLARGE', 'Large View');
-define('_AM_SYSTEM_MODULES_VIEWLINE', 'Line View');
+define('_AM_SYSTEM_MODULES_INSTALL', 'Telepítés');
+define('_AM_SYSTEM_MODULES_UNINSTALL', 'Eltávolítás');
+define('_AM_SYSTEM_MODULES_UPDATE', 'Frissítés');
+define('_AM_SYSTEM_MODULES_VIEWLARGE', 'Nagy Nézet');
+define('_AM_SYSTEM_MODULES_VIEWLINE', 'Vonal Nézet');
 // %s represents module name
-define('_AM_SYSTEM_MODULES_FAILINS', 'Unable to install %s.');
-define('_AM_SYSTEM_MODULES_FAILACT', 'Unable to activate %s.');
-define('_AM_SYSTEM_MODULES_FAILDEACT', 'Unable to deactivate %s.');
-define('_AM_SYSTEM_MODULES_FAILUPD', 'Unable to update %s.');
-define('_AM_SYSTEM_MODULES_FAILUNINS', 'Unable to uninstall %s.');
-define('_AM_SYSTEM_MODULES_FAILORDER', 'Unable to reorder %s.');
-define('_AM_SYSTEM_MODULES_FAILWRITE', 'Unable to write to main menu.');
-define('_AM_SYSTEM_MODULES_ALEXISTS', 'Module %s already exists.');
-define('_AM_SYSTEM_MODULES_ERRORSC', 'Error(s):');
-define('_AM_SYSTEM_MODULES_OKINS', 'Module %s installed successfully.');
-define('_AM_SYSTEM_MODULES_OKACT', 'Module %s activated successfully.');
-define('_AM_SYSTEM_MODULES_OKDEACT', 'Module %s deactivated successfully.');
-define('_AM_SYSTEM_MODULES_OKUPD', 'Module %s updated successfully.');
-define('_AM_SYSTEM_MODULES_OKUNINS', 'Module %s uninstalled successfully.');
-define('_AM_SYSTEM_MODULES_OKORDER', 'Module %s changed successfully.');
-define('_AM_SYSTEM_MODULES_MODULE', 'Module');
-define('_AM_SYSTEM_MODULES_VERSION', 'Version');
-define('_AM_SYSTEM_MODULES_LASTUP', 'Last Update');
-define('_AM_SYSTEM_MODULES_DEACTIVATED', 'Deactivated');
-define('_AM_SYSTEM_MODULES_ACTION', 'Action');
-define('_AM_SYSTEM_MODULES_MENU', 'Menu');
-define('_AM_SYSTEM_MODULES_HIDE', 'Hide');
-define('_AM_SYSTEM_MODULES_SHOW', 'Show');
-define('_AM_SYSTEM_MODULES_DUPEN', 'Duplicate entry in modules table!');
-define('_AM_SYSTEM_MODULES_DEACTED', 'The selected module has been deactivated. You can now safely uninstall the module.');
-define('_AM_SYSTEM_MODULES_ACTED', 'The selected module has been activated!');
-define('_AM_SYSTEM_MODULES_UPDTED', 'The selected module has been updated!');
-define('_AM_SYSTEM_MODULES_SYSNO', 'System module cannot be deactivated.');
-define('_AM_SYSTEM_MODULES_STRTNO', 'This module is set as your default start page. Please change the start module to whatever suits your preferences.');
-define('_AM_SYSTEM_MODULES_ORDER', 'Order');
-define('_AM_SYSTEM_MODULES_ORDER0', '(0 = hide)');
-define('_AM_SYSTEM_MODULES_ACTIVE', 'Active');
-define('_AM_SYSTEM_MODULES_INACTIVE', 'Inactive');
-define('_AM_SYSTEM_MODULES_NOTINSTALLED', 'Not Installed');
-define('_AM_SYSTEM_MODULES_NOCHANGE', 'No Change');
-define('_AM_SYSTEM_MODULES_SUBMIT', 'Submit');
-define('_AM_SYSTEM_MODULES_CANCEL', 'Cancel');
-define('_AM_SYSTEM_MODULES_DBUPDATE', _AM_SYSTEM_DBUPDATED);
-define('_AM_SYSTEM_MODULES_LISTUPBLKS', 'The following blocks will be updated.<br>Select the blocks of which contents (template and options) may be overwritten.<br>');
-define('_AM_SYSTEM_MODULES_NEWBLKS', 'New Blocks');
-define('_AM_SYSTEM_MODULES_DEPREBLKS', 'Deprecated Blocks');
-// Logger
-define('_AM_SYSTEM_MODULES_TABLE_RESERVED', '%s is a reserved table!');
-define('_AM_SYSTEM_MODULES_CREATE_TABLES', 'Creating tables...');
-define('_AM_SYSTEM_MODULES_TABLE_CREATED', 'Table %s created');
-define('_AM_SYSTEM_MODULES_INSERT_DATA', '&nbsp;&nbsp; Data inserted to table %s');
-define('_AM_SYSTEM_MODULES_INSERT_DATA_FAILD', 'Could not insert %s to database.');
-define('_AM_SYSTEM_MODULES_INSERT_DATA_DONE', 'Module data inserted successfully.');
-define('_AM_SYSTEM_MODULES_MODULEID', ' Module ID: %s');
-define('_AM_SYSTEM_MODULES_SQL_FOUND', 'SQL file found at %s ');
-define('_AM_SYSTEM_MODULES_SQL_NOT_FOUND', 'SQL file not found at %s');
-define('_AM_SYSTEM_MODULES_SQL_NOT_CREATE', 'ERROR: Could not create %s ');
-define('_AM_SYSTEM_MODULES_SQL_NOT_VALID', '%s is not a valid SQL!');
-define('_AM_SYSTEM_MODULES_GROUP_ID', ' Group ID: %s ');
-define('_AM_SYSTEM_MODULES_NAME', ' Name: ');
-define('_AM_SYSTEM_MODULES_VALUE', ' Value: ');
-// Templates
-define('_AM_SYSTEM_MODULES_TEMPLATES_ADD', 'Adding templates...');
-define('_AM_SYSTEM_MODULES_TEMPLATES_DELETE', 'Deleting templates...');
-define('_AM_SYSTEM_MODULES_TEMPLATES_UPDATE', 'Updating templates...');
-define('_AM_SYSTEM_MODULES_TEMPLATE_ID', 'Template ID: %s ');
-define('_AM_SYSTEM_MODULES_TEMPLATE_ADD_DATA', 'Template %s added to the database');
-define('_AM_SYSTEM_MODULES_TEMPLATE_ADD_ERROR', 'ERROR: Could not insert template %s to the database.');
-define('_AM_SYSTEM_MODULES_TEMPLATE_COMPILED', 'Template %s compiled ');
-define('_AM_SYSTEM_MODULES_TEMPLATE_COMPILED_FAILED', 'ERROR: Failed compiling template %s ');
-define('_AM_SYSTEM_MODULES_TEMPLATE_DELETE_DATA', 'Template %s deleted from the database. ');
-define('_AM_SYSTEM_MODULES_TEMPLATE_DELETE_DATA_FAILD', 'ERROR: Could not delete template %s from the database. ');
-define('_AM_SYSTEM_MODULES_TEMPLATE_INSERT_DATA', 'Template %s inserted to the database. ');
-define('_AM_SYSTEM_MODULES_TEMPLATE_RECOMPILE', 'Template %s recompiled');
-define('_AM_SYSTEM_MODULES_TEMPLATE_RECOMPILE_FAILD', 'ERROR: Template %s recompile failed');
-define('_AM_SYSTEM_MODULES_TEMPLATE_RECOMPILE_ERROR', 'ERROR: Could not recompile template %s ');
-define('_AM_SYSTEM_MODULES_TEMPLATE_DELETE_OLD_ERROR', 'ERROR: Could not delete old template %s. Aborting update of this file. ');
-define('_AM_SYSTEM_MODULES_TEMPLATE_UPDATE', 'Template %s updated. ');
-define('_AM_SYSTEM_MODULES_TEMPLATE_UPDATE_ERROR', 'ERROR: Could not update %s template. ');
-// Blocks
-define('_AM_SYSTEM_MODULES_BLOCKS_ADD', 'Adding blocks...');
-define('_AM_SYSTEM_MODULES_BLOCKS_DELETE', 'Deleting block...');
-define('_AM_SYSTEM_MODULES_BLOCKS_REBUILD', 'Rebuilding blocks...');
-define('_AM_SYSTEM_MODULES_BLOCK_ID', ' Block ID: %s ');
-define('_AM_SYSTEM_MODULES_BLOCK_ACCESS', 'Added block access right');
-define('_AM_SYSTEM_MODULES_BLOCK_ACCESS_ERROR', 'ERROR: Could not add block access right');
-define('_AM_SYSTEM_MODULES_BLOCK_ADD', 'Block %s added ');
-define('_AM_SYSTEM_MODULES_BLOCK_ADD_ERROR', 'ERROR: Could not add block %s to the database! ');
-define('_AM_SYSTEM_MODULES_BLOCK_ADD_ERROR_DATABASE', 'Database error: %s ');
-define('_AM_SYSTEM_MODULES_BLOCK_CREATED', 'Block %s created ');
-define('_AM_SYSTEM_MODULES_BLOCK_DELETE', 'Block %s deleted. ');
-define('_AM_SYSTEM_MODULES_BLOCK_DELETE_DATA', 'Block template %s deleted from the database. ');
-define('_AM_SYSTEM_MODULES_BLOCK_DELETE_ERROR', 'ERROR: Could not delete block %s');
-define('_AM_SYSTEM_MODULES_BLOCK_DELETE_TEMPLATE_ERROR', 'ERROR: Could not delete block template %s from the database');
-define('_AM_SYSTEM_MODULES_BLOCK_DEPRECATED', 'Block template %s deprecated ');
-define('_AM_SYSTEM_MODULES_BLOCK_DEPRECATED_ERROR', 'ERROR: Could not remove deprecated block template.  ');
-define('_AM_SYSTEM_MODULES_BLOCK_UPDATE', 'Block %s updated. ');
-// Configs
-define('_AM_SYSTEM_MODULES_GONFIG_ID', 'Config ID: %s');
-define('_AM_SYSTEM_MODULES_MODULE_DATA_ADD', 'Adding module config data...');
-define('_AM_SYSTEM_MODULES_MODULE_DATA_DELETE', 'Deleting module config options...');
-define('_AM_SYSTEM_MODULES_MODULE_DATA_UPDATE', 'Module data updated.');
-define('_AM_SYSTEM_MODULES_CONFIG_ADD', ' Config option added');
-define('_AM_SYSTEM_MODULES_CONFIG_DATA_ADD', ' Config %s added to the database');
-define('_AM_SYSTEM_MODULES_CONFIG_DATA_ADD_ERROR', ' ERROR: Could not insert config %s to the database. ');
-define('_AM_SYSTEM_MODULES_GONFIG_DATA_DELETE', 'Config data deleted from the database. ');
-define('_AM_SYSTEM_MODULES_CONFIG_DATA_DELETE_ERROR', 'ERROR: Could not delete config data from the database');
-// Access
-define('_AM_SYSTEM_MODULES_GROUP_SETTINGS_ADD', 'Setting group rights...');
-define('_AM_SYSTEM_MODULES_GROUP_PERMS_DELETE_ERROR', 'ERROR: Could not delete group permissions ');
-define('_AM_SYSTEM_MODULES_GROUP_PERMS_DELETED', 'Group permissions deleted ');
-define('_AM_SYSTEM_MODULES_ACCESS_ADMIN_ADD', 'Added admin access right for Group ID %s');
-define('_AM_SYSTEM_MODULES_ACCESS_ADMIN_ADD_ERROR', 'ERROR: Could not add admin access right for Group ID %s');
-define('_AM_SYSTEM_MODULES_ACCESS_USER_ADD_ERROR', 'Added user access right for Group ID: %s');
-define('_AM_SYSTEM_MODULES_ACCESS_USER_ADD_ERROR_ERROR', 'ERROR: Could not add user access right for Group ID: %s');
-// execute module specific install script if any
-define('_AM_SYSTEM_MODULES_FAILED_EXECUTE', 'Failed to execute %s');
-define('_AM_SYSTEM_MODULES_FAILED_SUCESS', '%s executed successfully.');
-define('_AM_SYSTEM_MODULES_DELETE_ERROR', 'ERROR: Could not delete %s');
-define('_AM_SYSTEM_MODULES_UPDATE_ERROR', 'ERROR: Could not update %s');
-define('_AM_SYSTEM_MODULES_DELETE_MOD_TABLES', 'Deleting module tables...');
-define('_AM_SYSTEM_MODULES_COMMENTS_DELETE', 'Deleting comments...');
-define('_AM_SYSTEM_MODULES_COMMENTS_DELETE_ERROR', 'ERROR: Could not delete comments');
-define('_AM_SYSTEM_MODULES_COMMENTS_DELETED', 'Comments deleted');
-define('_AM_SYSTEM_MODULES_NOTIFICATIONS_DELETE', 'Deleting notifications...');
-define('_AM_SYSTEM_MODULES_NOTIFICATIONS_DELETE_ERROR', 'ERROR: Could not delete notifications');
-define('_AM_SYSTEM_MODULES_NOTIFICATIONS_DELETED', 'Notifications deleted');
-define('_AM_SYSTEM_MODULES_TABLE_DROPPED', 'Table %s dropped!');
-define('_AM_SYSTEM_MODULES_TABLE_DROPPED_ERROR', 'ERROR: Could not drop table %s');
-define('_AM_SYSTEM_MODULES_TABLE_DROPPED_FAILDED', 'ERROR: Not allowed to drop table %s !');
-// Tips
-define('_AM_SYSTEM_MODULES_TIPS', '<ul>
-<li>If you install a new module, remember to set up module preferences, blocks and users permissions!</li>
-<li>For hide module into Main Menu block, set order to 0</li>
-<li>Delete unused module files from your server to avoid security issues and keep your website safe.</li>
-<li>To change order of modules (which will be reflected in the Menu), you just need to drag and drop the modules to the desired placement.</li>
-</ul>');
-define('_AM_SYSTEM_MODULES_CONFIRM_TIPS', '<ul>
-<li>Check all modifications for validate.</li>
-</ul>');
-// 2.5.7
-define('_AM_SYSTEM_MODULES_INSTALL_TESTDATA', 'Add Test Data');
-// 2.5.8
-define('_AM_SYSTEM_MODULES_INSTALL_MORE', 'Install more modules');
-// 2.5.11
-define('_AM_SYSTEM_MODULES_INSTALL_THISMODULE', 'This Module Admin');
+define('_AM_SYSTEM_MODULES_FAILINS', 'Nem sikerült telepíteni a(z) %s modult.');
+define('_AM_SYSTEM_MODULES_FAILACT', 'Nem sikerült aktiválni a(z) %s modult.');
+define('_AM_SYSTEM_MODULES_FAILDEACT', 'Nem sikerült deaktiválni a(z) %s modult.');
+define('_AM_SYSTEM_MODULES_FAILUPD', 'Nem sikerült frissíteni a(z) %s modult.');
+define('_AM_SYSTEM_MODULES_FAILUNINS', 'Nem sikerült eltávolítani a(z) %s modult.');
+define('_AM_SYSTEM_MODULES_FAILORDER', 'Nem sikerült újrarendezni a(z) %s modult.');
+define('_AM_SYSTEM_MODULES_FAILWRITE', 'Nem sikerült írni a főmenübe.');
+define('_AM_SYSTEM_MODULES_ALEXISTS', 'A(z) %s modul már létezik.');
+define('_AM_SYSTEM_MODULES_ERRORSC', 'Hiba(ák):');
+define('_AM_SYSTEM_MODULES_OKINS', 'A(z) %s modul sikeresen telepítve.');
+define('_AM_SYSTEM_MODULES_OKACT', 'A(z) %s modul sikeresen aktiválva.');
+define('_AM_SYSTEM_MODULES_OKDEACT', 'A(z) %s modul sikeresen deaktiválva.');
+define('_AM_SYSTEM_MODULES_OKUPD', 'A(z) %s modul sikeresen frissítve.');
+define('_AM_SYSTEM_MODULES_OKUNINS', 'A(z) %s modul sikeresen eltávolítva.');
+define('_AM_SYSTEM_MODULES_OKORDER', 'A(z) %s modul sikeresen megváltoztatva.');
+define('_AM_SYSTEM_MODULES_MODULE', 'Modul');
+define('_AM_SYSTEM_MODULES_VERSION', 'Verzió');
+define('_AM_SYSTEM_MODULES_LASTUP', 'Utolsó Frissítés');
+define('_AM_SYSTEM_MODULES_DEACTIVATED', 'Deaktiválva');
+define('_AM_SYSTEM_MODULES_ACTION', 'Művelet');
+define('_AM_SYSTEM_MODULES_MENU', 'Menü');
+define('_AM_SYSTEM_MODULES_HIDE', 'Elrejtés');
+define('_AM_SYSTEM_MODULES_SHOW', 'Megjelenítés');
+define('_AM_SYSTEM_MODULES_DUPEN', 'Ismétlődő bejegyzés a modulok táblában!');
+define('_AM_SYSTEM_MODULES_DEACTED', 'A kiválasztott modul deaktiválva. Most már biztonságosan eltávolíthatod a modult.');
+define('_AM_SYSTEM_MODULES_ACTED', 'A kiválasztott modul aktiválva!');
+define('_AM_SYSTEM_MODULES_UPDTED', 'A kiválasztott modul frissítve!');
+define('_AM_SYSTEM_MODULES_SYSNO', 'A Rendszer modult nem lehet deaktiválni.');
+define('_AM_SYSTEM_MODULES_STRTNO', 'Ez a modul az alapértelmezett kezdőoldalad. Kérlek, változtasd meg a kezdőmodult a beállításaidnak megfelelően.');
+define('_AM_SYSTEM_MODULES_ORDER', 'Sorrend');
+define('_AM_SYSTEM_MODULES_ORDER0', '(0 = elrejtés)');
+define('_AM_SYSTEM_MODULES_ACTIVE', 'Aktív');
+define('_AM_SYSTEM_MODULES_INACTIVE', 'Inaktív');
+define('_AM_SYSTEM_MODULES_NOTINSTALLED',
