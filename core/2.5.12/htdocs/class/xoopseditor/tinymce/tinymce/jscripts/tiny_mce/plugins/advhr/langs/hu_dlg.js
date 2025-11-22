ba2f39b5d0860441df1d527217b8db5dac627b81
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 07:54:09 +0100

    Add Hungarian language support for advhr plugin

---
+++ core/2.5.12/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/advhr/langs/hu_dlg.js
@@ -0,0 +1,5 @@
+tinyMCE.addI18n('hu.advhr_dlg',{size:"Magasság",
+                                noshade:"Árnyék Nélkül",
+                                width:"Szélesség",
+                                normal:"Normál",
+                                widthunits:"Egységek"});
