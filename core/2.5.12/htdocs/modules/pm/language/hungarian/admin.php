666463a3ddb6467c1939824746294a4ab78bf457
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:34:47 +0100

    Update admin.php

--- core/2.5.12/htdocs/modules/pm/language/hungarian/admin.php
+++ core/2.5.12/htdocs/modules/pm/language/hungarian/admin.php
@@ -1,13 +1,13 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_PM_AM_PRUNE', 'Prune');
-define('_PM_AM_PRUNEAFTER', 'Prune messages posted after this date (leave blank for no start date)');
-define('_PM_AM_PRUNEBEFORE', 'Prune messages posted before this date (leave blank for no end date)');
-define('_PM_AM_ONLYREADMESSAGES', 'Prune only read messages');
-define('_PM_AM_INCLUDESAVE', "Include messages in users' \"save\" folders");
-define('_PM_AM_NOTIFYUSERS', 'Notify affected users about the prune?');
-define('_PM_AM_MESSAGESPRUNED', '%u Messages Pruned');
-define('_PM_AM_ERRORWHILEPRUNING', 'An error occurred during prune');
+// Translator: Molnár Vilmos (XOOPS Translation Team)
+define('_PM_AM_PRUNE', 'Takarítás (Prune)');
+define('_PM_AM_PRUNEAFTER', 'Töröld az ezen dátum után feladott üzeneteket (hagyd üresen, ha nincs kezdő dátum)');
+define('_PM_AM_PRUNEBEFORE', 'Töröld az ezen dátum előtt feladott üzeneteket (hagyd üresen, ha nincs befejező dátum)');
+define('_PM_AM_ONLYREADMESSAGES', 'Csak az elolvasott üzeneteket töröld');
+define('_PM_AM_INCLUDESAVE', "Tartalmazza a felhasználók 'mentett' mappájában lévő üzeneteket is");
+define('_PM_AM_NOTIFYUSERS', 'Értesítsem az érintett felhasználókat a törlésről?');
+define('_PM_AM_MESSAGESPRUNED', '%u üzenet törölve');
+define('_PM_AM_ERRORWHILEPRUNING', 'Hiba történt a törlés során');
