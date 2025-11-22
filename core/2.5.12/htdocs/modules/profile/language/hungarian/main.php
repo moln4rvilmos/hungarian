cfd9096359f401d38e12e4e89be31d1bdfb9f298
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:39:51 +0100

    Update main.php

--- core/2.5.12/htdocs/modules/profile/language/hungarian/main.php
+++ core/2.5.12/htdocs/modules/profile/language/hungarian/main.php
@@ -1,45 +1,45 @@
 <?php
-// 
-// _LANGCODE: en
+//
+// _LANGCODE: hu
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
-define('_PROFILE_MA_REGISTER_NOTGROUP', 'New user is not registered to corresponding groups.');
-define('_PROFILE_MA_FINISH_LOGIN', 'Your account has been created successfully, please click to log on.');
-define('_PROFILE_MA_REGISTER_FINISH', 'Thanks for registering');
-define('_PROFILE_MA_REGISTER_STEPS', 'Register steps:');
-define('_PROFILE_MA_DEFAULT', 'Basic Information');
-define('_PROFILE_MA_ERRORDURINGSAVE', 'Error during save');
-define('_PROFILE_MA_NOSTEPSAVAILABLE', 'Registration is not allowed at this moment, please come back later.');
-define('_PROFILE_MA_EXPIRED', 'The process has been expired, please go back to try again.');
-define('_PROFILE_MA_RECENTACTIVITY', 'Recent Activities');
-define('_PROFILE_MA_THEME', 'Theme');
-define('_PROFILE_MA_ACTIVATE', 'Activate');
-define('_PROFILE_MA_DEACTIVATE', 'Deactivate');
-define('_PROFILE_MA_SENDPM', 'Send Message');
+// Translator: Molnár Vilmos (XOOPS Translation Team)
+define('_PROFILE_MA_REGISTER_NOTGROUP', 'Az új felhasználó nincs hozzárendelve a megfelelő csoportokhoz.');
+define('_PROFILE_MA_FINISH_LOGIN', 'A fiókod sikeresen létrejött, kattints a bejelentkezéshez.');
+define('_PROFILE_MA_REGISTER_FINISH', 'Köszönjük a regisztrációt');
+define('_PROFILE_MA_REGISTER_STEPS', 'Regisztrációs lépések:');
+define('_PROFILE_MA_DEFAULT', 'Alapvető Információk');
+define('_PROFILE_MA_ERRORDURINGSAVE', 'Hiba történt a mentés során');
+define('_PROFILE_MA_NOSTEPSAVAILABLE', 'A regisztráció jelenleg nem engedélyezett, kérlek, térj vissza később.');
+define('_PROFILE_MA_EXPIRED', 'A folyamat lejárt, kérlek, menj vissza, és próbáld újra.');
+define('_PROFILE_MA_RECENTACTIVITY', 'Legutóbbi Tevékenységek');
+define('_PROFILE_MA_THEME', 'Téma');
+define('_PROFILE_MA_ACTIVATE', 'Aktiválás');
+define('_PROFILE_MA_DEACTIVATE', 'Deaktiválás');
+define('_PROFILE_MA_SENDPM', 'Üzenet Küldése');
 //changepass.php
-define('_PROFILE_MA_CHANGEPASSWORD', 'Change Password');
-define('_PROFILE_MA_PASSWORDCHANGED', 'Password Changed Successfully');
-define('_PROFILE_MA_OLDPASSWORD', 'Current Password');
-define('_PROFILE_MA_NEWPASSWORD', 'New Password');
-define('_PROFILE_MA_WRONGPASSWORD', 'Old password is wrong');
+define('_PROFILE_MA_CHANGEPASSWORD', 'Jelszó Megváltoztatása');
+define('_PROFILE_MA_PASSWORDCHANGED', 'Jelszó sikeresen megváltoztatva');
+define('_PROFILE_MA_OLDPASSWORD', 'Jelenlegi Jelszó');
+define('_PROFILE_MA_NEWPASSWORD', 'Új Jelszó');
+define('_PROFILE_MA_WRONGPASSWORD', 'A régi jelszó helytelen');
 //search.php
-define('_PROFILE_MA_SORTBY', 'Sort By');
-define('_PROFILE_MA_ORDER', 'Order');
-define('_PROFILE_MA_PERPAGE', 'Users per page');
-define('_PROFILE_MA_LATERTHAN', '%s is later than');
-define('_PROFILE_MA_EARLIERTHAN', '%s is earlier than');
-define('_PROFILE_MA_LARGERTHAN', '%s is larger than');
-define('_PROFILE_MA_SMALLERTHAN', '%s is smaller than');
-define('_PROFILE_MA_NOUSERSFOUND', 'No users found');
-define('_PROFILE_MA_RESULTS', 'Search Results');
-define('_PROFILE_MA_SEARCH', 'Search Users');
+define('_PROFILE_MA_SORTBY', 'Rendezés alapja');
+define('_PROFILE_MA_ORDER', 'Sorrend');
+define('_PROFILE_MA_PERPAGE', 'Felhasználók száma oldalanként');
+define('_PROFILE_MA_LATERTHAN', '%s később van, mint');
+define('_PROFILE_MA_EARLIERTHAN', '%s korábban van, mint');
+define('_PROFILE_MA_LARGERTHAN', '%s nagyobb, mint');
+define('_PROFILE_MA_SMALLERTHAN', '%s kisebb, mint');
+define('_PROFILE_MA_NOUSERSFOUND', 'Nem található felhasználó');
+define('_PROFILE_MA_RESULTS', 'Keresési Eredmények');
+define('_PROFILE_MA_SEARCH', 'Felhasználók Keresése');
 //changemail.php
-define('_PROFILE_MA_CHANGEMAIL', 'Change Email');
-define('_PROFILE_MA_NEWMAIL', 'New Email Address');
-define('_PROFILE_MA_NEWEMAIL', 'New email address at %s');
-define('_PROFILE_MA_EMAILCHANGED', 'Your Email Address Has Been Changed');
-define('_PROFILE_MA_SITEDEFAULT', 'Site default');
-define('_PROFILE_MA_USERINFO', 'User profile');
-define('_PROFILE_MA_REGISTER', 'Registration form');
-define('_PROFILE_MA_ACTUS', 'Active Users: %s');
-define('_PROFILE_MA_FOUNDUSER', '%s users found');
+define('_PROFILE_MA_CHANGEMAIL', 'E-mail Megváltoztatása');
+define('_PROFILE_MA_NEWMAIL', 'Új E-mail Cím');
+define('_PROFILE_MA_NEWEMAIL', 'Új e-mail cím a(z) %s oldalon');
+define('_PROFILE_MA_EMAILCHANGED', 'Az e-mail címed megváltozott');
+define('_PROFILE_MA_SITEDEFAULT', 'Oldal alapértelmezése');
+define('_PROFILE_MA_USERINFO', 'Felhasználói profil');
+define('_PROFILE_MA_REGISTER', 'Regisztrációs űrlap');
+define('_PROFILE_MA_ACTUS', 'Aktív felhasználók: %s');
+define('_PROFILE_MA_FOUNDUSER', '%s felhasználó található');
