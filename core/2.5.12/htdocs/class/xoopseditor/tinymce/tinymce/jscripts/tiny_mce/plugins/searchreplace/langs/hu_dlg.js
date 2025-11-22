f63e959c38d502f235c91e454db21ba9054cde87
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:03:59 +0100

    Create hu_dlg.js

---
+++ core/2.5.12/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/searchreplace/langs/hu_dlg.js
@@ -0,0 +1,16 @@
+tinyMCE.addI18n('hu.searchreplace_dlg',{
+    findwhat:"Keresendő Szöveg",
+    replacewith:"Csere Szöveg",
+    direction:"Irány",
+    up:"Fel",
+    down:"Le",
+    mcase:"Kis- és Nagybetű Érzékenység",
+    findnext:"Keresés Tovább",
+    allreplaced:"A keresett szöveg összes előfordulását lecseréltük.",
+    "searchnext_desc":"Keresés Tovább",
+    notfound:"A keresés befejeződött. A keresett szöveg nem található.",
+    "search_title":"Keresés",
+    "replace_title":"Keresés/Csere",
+    replaceall:"Összes Cseréje",
+    replace:"Csere"
+});
