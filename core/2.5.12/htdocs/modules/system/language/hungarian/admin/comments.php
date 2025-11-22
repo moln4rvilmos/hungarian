2d0a256bd74108fe73616ffa868963811d145a3a
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:52:40 +0100

    Update comments.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/admin/comments.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/admin/comments.php
@@ -2,41 +2,42 @@
 /**
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
  * @license         GNU GPL 2.0 or later (https://www.gnu.org/licenses/gpl-2.0.html)
- * _LANGCODE    en
- * _CHARSET     UTF-8
+ * _LANGCODE        hu
+ * _CHARSET         UTF-8
+ * Translator: Molnár Vilmos (XOOPS Translation Team)
  */
 // Manager
-define('_AM_SYSTEM_COMMENTS_NAV_MANAGER', 'Comment Management');
-define('_AM_SYSTEM_COMMENTS_NAV_MAIN', 'List');
-define('_AM_SYSTEM_COMMENTS_NAV_PURGE', 'Purge');
+define('_AM_SYSTEM_COMMENTS_NAV_MANAGER', 'Hozzászólás Kezelés');
+define('_AM_SYSTEM_COMMENTS_NAV_MAIN', 'Lista');
+define('_AM_SYSTEM_COMMENTS_NAV_PURGE', 'Tisztítás');
 // Nav
-define('_AM_SYSTEM_COMMENTS_MODULE_ADMIN', 'Modules Administration');
+define('_AM_SYSTEM_COMMENTS_MODULE_ADMIN', 'Modulok Adminisztrációja');
 // Tips
 define('_AM_SYSTEM_COMMENTS_NAV_TIPS', '
 <ul>
-<li>Manage comments for all your modules.</li>
-<li>Delete comments easily with the purge.</li>
+<li>Kezeld az összes modulhoz tartozó hozzászólást.</li>
+<li>Töröld a hozzászólásokat könnyen a tisztítás (purge) funkcióval.</li>
 </ul>');
 // Form
-define('_AM_SYSTEM_COMMENTS_FORM_LIST_COMMENTS', 'List Comments');
-define('_AM_SYSTEM_COMMENTS_FORM_ALL_MODS', 'All modules');
-define('_AM_SYSTEM_COMMENTS_FORM_ALL_STATUS', 'Any status');
-define('_AM_SYSTEM_COMMENTS_FORM_PURGE', 'Purge');
-define('_AM_SYSTEM_COMMENTS_FORM_PURGE_DATE_AFTER', 'After this date (leave empty for any date)');
-define('_AM_SYSTEM_COMMENTS_FORM_PURGE_DATE_BEFORE', 'Before this date (leave empty for any date)');
-define('_AM_SYSTEM_COMMENTS_FORM_PURGE_GROUPS', 'Groups');
-define('_AM_SYSTEM_COMMENTS_FORM_PURGE_USER', 'User name');
-define('_AM_SYSTEM_COMMENTS_FORM_PURGE_STATUS', 'Status');
-define('_AM_SYSTEM_COMMENTS_FORM_PURGE_MODULES', 'Modules');
+define('_AM_SYSTEM_COMMENTS_FORM_LIST_COMMENTS', 'Hozzászólások Listázása');
+define('_AM_SYSTEM_COMMENTS_FORM_ALL_MODS', 'Minden modul');
+define('_AM_SYSTEM_COMMENTS_FORM_ALL_STATUS', 'Bármely státusz');
+define('_AM_SYSTEM_COMMENTS_FORM_PURGE', 'Tisztítás');
+define('_AM_SYSTEM_COMMENTS_FORM_PURGE_DATE_AFTER', 'E dátum után (hagyd üresen a bármely dátumhoz)');
+define('_AM_SYSTEM_COMMENTS_FORM_PURGE_DATE_BEFORE', 'E dátum előtt (hagyd üresen a bármely dátumhoz)');
+define('_AM_SYSTEM_COMMENTS_FORM_PURGE_GROUPS', 'Csoportok');
+define('_AM_SYSTEM_COMMENTS_FORM_PURGE_USER', 'Felhasználónév');
+define('_AM_SYSTEM_COMMENTS_FORM_PURGE_STATUS', 'Státusz');
+define('_AM_SYSTEM_COMMENTS_FORM_PURGE_MODULES', 'Modulok');
 // Tab
-define('_AM_SYSTEM_COMMENTS_TITLE', 'Title');
-define('_AM_SYSTEM_COMMENTS_POSTED', 'Author');
+define('_AM_SYSTEM_COMMENTS_TITLE', 'Cím');
+define('_AM_SYSTEM_COMMENTS_POSTED', 'Szerző');
 define('_AM_SYSTEM_COMMENTS_IP', 'IP');
-define('_AM_SYSTEM_COMMENTS_MODULE', 'Module');
-define('_AM_SYSTEM_COMMENTS_STATUS', 'Status');
-define('_AM_SYSTEM_COMMENTS_ACTION', 'Action');
-define('_AM_SYSTEM_COMMENTS_VIEW', 'See comment');
-define('_AM_SYSTEM_COMMENTS_NO_COMMENTS', 'No comments');
-define('_AM_SYSTEM_COMMENTS_COMMENTS_FOUND', '%s comment(s) found.');
+define('_AM_SYSTEM_COMMENTS_MODULE', 'Modul');
+define('_AM_SYSTEM_COMMENTS_STATUS', 'Státusz');
+define('_AM_SYSTEM_COMMENTS_ACTION', 'Művelet');
+define('_AM_SYSTEM_COMMENTS_VIEW', 'Hozzászólás megtekintése');
+define('_AM_SYSTEM_COMMENTS_NO_COMMENTS', 'Nincsenek hozzászólások');
+define('_AM_SYSTEM_COMMENTS_COMMENTS_FOUND', '%s hozzászólás található.');
 //XOOPS 2.5.9
-define('_MP_DELETECOUNT', 'The number of members in the group is too big ( > 4,000) ');
+define('_MP_DELETECOUNT', 'A csoportban lévő tagok száma túl nagy ( > 4000) ');
