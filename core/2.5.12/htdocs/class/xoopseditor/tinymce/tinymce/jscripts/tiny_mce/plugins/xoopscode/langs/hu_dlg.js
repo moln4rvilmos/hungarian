dc02ea3aed0bedad94e81d98d9d76c1c4e73aef4
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:10:08 +0100

    Create hu_dlg.js

---
+++ core/2.5.12/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/xoopscode/langs/hu_dlg.js
@@ -0,0 +1,5 @@
+tinyMCE.addI18n('hu.xoopscode_dlg',{
+    xoopscode_title:"Kód Beillesztése",
+    xoopscode_desc:"Kód Beillesztése",
+    xoopscode_sub:"Illeszd be a beillesztendő kódot az alábbi mezőbe:"
+});
