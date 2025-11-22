7ac1ce242ef0bdd1c05b110068a6b5b16d256699
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:49:42 +0100

    Update avatars.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/admin/avatars.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/admin/avatars.php
@@ -1,31 +1,32 @@
 <?php
 /**
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
- * @license             GNU GPL 2 (https://www.gnu.org/licenses/gpl-2.0.html)
- * _LANGCODE    en
- * _CHARSET     UTF-8
+ * @license         GNU GPL 2 (https://www.gnu.org/licenses/gpl-2.0.html)
+ * _LANGCODE        hu
+ * _CHARSET         UTF-8
+ * Translator: Molnár Vilmos (XOOPS Translation Team)
  */
 // Navigation
-define('_AM_SYSTEM_AVATAR_MANAGER', 'Avatars Management');
-define('_AM_SYSTEM_AVATAR_MAIN', 'Main');
-define('_AM_SYSTEM_AVATAR_SYSTEM', 'System Avatars');
-define('_AM_SYSTEM_AVATAR_CUSTOM', 'Custom Avatars');
-define('_AM_SYSTEM_AVATAR_ADD', 'Add Avatar');
-define('_AM_SYSTEM_AVATAR_EDIT', 'Edit Avatar');
-define('_AM_SYSTEM_AVATAR_DELETE', 'Delete Avatar');
+define('_AM_SYSTEM_AVATAR_MANAGER', 'Avatár Kezelés');
+define('_AM_SYSTEM_AVATAR_MAIN', 'Főoldal');
+define('_AM_SYSTEM_AVATAR_SYSTEM', 'Rendszer Avatárok');
+define('_AM_SYSTEM_AVATAR_CUSTOM', 'Egyedi Avatárok');
+define('_AM_SYSTEM_AVATAR_ADD', 'Avatár Hozzáadása');
+define('_AM_SYSTEM_AVATAR_EDIT', 'Avatár Szerkesztése');
+define('_AM_SYSTEM_AVATAR_DELETE', 'Avatár Törlése');
 // Main
-define('_AM_SYSTEM_AVATAR_MULTIUPLOAD', 'Multi uploads');
+define('_AM_SYSTEM_AVATAR_MULTIUPLOAD', 'Több fájl feltöltése');
 // Infos
-define('_AM_SYSTEM_AVATAR_ERROR', 'Errors');
-define('_AM_SYSTEM_AVATAR_USERS', 'Users using this avatar');
-define('_AM_SYSTEM_AVATAR_USE_FILE', 'Choose files in: %s');
-define('_AM_SYSTEM_AVATAR_UPLOAD', 'Upload:');
+define('_AM_SYSTEM_AVATAR_ERROR', 'Hibák');
+define('_AM_SYSTEM_AVATAR_USERS', 'Felhasználók, akik használják ezt az avatárt');
+define('_AM_SYSTEM_AVATAR_USE_FILE', 'Válassz fájlokat a következő helyen: %s');
+define('_AM_SYSTEM_AVATAR_UPLOAD', 'Feltöltés:');
 // Messages
-define('_AM_SYSTEM_AVATAR_FAILDEL', 'Failed deleting avatar %s from the database');
-define('_AM_SYSTEM_AVATAR_SUREDEL', 'Are you sure to delete this avatar image?');
+define('_AM_SYSTEM_AVATAR_FAILDEL', 'Az avatár %s törlése sikertelen az adatbázisból');
+define('_AM_SYSTEM_AVATAR_SUREDEL', 'Biztosan törölni szeretnéd ezt az avatár képet?');
 // Tips
 define('_AM_SYSTEM_AVATAR_TIPS', "
 <ul id='newsticker' class='newsticker'>
-<li>Manage all System or Custom avatars</li>
-<li>Site members can optionally create their own online personality called avatars.<br>This option can be turned off from System User preferences.</li>
+<li>Kezeld az összes Rendszer vagy Egyedi avatárt.</li>
+<li>Az oldaltagok opcionálisan létrehozhatják saját online személyiségüket, az úgynevezett avatárjaikat.<br>Ez az opció kikapcsolható a Rendszer/Felhasználói beállításoknál.</li>
 </ul>");
