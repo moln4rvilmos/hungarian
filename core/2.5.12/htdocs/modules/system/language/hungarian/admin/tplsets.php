dc76508c486e8b75afb439bd33602c8b6b2f6ef0
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 09:01:39 +0100

    Update tplsets.php

--- core/2.5.12/htdocs/modules/system/language/hungarian/admin/tplsets.php
+++ core/2.5.12/htdocs/modules/system/language/hungarian/admin/tplsets.php
@@ -2,41 +2,42 @@
 /**
  * @copyright       (c) 2000-2016 XOOPS Project (www.xoops.org)
  * @license         GNU GPL 2.0 or later (https://www.gnu.org/licenses/gpl-2.0.html)
- * _LANGCODE    en
- * _CHARSET     UTF-8
+ * _LANGCODE        hu
+ * _CHARSET         UTF-8
+ * Translator: Molnár Vilmos (XOOPS Translation Team)
  */
 //Nav
-define('_AM_SYSTEM_TEMPLATES_NAV_MANAGER', 'Templates Manager');
-define('_AM_SYSTEM_TEMPLATES_NAV_MAIN', 'Templates Manager');
+define('_AM_SYSTEM_TEMPLATES_NAV_MANAGER', 'Sablonkezelő');
+define('_AM_SYSTEM_TEMPLATES_NAV_MAIN', 'Sablonkezelő');
 //Tips
 define('_AM_SYSTEM_TEMPLATES_NAV_TIPS', '
 <ul>
-<li>Edit theme and modules templates, stylesheet online.</li>
-<li>Generate all override modules templates.</li>
-<li>If you force generation, this will erase all previous modification.</li>
+<li>Téma és modul sablonok, stíluslap (stylesheet) szerkesztése online.</li>
+<li>Összes felülíró (override) modul sablon generálása.</li>
+<li>Ha erőlteted a generálást, az összes korábbi módosítást törli.</li>
 </ul>
 ');
-define('_AM_SYSTEM_TEMPLATES_YOUR_THEMES', 'Your themes :');
-define('_AM_SYSTEM_TEMPLATES_GENERATE', 'Generated surcharge');
-define('_AM_SYSTEM_TEMPLATES_SELECT_THEME', 'Select theme');
-define('_AM_SYSTEM_TEMPLATES_FORCE_GENERATED', 'Forced file generation');
-define('_AM_SYSTEM_TEMPLATES_NAV_FILE_GENERATED', 'Files generated');
-define('_AM_SYSTEM_TEMPLATES_NOT_CREATED', 'No files created');
-define('_AM_SYSTEM_TEMPLATES_SAVE', 'Save');
-define('_AM_SYSTEM_TEMPLATES_RESTORE', 'Restore');
-define('_AM_SYSTEM_TEMPLATES_CANCEL', 'Cancel');
-define('_AM_SYSTEM_TEMPLATES_ERROR', 'Error');
-define('_AM_SYSTEM_TEMPLATES_EDIT', 'Edit of :');
-define('_AM_SYSTEM_TEMPLATES_SELECT_MODULES', 'Select Module');
-define('_AM_SYSTEM_TEMPLATES_MODULES', 'Modules : ');
-define('_AM_SYSTEM_TEMPLATES_FILES', 'Files');
-define('_AM_SYSTEM_TEMPLATES_EMPTY_FILE', 'Empty file');
-define('_AM_SYSTEM_TEMPLATES_TYPES', 'Types');
-define('_AM_SYSTEM_TEMPLATES_STATUS', 'Status');
-define('_AM_SYSTEM_TEMPLATES_TEMPLATES', 'Templates');
-define('_AM_SYSTEM_TEMPLATES_BLOCKS', 'Blocks');
-define('_AM_SYSTEM_TEMPLATES_SELECT_TEMPLATES', 'Select Templates');
-define('_AM_SYSTEM_TEMPLATES_ALL_MODULES', 'All modules');
-define('_AM_SYSTEM_TEMPLATES_RESTORE_OK', 'Successfully restored');
-define('_AM_SYSTEM_TEMPLATES_RESTORE_NOTOK', 'Failed to restore');
-define('_AM_SYSTEM_TEMPLATES_SET', 'Choose Template');
+define('_AM_SYSTEM_TEMPLATES_YOUR_THEMES', 'Témáid:');
+define('_AM_SYSTEM_TEMPLATES_GENERATE', 'Generált felülírás (surcharge)');
+define('_AM_SYSTEM_TEMPLATES_SELECT_THEME', 'Téma kiválasztása');
+define('_AM_SYSTEM_TEMPLATES_FORCE_GENERATED', 'Fájl generálásának erőltetése');
+define('_AM_SYSTEM_TEMPLATES_NAV_FILE_GENERATED', 'Generált fájlok');
+define('_AM_SYSTEM_TEMPLATES_NOT_CREATED', 'Nincsenek létrehozott fájlok');
+define('_AM_SYSTEM_TEMPLATES_SAVE', 'Mentés');
+define('_AM_SYSTEM_TEMPLATES_RESTORE', 'Visszaállítás');
+define('_AM_SYSTEM_TEMPLATES_CANCEL', 'Mégsem');
+define('_AM_SYSTEM_TEMPLATES_ERROR', 'Hiba');
+define('_AM_SYSTEM_TEMPLATES_EDIT', 'Szerkesztés:');
+define('_AM_SYSTEM_TEMPLATES_SELECT_MODULES', 'Modul kiválasztása');
+define('_AM_SYSTEM_TEMPLATES_MODULES', 'Modulok: ');
+define('_AM_SYSTEM_TEMPLATES_FILES', 'Fájlok');
+define('_AM_SYSTEM_TEMPLATES_EMPTY_FILE', 'Üres fájl');
+define('_AM_SYSTEM_TEMPLATES_TYPES', 'Típusok');
+define('_AM_SYSTEM_TEMPLATES_STATUS', 'Állapot');
+define('_AM_SYSTEM_TEMPLATES_TEMPLATES', 'Sablonok');
+define('_AM_SYSTEM_TEMPLATES_BLOCKS', 'Blokkok');
+define('_AM_SYSTEM_TEMPLATES_SELECT_TEMPLATES', 'Sablonok kiválasztása');
+define('_AM_SYSTEM_TEMPLATES_ALL_MODULES', 'Minden modul');
+define('_AM_SYSTEM_TEMPLATES_RESTORE_OK', 'Sikeresen visszaállítva');
+define('_AM_SYSTEM_TEMPLATES_RESTORE_NOTOK', 'A visszaállítás sikertelen');
+define('_AM_SYSTEM_TEMPLATES_SET', 'Sablon Kiválasztása');
