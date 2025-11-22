70cf8eec8e17022599e3dadde065c7e20aab331b
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:18:18 +0100

    Create hu_dlg.js

---
+++ core/2.5.12/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/xoopsquote/langs/hu_dlg.js
@@ -0,0 +1,5 @@
+tinyMCE.addI18n('hu.xoopsquote_dlg',{
+    xoopsquote_title:"Idézet Beillesztése",
+    xoopsquote_desc:"Idézet Beillesztése",
+    xoopsquote_sub:"Illeszd be az idézni kívánt szöveget az alábbi mezőbe:"
+});
