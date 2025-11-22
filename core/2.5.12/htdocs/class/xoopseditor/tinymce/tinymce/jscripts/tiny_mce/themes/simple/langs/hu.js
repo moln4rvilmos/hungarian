6fab536c6c048a45851c6935bb85222676f3587c
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:27:08 +0100

    Create hu.js

---
+++ core/2.5.12/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/themes/simple/langs/hu.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('hu.simple',{
+    "cleanup_desc":"Rendetlen Kód Tisztítása",
+    "redo_desc":"Ismét (Ctrl+Y)",
+    "undo_desc":"Visszavonás (Ctrl+Z)",
+    "numlist_desc":"Számozott Lista Beszúrása/Eltávolítása",
+    "bullist_desc":"Felsorolás Beszúrása/Eltávolítása",
+    "striketrough_desc":"Áthúzott",
+    "underline_desc":"Aláhúzás (Ctrl+U)",
+    "italic_desc":"Dőlt (Ctrl+I)",
+    "bold_desc":"Félkövér (Ctrl+B)"
+});
