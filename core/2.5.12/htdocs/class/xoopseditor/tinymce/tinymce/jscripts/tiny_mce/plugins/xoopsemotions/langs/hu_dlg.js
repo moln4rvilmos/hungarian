3485242dc1f05380c9331eaba50b2b23bdab722d
Molnár Vilmos <moln4rvilmos@gmail.com>
2025-11-22 08:11:59 +0100

    Create hu_dlg.js

---
+++ core/2.5.12/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/xoopsemotions/langs/hu_dlg.js
@@ -0,0 +1,9 @@
+/**
+ */
+
+tinyMCE.addI18n('hu.xoopsemotions_dlg',{
+    title : 'Xoops Hangulatjelek Beillesztése',
+    tab_emotionsbrowser: 'Hangulatjelek',
+    tab_emotionsadmin: 'Hangulatjelek Hozzáadása',
+    error_noemotions: 'Nincs hangulatjel az adatbázisban!!!'
+});
